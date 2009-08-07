eb0182b5aee2b228924736246d5bdb8a53bdb9be
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2009-08-07 05:59:15 +0000

    diser ver

---
+++ branches/cloudobserver/CloudObserverVirtualCamerasServiceLibrary/JPEG_camera.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using System.ServiceModel;
+using CloudObserverBroadcastServiceLibrary;
+using System.Net;
+
+namespace CloudObserverVirtualCamerasServiceLibrary
+{
+    class JPEG_camera : VirtualCamera
+    {
+        int cameraID;
+        string source;
+        string userName;
+        string password;
+        int framesCounter = 0;
+        Timer broadcastingTimer;
+        ICloudObserverBroadcastService broadcastServiceClient;
+
+        public JPEG_camera(int cameraID, string cloudObserverBroadcastServiceUri)
+        {
+            this.cameraID = cameraID;
+
+            broadcastServiceClient = ChannelFactory<ICloudObserverBroadcastService>.CreateChannel(new BasicHttpBinding(), new EndpointAddress(cloudObserverBroadcastServiceUri));
+
+            broadcastingTimer = new Timer(1000);
+            broadcastingTimer.Elapsed += new ElapsedEventHandler(broadcastingTimer_Elapsed);
+        }
+
+        private void broadcastingTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(source);
+            //httpWebRequest.Credentials = new NetworkCredential(userName, password);
+            WebClient webClient = new WebClient();
+            webClient.Credentials = new NetworkCredential(userName, password);
+            //HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            //if (httpWebRequest.ContentType == "image/jpeg")
+            broadcastServiceClient.WriteFrame(cameraID, webClient.DownloadData(source),DateTime.Now,false);
+            //httpWebResponse.Close();
+            framesCounter++;
+        }
+
+        public override void SetSource(string source)
+        {
+            this.source = source;
+        }
+
+        public override void SetCredentials(string userName, string password)
+        {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public override void SetFPS(int fps)
+        {
+            broadcastingTimer.Interval = 1000 / fps;
+        }
+
+        public override void StartBroadcasting()
+        {
+            broadcastingTimer.Start();
+        }
+
+        public override void StopBroadcasting()
+        {
+            broadcastingTimer.Stop();
+        }
+
+        public override int GetFramesCounter()
+        {
+            return framesCounter;
+        }
+    }
+}
---
+++ branches/cloudobserver/run.bat
@@ -0,0 +1,7 @@
+start bin\SimplePolicyRetrieverServiceHoster.exe
+start bin\SimpleServiceHoster.exe %cd%\bin\CloudObserverAccountsServiceLibrary.dll CloudObserverAccountsService
+start bin\SimpleServiceHoster.exe %cd%\bin\CloudObserverAuthorizationServiceLibrary.dll CloudObserverAuthorizationService
+start bin\SimpleServiceHoster.exe %cd%\bin\CloudObserverBroadcastServiceLibrary.dll CloudObserverBroadcastService
+start bin\SimpleServiceHoster.exe %cd%\bin\CloudObserverStorageServiceLibrary.dll CloudObserverStorageService
+start bin\SimpleServiceHoster.exe %cd%\bin\CloudObserverVirtualCamerasServiceLibrary.dll CloudObserverVirtualCamerasService
+CloudObserverWebsite\Default.html
