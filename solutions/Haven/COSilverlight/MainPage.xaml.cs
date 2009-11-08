568553a3f3ac49f25267697d2490ede9d2e833ec
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2009-11-08 22:03:39 +0000

    Haven project consists of 2 sub projects C# with Sample data Web with site ///// Only C# one is important. C# project shows how to use Sample Data, IsolatedStorageFileStream, Costume Lists Item Renderer's. ////// Open Tasks list. // server side task group 1 Task to connect it some how with SS project. Task to connect it to Data base and fill with it's data Item List Task to somehow send Name to DB or just WCF... // design side task group 1 !Task to Implement media element renderer plane Task to implement cool clouds back Task to key video black back Task to Create HLSL sun filter or somehow find a walkaround // Server task group 2 Connect and send data on requests to media element // Design task group 2 Implement Item List search, implement options

---
+++ solutions/Haven/COSilverlight/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.IO.IsolatedStorage;
+using System.IO;
+
+namespace COSilverlight
+{
+	public partial class MainPage : UserControl
+	{
+		private const string FILENAME = "localName.txt";
+
+        private IsolatedStorageFileStream isfs;
+        //private bool isShiftDown = false;
+		// begin=)
+		public MainPage()
+		{
+			InitializeComponent();
+			border.Visibility = invisiable.Visibility; // hide cameras list
+			loadLocalStorage(); // try to load data from local Storage
+		}
+
+      //ACTIONS
+		private void playBut(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            play();
+        }
+		
+        private void KeyUP(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+        	if(e.Key.ToString() == "Enter")
+			{
+				play();
+			}
+        }
+		//LOGIK
+        void play()
+        {
+            saveLocalStorage(UserName.Text); //save Text input "Name" data to local Storage
+            border.Visibility = image.Visibility; //SHOW ITEMS LIST
+            Canvas.Visibility = invisiable.Visibility; //HIDE "NAME" INPUT
+        }
+        // update data to local storage
+        void saveLocalStorage(string value)
+        {
+            IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication();
+            if(store.FileExists(FILENAME))
+                isfs = new IsolatedStorageFileStream(FILENAME, FileMode.Truncate, store);
+            else
+                isfs = new IsolatedStorageFileStream(FILENAME, FileMode.OpenOrCreate, store);
+
+            StreamWriter streamWriter = new StreamWriter(isfs);
+            streamWriter.Write(value);
+            streamWriter.Flush();
+            streamWriter.Close();
+            isfs = null;
+        }
+
+        // load date from local storage
+        void loadLocalStorage()
+        {
+            IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication();
+
+            if(store.FileExists(FILENAME)){
+                isfs = new IsolatedStorageFileStream(FILENAME, FileMode.OpenOrCreate, store);
+                StreamReader streamReader = new StreamReader(isfs);
+                string s;
+                while ((s = streamReader.ReadLine()) != null)
+                    UserName.Text = (s);
+				
+                streamReader.Close();
+            }
+        }
+
+	
+	}
+}
