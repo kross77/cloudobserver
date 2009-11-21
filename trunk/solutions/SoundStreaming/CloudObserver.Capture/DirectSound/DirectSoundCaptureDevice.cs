245f3fa96be1fe99cbb14d194ce5b7af3b6b6913
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2009-11-21 16:32:39 +0000

    CloudObserver.Capture assembly source code update (additional comments added).

--- trunk/solutions/SoundStreaming/CloudObserver.Capture/DirectSound/DirectSoundCaptureDevice.cs
+++ trunk/solutions/SoundStreaming/CloudObserver.Capture/DirectSound/DirectSoundCaptureDevice.cs
@@ -1,32 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.DirectX.DirectSound;
 
 namespace CloudObserver.Capture.DirectSound
 {
     /// <summary>
-    /// Represents a sound capture device like a microphone or line-in.
+    /// Represents an audio capture device like a microphone or line-in in terms of Microsoft DirectSound API.
     /// </summary>
     public struct DirectSoundCaptureDevice : IEquatable<DirectSoundCaptureDevice>
     {
+        /// <summary>
+        /// A textual description of the Microsoft DirectSound device.
+        /// </summary>
         private string description;
+
+        /// <summary>
+        /// The globally unique identifier (GUID) of a Microsoft DirectSound driver.
+        /// </summary>
         private Guid driverGuid;
+
+        /// <summary>
+        /// The module name of the Microsoft DirectSound driver corresponding to this device.
+        /// </summary>
         private string moduleName;
 
         /// <summary>
-        /// Operating system default sound capture device.
+        /// Operating system default audio capture device.
         /// </summary>
         public static readonly DirectSoundCaptureDevice Default = new DirectSoundCaptureDevice("Default", Guid.Empty, "");
 
         /// <summary>
-        /// Lists sound capture devices which are currently available in the operating system.
+        /// Lists audio capture devices which are currently available in the operating system.
         /// </summary>
         public static IEnumerable<DirectSoundCaptureDevice> AllAvailable
         {
             get
             {
-                CaptureDevicesCollection captureDevicesCollection = new CaptureDevicesCollection();
-                foreach (DeviceInformation deviceInformation in captureDevicesCollection)
+                Microsoft.DirectX.DirectSound.CaptureDevicesCollection captureDevicesCollection =
+                    new Microsoft.DirectX.DirectSound.CaptureDevicesCollection();
+                foreach (Microsoft.DirectX.DirectSound.DeviceInformation deviceInformation in captureDevicesCollection)
                 {
                     yield return new DirectSoundCaptureDevice(
                         deviceInformation.Description,
@@ -51,46 +62,78 @@ namespace CloudObserver.Capture.DirectSound
         /// </summary>
         public string ModuleName { get { return moduleName; } }
 
-        public static bool operator ==(DirectSoundCaptureDevice device1, DirectSoundCaptureDevice device2)
-        {
-            return device1.Equals(device2);
-        }
-
-        public static bool operator !=(DirectSoundCaptureDevice device1, DirectSoundCaptureDevice device2)
-        {
-            return !(device1 == device2);
-        }
-
-        public bool Equals(DirectSoundCaptureDevice other)
-        {
-            return driverGuid == other.driverGuid;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (!(obj is DirectSoundCaptureDevice))
-            {
-                return false;
-            }
-
-            return base.Equals((DirectSoundCaptureDevice)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return driverGuid.GetHashCode();
-        }
-
-        public override string ToString()
-        {
-            return Description;
-        }
-
+        /// <summary>
+        /// Initializes a new instance of the CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice class.
+        /// </summary>
+        /// <param name="description">A textual description of the Microsoft DirectSound device.</param>
+        /// <param name="driverGuid">The globally unique identifier (GUID) of a Microsoft DirectSound driver.</param>
+        /// <param name="moduleName">The module name of the Microsoft DirectSound driver corresponding to this device.</param>
         public DirectSoundCaptureDevice(string description, Guid driverGuid, string moduleName)
         {
             this.description = description;
             this.driverGuid = driverGuid;
             this.moduleName = moduleName;
+        }
+
+        /// <summary>
+        /// Compares two CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice objects.
+        /// </summary>
+        /// <param name="device1">A CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice to compare.</param>
+        /// <param name="device2">A CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice to compare.</param>
+        /// <returns>true if the device1 and device2 values are not equal; otherwise, false.</returns>
+        public static bool operator !=(DirectSoundCaptureDevice device1, DirectSoundCaptureDevice device2)
+        {
+            return !(device1 == device2);
+        }
+
+        /// <summary>
+        /// Compares two CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice objects.
+        /// </summary>
+        /// <param name="device1">A CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice to compare.</param>
+        /// <param name="device2">A CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice to compare.</param>
+        /// <returns>true if the device1 and device2 values are equal; otherwise, false.</returns>
+        public static bool operator ==(DirectSoundCaptureDevice device1, DirectSoundCaptureDevice device2)
+        {
+            return device1.Equals(device2);
+        }
+
+        /// <summary>
+        /// Determines whether the specified System.Object is equal to the current System.Object.
+        /// </summary>
+        /// <param name="obj">The System.Object to compare with the current System.Object.</param>
+        /// <returns>true if the specified System.Object is equal to the current System.Object; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is DirectSoundCaptureDevice)) { return false; }
+            return base.Equals((DirectSoundCaptureDevice)obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice type.
+        /// </summary>
+        /// <returns>A hash code for the current CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice.</returns>
+        public override int GetHashCode()
+        {
+            return driverGuid.GetHashCode();
+        }
+
+        /// <summary>
+        /// Returns a System.String that represents the current CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice.
+        /// </summary>
+        /// <returns>A System.String that represents the current CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice.</returns>
+        public override string ToString()
+        {
+            return Description;
+        }
+
+        /// <summary>
+        /// Indicates whether the current object represents the same Microsoft DirectSound device as the another one.
+        /// </summary>
+        /// <param name="other">An CloudObserver.Capture.DirectSound.DirectSoundCaptureDevice object to compare with this object.</param>
+        /// <returns>true if the current object represents the same Microsoft DirectSound device as the another one; otherwise, false.</returns>
+        public bool Equals(DirectSoundCaptureDevice other)
+        {
+            return driverGuid == other.driverGuid;
         }
     }
 }
--- trunk/solutions/SoundStreaming/CloudObserver.Capture/ICapture.cs
+++ trunk/solutions/SoundStreaming/CloudObserver.Capture/ICapture.cs
@@ -2,11 +2,24 @@
 
 namespace CloudObserver.Capture
 {
+    /// <summary>
+    /// Interface of a type which represents some capture class (e.g. DirectSound or DirectShow captures).
+    /// </summary>
     public interface ICapture
     {
+        /// <summary>
+        /// Occurs when the new chunk of data has been captured.
+        /// </summary>
         event EventHandler<ChunkCapturedEventArgs> ChunkCaptured;
 
+        /// <summary>
+        /// Starts capturing.
+        /// </summary>
         void Start();
+
+        /// <summary>
+        /// Stops capturing.
+        /// </summary>
         void Stop();
     }
 }
