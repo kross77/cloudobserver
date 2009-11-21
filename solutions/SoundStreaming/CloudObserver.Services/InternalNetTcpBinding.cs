94491b07659f791649504013387de21fb8d15ef3
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2009-11-21 20:58:19 +0000

    !CloudObserver.Services assembly update (additional comments added).

--- solutions/SoundStreaming/CloudObserver.Services/InternalNetTcpBinding.cs
+++ solutions/SoundStreaming/CloudObserver.Services/InternalNetTcpBinding.cs
@@ -3,18 +3,24 @@ using System.ServiceModel;
 
 namespace CloudObserver.Services
 {
+    /// <summary>
+    /// A binding derived from a System.ServiceModel.NetTcpBinding used to communicate between services in cloud.
+    /// </summary>
     public class InternalNetTcpBinding : NetTcpBinding
     {
-        private const int maxReceivedMessageSize = 2147483647;
-        private const int maxArrayLength = 2147483647;
+        private const int maxReceivedMessageSize = 2147483647; // 2 GB
+        private const int maxArrayLength = 2147483647; // 2 GB
 
+        /// <summary>
+        /// Initializes a new instance of the CloudObserver.Services.InternalNetTcpBinding class.
+        /// </summary>
         public InternalNetTcpBinding()
         {
             this.MaxReceivedMessageSize = maxReceivedMessageSize;
-            this.OpenTimeout = TimeSpan.FromMinutes(5);
-            this.CloseTimeout = TimeSpan.FromMinutes(5);
-            this.ReceiveTimeout = TimeSpan.FromMinutes(30);
-            this.SendTimeout = TimeSpan.FromMinutes(30);
+            this.OpenTimeout = TimeSpan.FromSeconds(30);
+            this.CloseTimeout = TimeSpan.FromSeconds(30);
+            this.ReceiveTimeout = TimeSpan.FromSeconds(30);
+            this.SendTimeout = TimeSpan.FromSeconds(30);
             this.ReaderQuotas.MaxArrayLength = maxArrayLength;
         }
     }
