a7f86683948a9d535afcf10523012c5b64d26e04
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2009-11-21 16:32:39 +0000

    CloudObserver.Capture assembly source code update (additional comments added).

--- solutions/SoundStreaming/CloudObserver.Capture/ChunkCapturedEventArgs.cs
+++ solutions/SoundStreaming/CloudObserver.Capture/ChunkCapturedEventArgs.cs
@@ -2,12 +2,22 @@
 
 namespace CloudObserver.Capture
 {
+    /// <summary>
+    /// Provides data for the CloudObserver.Capture.ICapture.ChunkCaptured event. Containts captured chunk of data.
+    /// </summary>
     public class ChunkCapturedEventArgs : EventArgs
     {
         private byte[] chunkData;
 
+        /// <summary>
+        /// Captured chunk of data.
+        /// </summary>
         public byte[] ChunkData { get { return chunkData; } }
 
+        /// <summary>
+        /// Initializes a new instance of the CloudObserver.Capture.ChunkCapturedEventArgs class.
+        /// </summary>
+        /// <param name="chunkData">Captured chunk of data.</param>
         public ChunkCapturedEventArgs(byte[] chunkData)
         {
             this.chunkData = chunkData;
--- solutions/SoundStreaming/CloudObserver.Capture/DirectSound/DirectSoundCapture.cs
+++ solutions/SoundStreaming/CloudObserver.Capture/DirectSound/DirectSoundCapture.cs
@@ -1,66 +1,79 @@
 ﻿using System;
 using System.Threading;
-using Microsoft.DirectX.DirectSound;
 
 using CloudObserver.Formats.Audio;
 
 namespace CloudObserver.Capture.DirectSound
 {
     /// <summary>
-    /// This class captures raw PCM audio data using DirectSound technology.
+    /// Captures raw PCM audio data using Microsoft DirectSound API.
     /// </summary>
     public class DirectSoundCapture : ICapture
     {
         #region Fields
+        /// <summary>
+        /// An indicator of whether audio data is capturing.
+        /// </summary>
         private bool capturing;
+
+        /// <summary>
+        /// A PCM audio format in which data is captured.
+        /// </summary>
         private PcmAudioFormat pcmAudioFormat;
+
+        /// <summary>
+        /// Microsoft DirectSound device to capture audio data.
+        /// </summary>
         private DirectSoundCaptureDevice directSoundCaptureDevice;
 
-        private int notifySize;
+        /// <summary>
+        /// Size of a chunk of audio data.
+        /// </summary>
+        private int chunkSize;
+
+        /// <summary>
+        /// Size of a capture buffer.
+        /// </summary>
         private int captureBufferSize;
-        private CaptureBuffer captureBuffer;
+
+        /// <summary>
+        /// A buffer to store captured audio data.
+        /// </summary>
+        private Microsoft.DirectX.DirectSound.CaptureBuffer captureBuffer;
+
+        /// <summary>
+        /// Microsoft.DirectX.DirectSound.WaveFormat in which data is captured.
+        /// </summary>
         private Microsoft.DirectX.DirectSound.WaveFormat? cachedWaveFormat;
+
+        /// <summary>
+        /// A thread to process notifications sent by DirectSound API.
+        /// </summary>
         private Thread notificationListenerThread;
+
+        /// <summary>
+        /// An event object representing notification arrival.
+        /// </summary>
         private AutoResetEvent notificationArrivalEvent;
-        #endregion
+        #endregion Fields
 
         #region Events
+        /// <summary>
+        /// Occurs when the new chunk of data has been captured.
+        /// </summary>
         public event EventHandler<ChunkCapturedEventArgs> ChunkCaptured;
-        #endregion
+        #endregion Events
 
         #region Constants
-        private const int notifyPositions = 16;
-        #endregion
-
-        #region Constructors
-        public DirectSoundCapture() : this(PcmAudioFormat.Pcm22kHz16bitMono, DirectSoundCaptureDevice.Default) { }
-
-        public DirectSoundCapture(PcmAudioFormat pcmAudioFormat, DirectSoundCaptureDevice directSoundCaptureDevice)
-        {
-            this.pcmAudioFormat = pcmAudioFormat;
-            this.directSoundCaptureDevice = directSoundCaptureDevice;
-        }
-        #endregion
-
-        #region Public Methods
-        public void Start()
-        {
-            capturing = true;
-            InitializeDirectSound();
-            captureBuffer.Start(true);
-        }
-
-        public void Stop()
-        {
-            capturing = false;
-            if (captureBuffer != null)
-                captureBuffer.Stop();
-        }
-        #endregion
-
-        #region Private Methods
         /// <summary>
-        /// Converts PcmAudioFormat to DirectX WaveFormat.
+        /// The number of notification positions in the capture buffer.
+        /// </summary>
+        private const int notificationPositions = 16;
+        #endregion Constants
+
+        #region Properties
+        /// <summary>
+        /// Retrieves a CloudObserver.Formats.Audio.PcmAudioFormat converted into Microsoft.DirectX.DirectSound.WaveFormat.
         /// </summary>
         private Microsoft.DirectX.DirectSound.WaveFormat CaptureFormat
         {
@@ -69,46 +82,96 @@ namespace CloudObserver.Capture.DirectSound
                 if (!cachedWaveFormat.HasValue)
                 {
                     Microsoft.DirectX.DirectSound.WaveFormat waveFormat = new Microsoft.DirectX.DirectSound.WaveFormat();
-                    waveFormat.FormatTag = WaveFormatTag.Pcm;
+                    waveFormat.FormatTag = Microsoft.DirectX.DirectSound.WaveFormatTag.Pcm;
                     waveFormat.Channels = pcmAudioFormat.Channels;
                     waveFormat.BitsPerSample = pcmAudioFormat.BitsPerSample;
                     waveFormat.SamplesPerSecond = pcmAudioFormat.SamplesPerSecond;
                     waveFormat.BlockAlign = pcmAudioFormat.BlockAlign;
                     waveFormat.AverageBytesPerSecond = pcmAudioFormat.AverageBytesPerSecond;
-                    notifySize = waveFormat.AverageBytesPerSecond / 8;
-                    captureBufferSize = notifySize * notifyPositions;
+                    chunkSize = waveFormat.AverageBytesPerSecond / 8;
+                    captureBufferSize = chunkSize * notificationPositions;
 
                     cachedWaveFormat = waveFormat;
                 }
                 return cachedWaveFormat.Value;
             }
         }
+        #endregion Properties
 
+        #region Constuctors
+        /// <summary>
+        /// Initializes a new instance of the CloudObserver.Capture.DirectSound.DirectSoundCapture class
+        /// with the operating system default audio capture device and 22050 Hz, 16 bit, Mono audio format.
+        /// </summary>
+        public DirectSoundCapture() : this(PcmAudioFormat.Pcm22kHz16bitMono, DirectSoundCaptureDevice.Default) { }
+
+        /// <summary>
+        /// Initializes a new instance of the CloudObserver.Capture.DirectSound.DirectSoundCapture class
+        /// with the provided PCM audio format and Microsoft DirectSound device.
+        /// </summary>
+        /// <param name="pcmAudioFormat">A PCM audio format in which data will be captured.</param>
+        /// <param name="directSoundCaptureDevice">Microsoft DirectSound device to capture audio data.</param>
+        public DirectSoundCapture(PcmAudioFormat pcmAudioFormat, DirectSoundCaptureDevice directSoundCaptureDevice)
+        {
+            this.pcmAudioFormat = pcmAudioFormat;
+            this.directSoundCaptureDevice = directSoundCaptureDevice;
+        }
+        #endregion Constructors
+
+        #region Public Methods
+        /// <summary>
+        /// Starts capturing.
+        /// </summary>
+        public void Start()
+        {
+            capturing = true;
+            InitializeDirectSound();
+            captureBuffer.Start(true);
+        }
+
+        /// <summary>
+        /// Stops capturing.
+        /// </summary>
+        public void Stop()
+        {
+            capturing = false;
+            if (captureBuffer != null)
+                captureBuffer.Stop();
+        }
+        #endregion Public Methods
+
+        #region Private Methods
+        /// <summary>
+        /// Initializes Microsoft DirectSound capture buffer, sets notifications on it and starts notification listener thread.
+        /// </summary>
         private void InitializeDirectSound()
         {
             Microsoft.DirectX.DirectSound.Capture captureDevice = (directSoundCaptureDevice == DirectSoundCaptureDevice.Default) ?
                 new Microsoft.DirectX.DirectSound.Capture() : new Microsoft.DirectX.DirectSound.Capture(directSoundCaptureDevice.DriverGuid);
-            CaptureBufferDescription captureBufferDescription = new CaptureBufferDescription();
+            Microsoft.DirectX.DirectSound.CaptureBufferDescription captureBufferDescription = new Microsoft.DirectX.DirectSound.CaptureBufferDescription();
             captureBufferDescription.Format = CaptureFormat;
             captureBufferDescription.BufferBytes = captureBufferSize;
-            captureBuffer = new CaptureBuffer(captureBufferDescription, captureDevice);
+            captureBuffer = new Microsoft.DirectX.DirectSound.CaptureBuffer(captureBufferDescription, captureDevice);
 
             notificationArrivalEvent = new AutoResetEvent(false);
-            BufferPositionNotify[] positionNotifies = new BufferPositionNotify[notifyPositions];
-            for (int i = 0; i < notifyPositions; i++)
+            Microsoft.DirectX.DirectSound.BufferPositionNotify[] positionNotifies = new Microsoft.DirectX.DirectSound.BufferPositionNotify[notificationPositions];
+            for (int i = 0; i < notificationPositions; i++)
             {
-                positionNotifies[i].Offset = notifySize * (i + 1) - 1;
+                positionNotifies[i].Offset = chunkSize * (i + 1) - 1;
                 positionNotifies[i].EventNotifyHandle = notificationArrivalEvent.SafeWaitHandle.DangerousGetHandle();
             }
 
-            Notify applicationNotify = new Notify(captureBuffer);
-            applicationNotify.SetNotificationPositions(positionNotifies, notifyPositions);
+            Microsoft.DirectX.DirectSound.Notify applicationNotify = new Microsoft.DirectX.DirectSound.Notify(captureBuffer);
+            applicationNotify.SetNotificationPositions(positionNotifies, notificationPositions);
 
             notificationListenerThread = new Thread(new ThreadStart(ListenDirectSoundNotifications));
             notificationListenerThread.IsBackground = true;
             notificationListenerThread.Start();
         }
 
+        /// <summary>
+        /// Listens Microsoft DirectSound capture buffer notifications and invokes ChunkCaptured event when a new data is captured.
+        /// </summary>
         private void ListenDirectSoundNotifications()
         {
             try
@@ -117,8 +180,9 @@ namespace CloudObserver.Capture.DirectSound
                 while (capturing)
                 {
                     notificationArrivalEvent.WaitOne(Timeout.Infinite, true);
-                    ChunkCaptured.Invoke(this, new ChunkCapturedEventArgs((byte[])captureBuffer.Read(offset, typeof(byte), LockFlag.None, notifySize)));
-                    offset = (offset + notifySize) % captureBufferSize;
+                    ChunkCaptured.Invoke(this, new ChunkCapturedEventArgs((byte[])captureBuffer.Read(offset, typeof(byte),
+                        Microsoft.DirectX.DirectSound.LockFlag.None, chunkSize)));
+                    offset = (offset + chunkSize) % captureBufferSize;
                 }
             }
             finally
@@ -126,6 +190,6 @@ namespace CloudObserver.Capture.DirectSound
                 capturing = false;
             }
         }
-        #endregion
+        #endregion Private Methods
     }
 }
