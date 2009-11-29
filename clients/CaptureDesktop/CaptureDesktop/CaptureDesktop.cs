d5791c1827319f63f17c821b808a7f4359c85977
anton.bondarev1978 <anton.bondarev1978@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2009-11-29 19:18:55 +0000

    added cursor capture to capture desktop client

--- clients/CaptureDesktop/CaptureDesktop/CaptureDesktop.cs
+++ clients/CaptureDesktop/CaptureDesktop/CaptureDesktop.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Drawing.Imaging;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace CaptureDesktop
 {
@@ -45,21 +46,182 @@ namespace CaptureDesktop
                     return 1;                        
             }
         }
-        /// <summary>
+/*
+        static Bitmap CaptureFrame()
+        {
+            SIZE size;
+            IntPtr hBitmap;
+            IntPtr hDC = Win32Stuff.GetDC(Win32Stuff.GetDesktopWindow());
+            IntPtr hMemDC = GDIStuff.CreateCompatibleDC(hDC);
+
+            size.cx = Win32Stuff.GetSystemMetrics
+                      (Win32Stuff.SM_CXSCREEN);
+
+            size.cy = Win32Stuff.GetSystemMetrics
+                      (Win32Stuff.SM_CYSCREEN);
+
+            hBitmap = GDIStuff.CreateCompatibleBitmap(hDC, size.cx, size.cy);
+
+            if (hBitmap != IntPtr.Zero)
+            {
+                IntPtr hOld = (IntPtr)GDIStuff.SelectObject
+                                       (hMemDC, hBitmap);
+
+                GDIStuff.BitBlt(hMemDC, 0, 0, size.cx, size.cy, hDC,
+                                               0, 0, GDIStuff.SRCCOPY);
+
+                GDIStuff.SelectObject(hMemDC, hOld);
+                GDIStuff.DeleteDC(hMemDC);
+                Win32Stuff.ReleaseDC(Win32Stuff.GetDesktopWindow(), hDC);
+                Bitmap bmp = System.Drawing.Image.FromHbitmap(hBitmap);
+                GDIStuff.DeleteObject(hBitmap);
+                GC.Collect();
+                return bmp;
+            }
+            return null;
+
+        }
+
+        public static Bitmap CaptureDesktopWithCursor()
+        {
+            int cursorX = 0;
+            int cursorY = 0;
+            Bitmap desktopBMP;
+            Bitmap cursorBMP;
+            Bitmap finalBMP;
+            Graphics g;
+            Rectangle r;
+
+            desktopBMP = CaptureFrame();
+            cursorBMP = CaptureCursor(ref cursorX, ref cursorY);
+            if (desktopBMP != null)
+            {
+                if (cursorBMP != null)
+                {
+                    r = new Rectangle(cursorX, cursorY, cursorBMP.Width, cursorBMP.Height);
+                    g = Graphics.FromImage(desktopBMP);
+                    g.DrawImage(cursorBMP, r);
+                    g.Flush();
+
+                    return desktopBMP;
+                }
+                else
+                    return desktopBMP;
+            }
+
+            return null;        
+        }
+        */
+       /// <summary>
         /// internal function/ it's called by timer for capture one desktop frame
         /// </summary>
         /// <param name="stateInfo"></param>
         private static void CaptureFrame(Object stateInfo)
         {            
-            Bitmap Bitmap;
-            Graphics Graps;
+            Bitmap bmpDesctop, bmpCursor;
+            Graphics g;
+            int curX = 0, curY = 0;
 
-            Bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
-            Graps = Graphics.FromImage(Bitmap);
-            Graps.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
-            //Bitmap.Save(System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\YourShot.jpeg", ImageFormat.Jpeg);
-            Bitmap.Save("YourShot" + DateTime.Now.ToString("h_mm_ss_fff") + ".jpeg", ImageFormat.Jpeg);
+            bmpDesctop = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            g = Graphics.FromImage(bmpDesctop);
+            g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            
+            bmpCursor = CaptureCursor(ref curX, ref curY);
+            Rectangle r = new Rectangle(curX, curY, bmpCursor.Width, bmpCursor.Height);
+            g.DrawImage(bmpCursor, r);
+            g.Flush();
+            bmpDesctop.Save("YourShot" + DateTime.Now.ToString("h_mm_ss_fff") + ".jpeg", ImageFormat.Jpeg);
         }
+/*    
+        static Bitmap CaptureCursor(ref int x, ref int y)
+        {
+            Bitmap bmp;
+            IntPtr hicon;
+            Win32Stuff.CURSORINFO ci = new Win32Stuff.CURSORINFO();
+            Win32Stuff.ICONINFO icInfo;
+            ci.cbSize = Marshal.SizeOf(ci);
+            if (Win32Stuff.GetCursorInfo(out ci))
+            {
+                if (ci.flags == Win32Stuff.CURSOR_SHOWING)
+                {
+                    hicon = Win32Stuff.CopyIcon(ci.hCursor);
+                    if (Win32Stuff.GetIconInfo(hicon, out icInfo))
+                    {
+                        x = ci.ptScreenPos.x - ((int)icInfo.xHotspot);
+                        y = ci.ptScreenPos.y - ((int)icInfo.yHotspot);
+
+                        Icon ic = Icon.FromHandle(hicon);
+                        bmp = ic.ToBitmap();
+                        return bmp;
+                    }
+                }
+            }
+
+            return null;
+        }
+*/
+        private static Bitmap CaptureCursor(ref int x, ref int y)
+        {
+            Win32Stuff.CURSORINFO cursorInfo = new Win32Stuff.CURSORINFO();
+            cursorInfo.cbSize = Marshal.SizeOf(cursorInfo);
+            if (!Win32Stuff.GetCursorInfo(out cursorInfo))
+                return null;
+
+            if (cursorInfo.flags != Win32Stuff.CURSOR_SHOWING)
+                return null;
+
+            IntPtr hicon = Win32Stuff.CopyIcon(cursorInfo.hCursor);
+            if (hicon == IntPtr.Zero)
+                return null;
+
+            Win32Stuff.ICONINFO iconInfo;
+            if (!Win32Stuff.GetIconInfo(hicon, out iconInfo))
+                return null;
+
+            x = cursorInfo.ptScreenPos.x - ((int)iconInfo.xHotspot);
+            y = cursorInfo.ptScreenPos.y - ((int)iconInfo.yHotspot);
+
+            using (Bitmap maskBitmap = Bitmap.FromHbitmap(iconInfo.hbmMask))
+            {
+                // Is this a monochrome cursor?
+                if (maskBitmap.Height == maskBitmap.Width * 2)
+                {
+                    Bitmap resultBitmap = new Bitmap(maskBitmap.Width, maskBitmap.Width);
+
+                    Graphics desktopGraphics = Graphics.FromHwnd(Win32Stuff.GetDesktopWindow());
+                    IntPtr desktopHdc = desktopGraphics.GetHdc();
+
+                    IntPtr maskHdc = Win32Stuff.CreateCompatibleDC(desktopHdc);
+                    IntPtr oldPtr = Win32Stuff.SelectObject(maskHdc, maskBitmap.GetHbitmap());
+
+                    using (Graphics resultGraphics = Graphics.FromImage(resultBitmap))
+                    {
+                        IntPtr resultHdc = resultGraphics.GetHdc();
+
+                        // These two operation will result in a black cursor over a white background.
+                        // Later in the code, a call to MakeTransparent() will get rid of the white background.
+                        Win32Stuff.BitBlt(resultHdc, (IntPtr)0, (IntPtr)0, (IntPtr)32, (IntPtr)32, maskHdc, (IntPtr)0, (IntPtr)32, ((IntPtr)Win32Stuff.TernaryRasterOperations.SRCCOPY));
+                        Win32Stuff.BitBlt(resultHdc, (IntPtr)0, (IntPtr)0, (IntPtr)32, (IntPtr)32, maskHdc, (IntPtr)0, (IntPtr)0, (IntPtr)Win32Stuff.TernaryRasterOperations.SRCINVERT);
+
+                        resultGraphics.ReleaseHdc(resultHdc);
+                    }
+
+                    IntPtr newPtr = Win32Stuff.SelectObject(maskHdc, oldPtr);
+                    Win32Stuff.DeleteDC(newPtr);
+                    Win32Stuff.DeleteDC(maskHdc);
+                    desktopGraphics.ReleaseHdc(desktopHdc);
+
+                    // Remove the white background from the BitBlt calls,
+                    // resulting in a black cursor over a transparent background.
+                    resultBitmap.MakeTransparent(Color.White);
+                    return resultBitmap;
+                }
+            }
+
+            Icon icon = Icon.FromHandle(hicon);
+            return icon.ToBitmap();
+        }
+
 
         public int Start(int freq)
         {
