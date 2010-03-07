620a1fc8a0ca057088bd267af6405b216e018918
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-03-07 17:40:34 +0000

    #19 Kernel: Создать приложение для запуска сервиса 1. Создано приложение для запуска сервиса (проект CloudObserver.ServiceHostApplication). 2. Создан класс ServicesHelper, содержащий вспомогательные методы для работы с сервисами.

---
+++ trunk/co-kernel/Projects/CloudObserver/Services/ServicesHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CloudObserver.Services
+{
+    /// <summary>
+    /// Contains some static helper methods to work with services.
+    /// </summary>
+    public static class ServicesHelper
+    {
+        /// <summary>
+        /// Returns a service contract type from a string representation of the service type.
+        /// </summary>
+        /// <param name="serviceType">String representation of the service type.</param>
+        /// <exception cref="System.ArgumentException">Thrown when there is no service type with given string representation.</exception>
+        /// <returns>Service contract type.</returns>
+        public static Type GetServiceContract(string serviceType)
+        {
+            switch (serviceType)
+            {
+                default:
+                    throw new ArgumentException("Invalid service type.");
+            }
+        }
+
+        /// <summary>
+        /// Creates an instance of a service by a string representation of the service type.
+        /// </summary>
+        /// <param name="serviceType">String representation of the service type.</param>
+        /// <exception cref="System.ArgumentException">Thrown when there is no service type with given string representation.</exception>
+        /// <returns>An instance of the service.</returns>
+        public static Object CreateServiceInstance(string serviceType)
+        {
+            switch (serviceType)
+            {
+                default:
+                    throw new ArgumentException("Invalid service type.");
+            }
+        }
+    }
+}
