32d3a1dbcc365705181371014fa7a2213be5044c
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-03-17 16:31:24 +0000

    Global CMS constants update now FP supports only Gateway addresses in such form - http://localhost:4773/cloudobserver NO "?wsdl" at the end needed.

--- co-cms/CMS/includes/constants.php
+++ co-cms/CMS/includes/constants.php
@@ -5,5 +5,5 @@ define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "000000");
 define("DB_NAME", "cms4");
-define("CLOUD_WSDL_URL", "http://localhost:4773/cloudobserver?wsdl");
+define("CLOUD_WSDL_URL", "http://localhost:4773/cloudobserver");
 ?>
