ac9c8a783ace88c42233e46c9fcf0c70d7b1ddae
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-03-28 21:17:55 +0000

    #38 Kernel: Прототип HTTP сервера для WB * Создан прототип HTTP сервера для WB. * Создан тестовый клиент для HTTP сервера.

---
+++ trunk/co-kernel/Projects/HttpClient/HttpClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+
+namespace HttpServerPrototype.HttpClient
+{
+    public class HttpClient
+    {
+        private const int bufferSize = 1024;
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Hostname: ");
+            string hostname = Console.ReadLine();
+            Console.Write("Port: ");
+            int port = Int32.Parse(Console.ReadLine());
+            Console.Write("Resource: ");
+            string resource = Console.ReadLine();
+
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(hostname, port);
+            NetworkStream networkStream = tcpClient.GetStream();
+            byte[] request = Encoding.UTF8.GetBytes("GET " + resource + " HTTP/1.0\r\n\r\n");
+            networkStream.Write(request, 0, request.Length);
+            networkStream.Flush();
+
+            byte[] buffer = new byte[bufferSize];
+            int read = networkStream.Read(buffer, 0, bufferSize);
+            while (read > 0)
+            {
+                Console.Write(Encoding.UTF8.GetString(buffer, 0, read));
+                Console.ReadKey();
+                read = networkStream.Read(buffer, 0, bufferSize);
+            }
+
+            networkStream.Close();
+            tcpClient.Close();
+            Console.ReadKey();
+        }
+    }
+}
