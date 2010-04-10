42196929e58e10739b531504ad4fa01d1422d6e9
anton.bondarev1978 <anton.bondarev1978@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-04-10 13:36:47 +0000

    work at control interface to DShowFilters

---
+++ trunk/ds_filters/cloud_stream_renderer/clouddslib.cs
@@ -0,0 +1,10 @@
+namespace CloudDirectShowLib
+{
+    [SuppressUnmanagedCodeSecurity]
+    [Guid("80AE3F22-17DE-4d6c-BC3E-F2A38AFC53CC")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ICoudInetControl
+    {
+        int GetCurFile(out string pszAddress);
+    }
+}
