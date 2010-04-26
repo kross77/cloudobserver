a95a7373a5628101f176308718490d9ad7e8a79b
anton.bondarev1978 <anton.bondarev1978@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-04-26 20:36:47 +0000

    now ds_filtr can connect to socket

--- ds_filters/cloud_stream_renderer/Filter.cpp
+++ ds_filters/cloud_stream_renderer/Filter.cpp
@@ -10,11 +10,17 @@ CFilter::CFilter(TCHAR *tszName, LPUNKNOWN punk, HRESULT *phr) :
 {
 	// Class Constructor makes all routines for 
 	// preparation to samples sendings via sockets
-	//WBADDRESS = OLESTR("127.0.0.1");
-	//PORT = 31123;
+	WBADDRESS = OLESTR("127.0.0.1");
+	PORT = 8001;
 
+	    WORD wVersionRequested;
 
-	Connect(WBADDRESS,PORT);
+		WSADATA wsaData;
+		wVersionRequested = MAKEWORD(2, 2);
+
+	WSAStartup(wVersionRequested, &wsaData);
+
+//	Connect(WBADDRESS,PORT);
 
 }
 CFilter::~CFilter()
@@ -24,7 +30,9 @@ CFilter::~CFilter()
 
 CUnknown * WINAPI CFilter::CreateInstance(LPUNKNOWN punk, HRESULT *phr)
 {
-	ASSERT(phr);    
+	ASSERT(phr); 
+
+
 	CFilter *pNewObject = new CFilter(FILTER_TCHAR_NAME, punk, phr);
     if (pNewObject == NULL) 
 	{
@@ -48,21 +56,29 @@ hr = pAsfWriter->QueryInterface(IID_ICoudInetControl, (void **)
 &m_FileSink);
 m_FileSink->SetFileName(tfilename, NULL)
 */
-	send ( Socket,(const char *) buff, pMediaSample->GetActualDataLength(), 0 ) ;
+	//send ( Socket,(const char *) buff, pMediaSample->GetActualDataLength(), 0 ) ;
 
 	return NOERROR;
 }
 
-void CFilter::Connect(LPCOLESTR WBADDRESS, int PORT)
+int CFilter::Connect(LPCOLESTR addr, int port)
 {
-	WSAStartup ( 0x202, &WSAInformation );
-	Socket = socket (AF_INET, SOCK_STREAM, 0);
+	int sock;
+	struct sockaddr_in socketaddr;
+	memset((char *)&socketaddr, NULL, sizeof(socketaddr));
+	socketaddr.sin_family = AF_INET;
+	socketaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	socketaddr.sin_port = htons(port);
+	sock = socket(AF_INET,SOCK_STREAM, IPPROTO_TCP);
 
-	ConnectionInfo.sin_family			= AF_INET;
-	ConnectionInfo.sin_port				= htons (PORT);
-	ConnectionInfo.sin_addr.S_un.S_addr = inet_addr((const char *)WBADDRESS) ;
+	if (sock == INVALID_SOCKET)
+	{
+		return -1;
+	}
 
-	connect (Socket, (sockaddr *) &ConnectionInfo , sizeof (ConnectionInfo));
+	int size=sizeof(socketaddr);
+
+	sock = connect(sock,(LPSOCKADDR)&socketaddr,size);
 }
 
 void CFilter::Disconnect()
@@ -74,8 +90,9 @@ void CFilter::Disconnect()
 
 HRESULT CFilter::SetAddress( LPCOLESTR pszAddress, int port) 
 {
-	PORT = port;
-	WBADDRESS = pszAddress;
+	//PORT = port;
+	//WBADDRESS = pszAddress;
+	Connect(WBADDRESS,PORT);
 	return NOERROR;
 }
 
