32e564e86f4d7cd5fc0707ecb64a1d4d236e87f6
anton.bondarev1978 <anton.bondarev1978@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-05-03 14:54:02 +0000

    modify test for cloud stream filter

--- trunk/ds_filters/StreamReceiver/Program.cs
+++ trunk/ds_filters/StreamReceiver/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
 
 namespace StreamReceiver
 {
@@ -23,16 +24,27 @@ namespace StreamReceiver
                 Console.WriteLine("Waiting for a connection.....");
                 Socket s = myList.AcceptSocket();
                 Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
-                byte[] b = new byte[100];
-                int k = s.Receive(b);
-                Console.WriteLine("Recieved...");
-                for (int i = 0; i < k; i++)
-                    Console.Write(Convert.ToChar(b[i]));
-                ASCIIEncoding asen = new ASCIIEncoding();
-                s.Send(asen.GetBytes("The string was recieved by the server."));
-                Console.WriteLine("\nSent Acknowledgement");
+                byte[] b = new byte[1024];
+                string path = @"c:\copied.mp3";
+                using (FileStream sw = File.Create(path))
+                {
+
+                    while (true)
+                    {
+                        int k = s.Receive(b);
+                        sw.Write(b, 0, k);                        
+                    }
+                    sw.Close();
+                }
+                
+                //for (int i = 0; i < k; i++)
+                 //   Console.Write(Convert.ToChar(b[i]));
+                //ASCIIEncoding asen = new ASCIIEncoding();
+                //s.Send(asen.GetBytes("The string was recieved by the server."));
+                //Console.WriteLine("\nSent Acknowledgement");
                 /* clean up */
                 s.Close();
+                Console.WriteLine("Recieved...");
                 myList.Stop();
             }
             catch (Exception e)
