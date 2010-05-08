992c053c2794eb92bc3cd1c60a93ae059b5a6d45
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-05-08 14:46:44 +0000

    #66 Kernel: Вынести все клиентские приложения 1. Проект !MultimediaPlayer перенесён в раздел клиентских приложений. 2. Проект !MultimediaTransmitter перенесён в раздел клиентских приложений. 3. Проект !WriterClient перенесён в раздел клиентских приложений. 4. Проект !TestClient удалён в связи с ненадобностью. 5. Проекты !CloudPhone и !CloudPhoneClient удалены в связи с ненадобностью. 6. Проект Mp3Sharp удалён в связи с ненадобностью (отныне интегрирован в библиотеку Auxiliaries). 7. Проект Multimedia удалён в связи с ненадобностью (отныне интегрирован в библиотеку Auxiliaries). 8. Все клиентские приложения модифицированы в связи с отделением модулей работы с мультимедиа от библиотеки ядра.

---
+++ co-clients/Projects/CloudObserver.Clients.MultimediaPlayer/WindowMain.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+
+using CloudObserver.Clients.Auxiliaries.Multimedia;
+using Microsoft.DirectX.DirectSound;
+
+namespace CloudObserver.Clients.MultimediaPlayer
+{
+    public partial class WindowMain : Window
+    {
+        private Device device;
+        private Player player;
+        private bool playing = false;
+
+        public WindowMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            device = new Device();
+            device.SetCooperativeLevel(new WindowInteropHelper(this).Handle, CooperativeLevel.Priority);
+        }
+
+        private void buttonEldoradioPlayStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonEldoradioPlayStop.Content.ToString() == "Play")
+            {
+                if (playing)
+                    player.Stop();
+                buttonEldoradioPlayStop.Content = "Stop";
+                buttonLoveRadioPlayStop.Content = "Play";
+                buttonKCDXPlayStop.Content = "Play";
+                buttonOtherPlayStop.Content = "Play";
+                player = new Player(textBoxEldoradioUri.Text, device);
+                player.Play();
+                playing = true;
+            }
+            else
+            {
+                buttonEldoradioPlayStop.Content = "Play";
+                player.Stop();
+                playing = false;
+            }
+        }
+
+        private void buttonLoveRadioPlayStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonLoveRadioPlayStop.Content.ToString() == "Play")
+            {
+                if (playing)
+                    player.Stop();
+                buttonEldoradioPlayStop.Content = "Play";
+                buttonLoveRadioPlayStop.Content = "Stop";
+                buttonKCDXPlayStop.Content = "Play";
+                buttonOtherPlayStop.Content = "Play";
+                player = new Player(textBoxLoveRadioUri.Text, device);
+                player.Play();
+                playing = true;
+            }
+            else
+            {
+                buttonLoveRadioPlayStop.Content = "Play";
+                player.Stop();
+                playing = false;
+            }
+        }
+
+        private void buttonKCDXPlayStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonKCDXPlayStop.Content.ToString() == "Play")
+            {
+                if (playing)
+                    player.Stop();
+                buttonEldoradioPlayStop.Content = "Play";
+                buttonLoveRadioPlayStop.Content = "Play";
+                buttonKCDXPlayStop.Content = "Stop";
+                buttonOtherPlayStop.Content = "Play";
+                player = new Player(textBoxKCDXUri.Text, device);
+                player.Play();
+                playing = true;
+            }
+            else
+            {
+                buttonKCDXPlayStop.Content = "Play";
+                player.Stop();
+                playing = false;
+            }
+        }
+
+        private void buttonOtherPlayStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonOtherPlayStop.Content.ToString() == "Play")
+            {
+                if (playing)
+                    player.Stop();
+                buttonEldoradioPlayStop.Content = "Play";
+                buttonLoveRadioPlayStop.Content = "Play";
+                buttonKCDXPlayStop.Content = "Play";
+                buttonOtherPlayStop.Content = "Stop";
+                textBoxOtherUri.IsReadOnly = true;
+                player = new Player(textBoxOtherUri.Text, device);
+                player.Play();
+                playing = true;
+            }
+            else
+            {
+                buttonOtherPlayStop.Content = "Play";
+                textBoxOtherUri.IsReadOnly = false;
+                player.Stop();
+                playing = false;
+            }
+        }
+    }
+}
--- co-kernel/Projects/CloudPhone/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Windows;
-
-namespace CloudObserver.CloudPhone
-{
-    public partial class App : Application
-    {
-    }
-}
