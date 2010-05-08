8027b81ec362df893cd8803310e2261eb0c8fdca
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-05-08 13:42:28 +0000

    #56 Kernel: Почистить библиотеку ядра 1. Создана папка для клиентских приложений. 2. Модули работы с мультимедиа вынесены в отдельную сборку !CloudObserver.Clients.Auxiliaries. 3. Почищен проект библиотеки ядра.

---
+++ trunk/co-clients/Projects/CloudObserver.Clients.Auxiliaries/Formats/Audio/WaveFormats.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CloudObserver.Clients.Auxiliaries.Formats.Audio
+{
+    public enum WaveFormats
+    {
+        Pcm = 1,
+        Float = 3
+    }
+}
---
+++ trunk/co-clients/Projects/CloudObserver.Clients.Auxiliaries/Services/GW/IGateway.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceModel;
+
+namespace CloudObserver.Clients.Auxiliaries.Services.GW
+{
+    [ServiceContract]
+    public interface IGateway// : IService
+    {
+        //[OperationContract]
+        //string GetWorkBlock();
+
+        [OperationContract]
+        string IWannaRead(int[] ids, out string contentTypes);
+
+        [OperationContract]
+        string IWannaWrite(int id);
+
+        [OperationContract]
+        string IWannaWriteCustom(int id, string contentType);
+    }
+}
