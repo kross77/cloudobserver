0ed28ad3f94a96f0f45e0175f1deaf4f6dee0492
anton.bondarev1978 <anton.bondarev1978@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-05-10 20:01:24 +0000

    added skelet for cloud ds client

---
+++ trunk/ds_filters/CloudDsWpfClient/clouddslib.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Security;
+using DirectShowLib;
+using System.Windows.Forms;
+
+namespace CloudObserver.DirectShow.Interfaces
+{
+    [SuppressUnmanagedCodeSecurity]
+    [Guid("80AE3F22-17DE-4d6c-BC3E-F2A38AFC53CC")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ICloudInetControl
+    {
+        int SetAddress(out string pszAddress, int port);
+    }
+}
+
+namespace CloudObserver.DirectShow.Filters
+{
+
+    //[Guid("37AC047C-BED1-49ef-AB43-BB906A158DD6")]
+    public class StreamRenderer
+    {
+        private Guid CLSID_CloudStreamRenderer = new Guid("{37AC047C-BED1-49ef-AB43-BB906A158DD6}");
+        public StreamRenderer()
+        {
+
+        }
+    }
+    //37AF7C74-9887-42bd-858A-FF4D4035ED47
+    public static class CloudFileWriter
+    {
+        private static Guid CLSID_CloudFileWriter = new Guid("{37AF7C74-9887-42bd-858A-FF4D4035ED47}");
+        private static IBaseFilter baseFilter;
+
+        public static IBaseFilter BaseFilter
+        {
+            get 
+            {
+                if(null == CloudFileWriter.baseFilter)
+                {
+                    CloudFileWriter.baseFilter = (IBaseFilter)Activator.CreateInstance(Type.GetTypeFromCLSID(CLSID_CloudFileWriter));                   
+                }
+                return CloudFileWriter.baseFilter;
+            }            
+        }
+        public static int SetFileName(string fileName)
+        {
+            IFileSinkFilter fileSink = CloudFileWriter.BaseFilter as IFileSinkFilter;
+            if (null == fileSink)
+                return -1;
+
+            return fileSink.SetFileName(fileName, null);
+        }
+    }
+}
+
+namespace CloudObserver.DirectShow
+{
+    public static class Utils
+    {
+        public static void checkHR(int hr, string message)
+        {
+            if (hr < 0)
+            {
+                MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                DsError.ThrowExceptionForHR(hr);
+            }
+        }
+        public static IPin GetPin(IBaseFilter filter, string pinname)
+        {
+            IEnumPins epins;
+            int hr = filter.EnumPins(out epins);
+            checkHR(hr, "Can't enumerate pins.");
+            IntPtr fetched = Marshal.AllocCoTaskMem(4);
+            IPin[] pins = new IPin[1];
+            while (epins.Next(1, pins, fetched) == 0)
+            {
+                PinInfo pinfo;
+                pins[0].QueryPinInfo(out pinfo);
+                bool found = (pinfo.name == pinname);
+                DsUtils.FreePinInfo(pinfo);
+                if (found) return pins[0];
+            }
+            checkHR(-1, "Pin not found.");
+            return null;
+        }
+
+        public static IPin GetFirstOutputPin(IBaseFilter filter)
+        {
+            IEnumPins epins;
+            int hr = filter.EnumPins(out epins);
+            checkHR(hr, "Can't enumerate pins.");
+            IntPtr fetched = Marshal.AllocCoTaskMem(4);
+            IPin[] pins = new IPin[1];
+            while (epins.Next(1, pins, fetched) == 0)
+            {
+                PinInfo pinfo;
+                pins[0].QueryPinInfo(out pinfo);
+                bool found = (pinfo.dir == PinDirection.Output);
+                DsUtils.FreePinInfo(pinfo);
+                if (found) return pins[0];
+            }
+            checkHR(-1, "Pin not found.");
+            return null;
+        }
+    }
+}
+
+
