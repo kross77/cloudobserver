f19266dbce465ad7b4abf6b8e7b5c82cd0abd3ae
anton.bondarev1978 <anton.bondarev1978@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-05-10 11:34:37 +0000

    rework filefriter filter

--- ds_filters/renderer_writer/Filter.cpp
+++ ds_filters/renderer_writer/Filter.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include"guids.h"
+#include "dll.h"
 #include "Filter.h"
 
 EXTERN_C const GUID CLSID_RendererSkelet;
@@ -8,34 +9,20 @@ CFilter::CFilter(TCHAR *tszName, LPUNKNOWN punk, HRESULT *phr) :
     CBaseRenderer(CLSID_RendererSkelet, tszName, punk, phr),
 		m_InputPin(NAME("Input Pin"),this,&m_InterfaceLock,phr,L"Input")
 {
-	m_hFile = CreateFile("c:/br_br.avi",       // name of the write
-                       GENERIC_WRITE,          // open for writing
-                       0,                      // do not share
-                       NULL,                   // default security
-                       CREATE_NEW,             // create new file only
-                       FILE_ATTRIBUTE_NORMAL,  // normal file
-                       NULL);                  // no attr. template
-	if (m_hFile == INVALID_HANDLE_VALUE) 
-    { 
-	m_hFile = CreateFile("c:/br_br_br.avi",       // name of the write
-                       GENERIC_WRITE,          // open for writing
-                       0,                      // do not share
-                       NULL,                   // default security
-                       CREATE_NEW,             // create new file only
-                       FILE_ATTRIBUTE_NORMAL,  // normal file
-                       NULL);                  // no attr. template
-    }
-
 
 }
+
 CFilter::~CFilter()
 {
+	FileClose();
+
+    //delete m_pFileName;
 }
 
 CUnknown * WINAPI CFilter::CreateInstance(LPUNKNOWN punk, HRESULT *phr)
 {
 	ASSERT(phr);    
-	CFilter *pNewObject = new CFilter(NAME("fv Add Graph To ROT Sample Filter"), punk, phr);
+	CFilter *pNewObject = new CFilter(FILTER_TCHAR_NAME, punk, phr);
     if (pNewObject == NULL) {
         if (phr)
             *phr = E_OUTOFMEMORY;
@@ -65,15 +52,22 @@ HRESULT CFilter::DoRenderSample(IMediaSample *pMediaSample)
 STDMETHODIMP CFilter::NonDelegatingQueryInterface(REFIID riid, void ** ppv)
 {
     CheckPointer(ppv,E_POINTER);
- //   CAutoLock lock(&m_Lock);
+	CAutoLock lock(&m_Lock);
 
-    // Do we have this interface
-
+	if (riid == IID_IFileSinkFilter) {
+        return GetInterface((IFileSinkFilter *) this, ppv);
+    }
 
 	if (riid == IID_IBaseFilter)
 	{
 		return GetInterface((IBaseFilter *) this, ppv);
 	}
+
+	if (riid == IID_IMediaFilter) 
+	{
+		return GetInterface((IMediaFilter *) this, ppv);
+	}
+
 	/*we must return the same way || riid == IID_IMediaFilter || riid == IID_IMediaPosition */
 	/*if (riid == IID_IBaseFilter)
 	{
@@ -83,3 +77,148 @@ STDMETHODIMP CFilter::NonDelegatingQueryInterface(REFIID riid, void ** ppv)
 	/* return all not support interfaces */
 	return CBaseRenderer::NonDelegatingQueryInterface(riid,ppv);
 }
+
+
+HRESULT CFilter::OnStopStreaming()
+{
+	FileClose();
+	return NOERROR;
+}
+
+HRESULT	CFilter::OnStartStreaming()
+{
+ //ResetStreamingTimes();
+	
+	return FileOpen();
+}
+
+HRESULT	CFilter::FileOpen()
+{
+    TCHAR *pFileName = NULL;
+
+    // Is the file already opened
+    if (m_hFile != INVALID_HANDLE_VALUE) {
+        return NOERROR;
+    }
+
+    // Has a filename been set yet
+    if (m_pFileName == NULL) {
+        return ERROR_INVALID_NAME;
+    }
+
+    // Convert the UNICODE filename if necessary
+
+#if defined(WIN32) && !defined(UNICODE)
+    char convert[MAX_PATH];
+
+    if(!WideCharToMultiByte(CP_ACP,0,m_pFileName,-1,convert,MAX_PATH,0,0))
+        return ERROR_INVALID_NAME;
+
+    pFileName = convert;
+#else
+    pFileName = m_pFileName;
+#endif
+
+    // Try to open the file
+
+    m_hFile = CreateFile((LPCTSTR) pFileName,   // The filename
+                         GENERIC_WRITE,         // File access
+                         FILE_SHARE_READ,       // Share access
+                         NULL,                  // Security
+                         CREATE_ALWAYS,         // Open flags
+                         (DWORD) 0,             // More flags
+                         NULL);                 // Template
+
+    if (m_hFile == NULL) 
+    {
+        DWORD dwErr = GetLastError();
+        return HRESULT_FROM_WIN32(dwErr);
+    }
+
+    return S_OK;
+	}
+HRESULT	CFilter::FileClose()
+{
+    // Must lock this section to prevent problems related to
+    // closing the file while still receiving data in Receive()
+    CAutoLock lock(&m_Lock);
+
+    if (m_hFile == NULL) {
+        return NOERROR;
+    }
+
+    CloseHandle(m_hFile);
+    m_hFile = NULL; // Invalidate the file 
+
+    return NOERROR;
+}
+
+//
+// SetFileName
+//
+// Implemented for IFileSinkFilter support
+//
+STDMETHODIMP CFilter::SetFileName(LPCOLESTR pszFileName,const AM_MEDIA_TYPE *pmt)
+{
+    // Is this a valid filename supplied
+
+    CheckPointer(pszFileName,E_POINTER);
+    if(wcslen(pszFileName) > MAX_PATH)
+        return ERROR_FILENAME_EXCED_RANGE;
+
+    // Take a copy of the filename
+
+    size_t len = 1+lstrlenW(pszFileName);
+    m_pFileName = new WCHAR[len];
+    if (m_pFileName == 0)
+        return E_OUTOFMEMORY;
+
+    HRESULT hr = StringCchCopyW(m_pFileName, len, pszFileName);
+
+    // Clear the global 'write error' flag that would be set
+    // if we had encountered a problem writing the previous dump file.
+    // (eg. running out of disk space).
+//    m_fWriteError = FALSE;
+
+    // Create the file then close it
+
+    hr = FileOpen();
+    FileClose();
+
+    return hr;
+
+} // SetFileName
+
+
+//
+// GetCurFile
+//
+// Implemented for IFileSinkFilter support
+//
+STDMETHODIMP CFilter::GetCurFile(LPOLESTR * ppszFileName,AM_MEDIA_TYPE *pmt)
+{
+    CheckPointer(ppszFileName, E_POINTER);
+    *ppszFileName = NULL;
+
+    if (m_pFileName != NULL) 
+    {
+        size_t len = 1+lstrlenW(m_pFileName);
+        *ppszFileName = (LPOLESTR)
+        QzTaskMemAlloc(sizeof(WCHAR) * (len));
+
+        if (*ppszFileName != NULL) 
+        {
+            HRESULT hr = StringCchCopyW(*ppszFileName, len, m_pFileName);
+        }
+    }
+
+    if(pmt) 
+    {
+        ZeroMemory(pmt, sizeof(*pmt));
+        pmt->majortype = MEDIATYPE_NULL;
+        pmt->subtype = MEDIASUBTYPE_NULL;
+    }
+
+    return S_OK;
+
+} // GetCurFile
---
+++ ds_filters/renderer_writer/dll.h
@@ -0,0 +1,10 @@
+#ifndef __DLL_H_
+#define __DLL_H_
+
+#define FILTER_TCHAR_NAME "CloudFileWriter"
+#define FILTER_WCHAR_NAME L"CloudFileWriter"
+
+EXTERN_C const GUID CLSID_Filter;
+EXTERN_C const WCHAR g_wszName[];
+
+#endif
