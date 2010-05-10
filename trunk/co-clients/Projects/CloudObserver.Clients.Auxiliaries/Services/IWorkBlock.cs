fa375ae2ab7724ed04275469d15184d078e019a5
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-05-10 02:45:06 +0000

    #69 Clients: Модифицировать .NET-клиента (сервисная архитектура) 1. Внесены изменения в .NET-клиента в соответствии с новой сервисной архитектурой ядра. 2. Успешно проведено тестирование в связке с !MultimediaPlayer'ом.

---
+++ trunk/co-clients/Projects/CloudObserver.Clients.Auxiliaries/Services/IWorkBlock.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+
+namespace CloudObserver.Clients.Auxiliaries.Services
+{
+    [ServiceContract]
+    public interface IWorkBlock
+    {
+        [OperationContract]
+        string IWannaRead(int[] ids);
+
+        [OperationContract]
+        string IWannaWrite(int id, string contentType);
+
+        [OperationContract]
+        void ConnectToController(string controllerAddress);
+    }
+}
