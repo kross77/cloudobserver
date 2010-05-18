f0671542f28c927e5b2f0d567364b711342eae4a
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-05-18 20:10:33 +0000

    Some new explanations about Data return types formating (JSON); Test example is cleaner

--- trunk/co-cms/CMS.API/EXAMPLE/Program.cs
+++ trunk/co-cms/CMS.API/EXAMPLE/Program.cs
@@ -13,7 +13,8 @@ namespace EXAMPLE
             API ap = new API();
             
             Random random = new Random();
-            String str = random.Next(1, 100000).ToString();
+            int ran = random.Next(1, 100000);
+            String str = ran.ToString();
 
             Console.WindowWidth = 120;
             Console.WindowHeight = 50;
@@ -28,21 +29,20 @@ namespace EXAMPLE
             Console.WriteLine("\n Log Methods");
             Console.WriteLine("log In " + ap.logIn("myRealEmail" + str + "@real.su", "myPass"));
             Console.WriteLine("log Out " + ap.logOut(ap.logIn("myRealEmail" + str + "@real.su", "myPass")));
-        
+
+            Console.WriteLine("\n Set Methods");
+            Console.WriteLine("set a new user name: " + ap.setMyName(ap.logIn("myRealEmail" + str + "@real.su", "myPass"), "myName"));
+            Console.WriteLine("add a stream to users streams collection: " + ap.setStream(ap.logIn("myRealEmail" + str + "@real.su", "myPass"), ran));
+
             Console.WriteLine("\n Get Methods");
             Console.WriteLine("get Kernel Gateway Address (Yes! you should get Kernel address by calling CMS) " + ap.getGatewayAddress(ap.logIn("myRealEmail" + str + "@real.su", "myPass")));
             Console.WriteLine("get user name by KEY " + ap.getMyName(ap.logIn("myRealEmail" + str + "@real.su", "myPass")));
             Console.WriteLine("get users streams (KEY, AMMOUNT, ORDER (none = default, DO NOT CHANGE until CMS v3.5)) \n " + ap.getMyStreams(ap.logIn("myRealEmail" + str + "@real.su", "myPass"), 30, "none"));
             Console.WriteLine("get NOT user streams  (KEY, AMMOUNT, ORDER (none = default, DO NOT CHANGE until CMS v3.4)) \n " + ap.getStreamsFromAll(ap.logIn("myRealEmail" + str + "@real.su", "myPass"), 35, "none"));
       
-            Console.WriteLine("\n Set Methods");
-            Console.WriteLine("set a new user name: " + ap.setMyName(ap.logIn("myRealEmail" + str + "@real.su", "myPass"), "myName"));
-            // Canceled by now - not in spec of 3.1 Console.WriteLine("set new password to user" + ap.setMyNewPassword(ap.logIn("myRealEmail@real.life", "myPass"), "myRealEmail@real.life", "myPass", "iHaveNewPass"));
-            Console.WriteLine("add a stream to users streams collection: " + ap.setStream(ap.logIn("myRealEmail" + str + "@real.su", "myPass"), 4663));
-            // Not ready to go =(  
-            Console.WriteLine("\n Delete Methods");   
-            Console.WriteLine("delete stream from users streams collection " + ap.deleteStream(ap.logIn("myRealEmail@real.life", "iHaveNewPass"), 4663));
-            Console.WriteLine("delete a user " + ap.deleteUser(ap.logIn("myRealEmail@real.life", "iHaveNewPass"), "myName", "myRealEmail@real.life", "iHaveNewPass"));
+            Console.WriteLine("\n Delete Methods");
+            Console.WriteLine("delete stream from users streams collection " + ap.deleteStream(ap.logIn("myRealEmail" + str + "@real.su", "myPass"), ran));
+            Console.WriteLine("delete a user " + ap.deleteUser(ap.logIn("myRealEmail" + str + "@real.su", "myPass"), "myName", "myRealEmail" + str + "@real.su", "myPass"));
             
             Console.Read();
         }
