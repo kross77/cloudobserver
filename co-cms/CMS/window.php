201d254b34b03f3d65e6353da1ce518b73b86348
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-05-26 11:05:24 +0000

    Update  - included window opening functionality in Debug.php page

---
+++ co-cms/CMS/window.php
@@ -0,0 +1,165 @@
+<?php require_once("includes/session.php"); ?>
+<?php require_once("includes/connection.php"); ?>
+<?php require_once("includes/functions.php"); ?>
+<?php confirm_logged_in(); ?>
+<?php $adress = CLOUD_OBSERVER_GATEWAY_ADDRESS; ?>
+<?php $windowadress = "window.php"; ?>
+<?php $vidtoopen = (string)$_GET[videourl]; ?>
+<!-- 
+Smart developers always View Source. 
+ 
+This application was built using Adobe Flex, an open source framework
+for building rich Internet applications that get delivered via the
+Flash Player or to desktops via Adobe AIR. 
+ 
+Learn more about Flex at http://flex.org 
+// --> 
+ 
+<head> 
+	<link rel="stylesheet" href="css/common.css" type="text/css" />
+	<link type="text/css" rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/base/ui.all.css" />
+	<link type="text/css" href="css/ui.multiselect.css" rel="stylesheet" />
+		<link type="text/css" href="css/ui-darkness/jquery-ui.css" rel="stylesheet" />	
+		
+  	<link rel="stylesheet" href="css/slide.css" type="text/css" media="screen" />
+		
+		<script type="text/javascript" src="js/jquery-1.4.1.js"></script>
+	<script type="text/javascript" src="js/jquery-ui-1.7.1.custom.min.js"></script>
+	<script type="text/javascript" src="js/plugins/localisation/jquery.localisation-min.js"></script>
+	<script type="text/javascript" src="js/plugins/scrollTo/jquery.scrollTo-min.js"></script>
+	<script type="text/javascript" src="js/ui.multiselect.js"></script>
+	<script type="text/javascript">
+		$(function(){
+			$.localise('ui-multiselect', {/*language: 'en',*/ path: 'js/locale/'});
+			$(".multiselect").multiselect();
+			$('#switcher').themeswitcher();
+		});
+	</script>
+		<script type="text/javascript" src="js/ui.formValidator.js"></script>
+		<script src="media/AC_OETags.js" language="javascript"></script>
+		<link rel="stylesheet" type="text/css" href="media/history/history.css" />
+		<script src="media/history/history.js" language="javascript"></script>
+		<script language="JavaScript" type="text/javascript">
+
+// -----------------------------------------------------------------------------
+// Globals
+// Major version of Flash required
+var requiredMajorVersion = 10;
+// Minor version of Flash required
+var requiredMinorVersion = 0;
+// Minor version of Flash required
+var requiredRevision = 0;
+// -----------------------------------------------------------------------------
+
+</script>
+</head>
+
+<body>
+
+
+<script language="JavaScript" type="text/javascript">
+
+// Version check for the Flash Player that has the ability to start Player Product Install (6.0r65)
+var hasProductInstall = DetectFlashVer(6, 0, 65);
+
+// Version check based upon the values defined in globals
+var hasRequestedVersion = DetectFlashVer(requiredMajorVersion, requiredMinorVersion, requiredRevision);
+
+if ( hasProductInstall && !hasRequestedVersion ) {
+	// DO NOT MODIFY THE FOLLOWING FOUR LINES
+	// Location visited after installation is complete if installation is required
+	var MMPlayerType = (isIE == true) ? "ActiveX" : "PlugIn";
+	var MMredirectURL = window.location;
+    document.title = document.title.slice(0, 47) + " - Flash Player Installation";
+    var MMdoctitle = document.title;
+
+	AC_FL_RunContent(
+		"src", "media/playerProductInstall",
+		"FlashVars", "MMredirectURL="+MMredirectURL+'&MMplayerType='+MMPlayerType+'&MMdoctitle='+MMdoctitle+"",
+		"width", "100%",
+		"height", "100%",
+		"align", "middle",
+		"id", "CloudObserverCMSFP",
+		"quality", "high",
+		"bgcolor", "#ffffff",
+		"name", "CloudObserverCMSFP",
+		"allowScriptAccess","sameDomain",
+		"type", "application/x-shockwave-flash",
+		"pluginspage", "http://www.adobe.com/go/getflashplayer"
+	);
+} else if (hasRequestedVersion) {
+	// if we've detected an acceptable version
+	// embed the Flash Content SWF when all tests are passed
+	AC_FL_RunContent(
+			"src", "media/CloudObserverCMSFP",
+			   "flashVars", "
+			   <?php
+  if (isset($_POST['submitForm'])){
+	  	echo "url=" , $adress;
+	  	echo "&baseuri=" , $windowadress;
+	  		echo "&videourl" , $vidtoopen;
+	  $array =	$_POST[Streams];
+$count = count($array);
+echo "&sid=" ;
+for ($i = 0; $i < $count; $i++) {
+ echo $array[$i];
+ if($i <= $count - 2){echo ",";} else{}
+
+}}?>
+",
+			"width", "100%",
+			"height", "300",
+			"align", "middle",
+			"id", "CloudObserverCMSFP",
+			"quality", "high",
+			"bgcolor", "#ffffff",
+			"name", "CloudObserverCMSFP",
+			"allowScriptAccess","sameDomain",
+			"type", "application/x-shockwave-flash",
+			"pluginspage", "http://www.adobe.com/go/getflashplayer"
+	);
+  } else {  // flash is too old or we can't detect the plugin
+    var alternateContent = 'Alternate HTML content should be placed here. '
+  	+ 'This content requires the Adobe Flash Player. '
+   	+ '<a href=http://www.adobe.com/go/getflash/>Get Flash</a>';
+    document.write(alternateContent);  // insert non-flash content
+  }
+
+</script>
+<noscript>
+  	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+			id="CloudObserverCMSFP" width="100%" height="300"
+			codebase="http://fpdownload.macromedia.com/get/flashplayer/current/swflash.cab">
+			<param name="movie" value="media/CloudObserverCMSFP.swf" />
+			<param name="quality" value="high" />
+			<param name="bgcolor" value="#ffffff" />
+			<param name="allowScriptAccess" value="sameDomain" />
+			 <param name='flashVars' value='<?php
+			 
+
+  if (isset($_POST['submitForm'])){
+	  	echo ",url=" , $adress;
+	  	echo ",baseuri" , $windowadress;
+	  	echo ",videourl" , $vidtoopen;
+	  $array =	$_POST[Streams];
+$count = count($array);
+echo ",sid=" ;
+for ($i = 0; $i < $count; $i++) {
+ echo $array[$i];
+if($i <= $count - 2){echo ",";} else{}
+
+}}?>'/> 
+			<embed src="media/CloudObserverCMSFP.swf" quality="high" bgcolor="#ffffff"
+				width="100%" height="300" name="CloudObserverCMSFP" align="middle"
+				play="true"
+				loop="false"
+				quality="high"
+				allowScriptAccess="sameDomain"
+				type="application/x-shockwave-flash"
+				pluginspage="http://www.adobe.com/go/getflashplayer">
+			</embed>
+	</object>
+</noscript>
+</div>
+</body> 
+</html>
