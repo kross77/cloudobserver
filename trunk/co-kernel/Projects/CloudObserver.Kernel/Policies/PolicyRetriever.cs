334e59c32b581bb3fa8f80d1b00b58fc7e0fc52c
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-05-30 15:55:54 +0000

    #52 Kernel: Реализовать выдачу crossdomain.xml 1. Создан !PolicyRetriever-сервис, выдающий по HTTP-запросу policy-файл для Flash Player'а (crossdomain.xml). 2. Добавлен файл crossdomain.xml, разрешающий Flash Player'у все соединения с данным хостом. 3. При запуске RM-сервиса на 843 порту запускается !PolicyRetriever-сервис.

---
+++ trunk/co-kernel/Projects/CloudObserver.Kernel/Policies/PolicyRetriever.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.ServiceModel.Web;
+
+namespace CloudObserver.Kernel.Policies
+{
+    /// <summary>
+    /// Policy retriever service realization class.
+    /// </summary>
+    public class PolicyRetriever : IPolicyRetriever
+    {
+        /// <summary>
+        /// Provides Flash policy file.
+        /// </summary>
+        /// <returns>Stream containing Flash policy file.</returns>
+        public Stream GetFlashPolicy()
+        {
+            WebOperationContext.Current.OutgoingResponse.ContentType = "application/xml";
+            return new MemoryStream(Encoding.UTF8.GetBytes(Properties.Resources.crossdomain));
+        }
+    }
+}
