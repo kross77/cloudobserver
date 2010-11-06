4c050975d63ac7e1f93ce5d732ae0fe3e8fb6772
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-11-06 02:09:30 +0000

    Cleanup and refactoring in HelloVideoEncoding project.

--- HelloVideoEncoding/HelloVideoEncoding.cpp
+++ HelloVideoEncoding/HelloVideoEncoding.cpp
@@ -1,29 +1,170 @@
 #include "stdafx.h"
+#include "VideoEncoder.h"
+#include "Settings.h"
+
+// FFmpeg
 #include <avcodec.h>
 #include <avformat.h>
 #include <swscale.h>
 #include <windows.h>
 #include <iostream>
 
+// OpenCV
 #include <cv.h>
 #include <cxcore.h>
 #include <highgui.h>
 
-#include "VideoEncoder.h"
-#include "Settings.h"
+// Boost
 #include <boost/thread.hpp>
 #include <boost/timer.hpp>
 
-namespace this_thread = boost::this_thread;
 using namespace std;
+
+// FFmpeg
+VideoEncoder encoder;
+AVFrame* frame;
+AVFrame* readyFrame;
+int nSampleSize;
+char* sample;
+
+// OpenCV
+CvCapture* capture;
+
+void initOpenCV()
+{
+	/* initialize camera */
+	capture = cvCaptureFromCAM(0);
+
+	/* always check */
+	if (!capture)
+	{
+		fprintf(stderr, "Cannot initialize webcam!\n");
+		cin.get();
+	}
+
+	/* create a window for the video */
+	cvNamedWindow("HelloVideoEncoding", CV_WINDOW_AUTOSIZE);
+}
+
+void initOpenAL()
+{
+}
+
+void initFFmpeg(string filename, string container, int w, int h, int fps)
+{
+	encoder.SetFps(fps);
+
+	if (!encoder.InitFile(filename, container))
+	{
+		printf("Cannot initialize file!\n");
+		cin.get();
+	}
+
+	int bufferImgSize = avpicture_get_size(PIX_FMT_BGR24, w, h);
+
+	frame = avcodec_alloc_frame();
+	uint8_t* frameBuffer = (uint8_t*)av_mallocz(bufferImgSize);
+	avpicture_fill((AVPicture*)frame, frameBuffer, PIX_FMT_BGR24, w, h);
+
+	readyFrame = avcodec_alloc_frame();
+	uint8_t* readyFrameBuffer = (uint8_t*)av_mallocz(bufferImgSize);
+	avpicture_fill((AVPicture*)readyFrame, readyFrameBuffer, PIX_FMT_BGR24, w, h);
+
+	nSampleSize = 2 * 22050.0f / fps;
+	sample = new char[nSampleSize];
+}
+
+void init()
+{
+	initOpenCV();
+	initOpenAL();
+	initFFmpeg(FILE_NAME, CONTAINER, W_VIDEO, H_VIDEO, FPS);
+}
+
+void CaptureFrame(char* buffer, int w, int h, int bytespan)
+{
+	/* get a frame */
+	IplImage* CVframe = cvQueryFrame(capture);
+
+	/* always check */
+	if (!CVframe)
+	{
+		printf("No CV frame captured!\n");
+		cin.get();
+	}
+
+	/* display current frame */
+	IplImage* destination = cvCreateImage(cvSize(w, h), CVframe->depth, CVframe->nChannels);
+
+	//use cvResize to resize source to a destination image
+	cvResize(CVframe, destination);
+
+	IplImage* redchannel = cvCreateImage(cvGetSize(destination), 8, 1);
+	IplImage* greenchannel = cvCreateImage(cvGetSize(destination), 8, 1);
+	IplImage* bluechannel = cvCreateImage(cvGetSize(destination), 8, 1);
+
+	cvSplit(destination, bluechannel, greenchannel, redchannel, NULL);
+	for(int y = 0; y < destination->height; y++)
+	{
+		char* line = buffer + y * bytespan;
+		for(int x = 0; x < destination->width; x++)
+		{
+			line[0] = cvGetReal2D(redchannel, y, x);
+			line[1] = cvGetReal2D(greenchannel, y, x);
+			line[2] = cvGetReal2D(bluechannel, y, x);
+			line += 3;
+		}
+	}
+
+	cvShowImage("HelloVideoEncoding", destination);
+}
+
+void GenerateSample(short* buffer, int sampleCount, int offset)
+{
+	int frequency = 440;
+	int amplitude = 2000;
+	double temp =  3.14 * frequency / sampleCount;
+	for (int i = 0; i < sampleCount; i++)
+		buffer[i] = amplitude * sin(temp * (offset + i));
+}
+
+void closeOpenCV()
+{
+	cvDestroyWindow("HelloVideoEncoding");
+	cvReleaseCapture(&capture);
+}
+
+void closeOpenAL()
+{
+}
+
+void closeFFmpeg()
+{
+	encoder.Finish();
+
+	av_free(frame->data[0]);
+	av_free(frame);
+
+	av_free(readyFrame->data[0]);
+	av_free(readyFrame);
+
+	delete[] sample;
+	sample = NULL;
+}
+
+void close()
+{
+	closeOpenCV();
+	closeOpenAL();
+	closeFFmpeg();
+}
 
 class BaseThread
 {
 public:
-	BaseThread()
-	{ }
-	virtual ~BaseThread()
-	{ }
+	BaseThread() { }
+
+	virtual ~BaseThread() { }
 
 	void operator()()
 	{
@@ -32,7 +173,7 @@ public:
 			for (;;)
 			{
 				// Check if the thread should be interrupted
-				this_thread::interruption_point();
+				boost::this_thread::interruption_point();
 
 				DoStuff();
 			}
@@ -47,262 +188,50 @@ protected:
 	virtual void DoStuff() = 0;
 };
 
-
-//variables
-
-//for FFMpeg
-VideoEncoder encoder;
-int w = W_VIDEO;
-int h = H_VIDEO;
-AVFrame* frame;
-AVFrame* readyFrame;
-int nSampleSize;
-
-char* sample;
-//char* readySample;
-//for OpenAL
-
-//for OpenCV
-CvCapture *capture;
-IplImage  *CVframe;
-IplImage  *destination;
-IplImage* bluechannel;
-IplImage* redchannel;
-IplImage* greenchannel;
-
-int       key;
-int offset = 0;
-
-//set up OpenCV
-void initOpenCV()
-{
-	capture = 0;
-	CVframe = 0;
-	key = 0;
-
-	/* initialize camera */
-	capture = cvCaptureFromCAM( 0 );
-
-	/* always check */
-	if ( !capture ) {
-		fprintf( stderr, "Cannot open initialize webcam!\n" );
-		cin.get();
-	}
-
-	/* create a window for the video */
-	cvNamedWindow( "result", CV_WINDOW_AUTOSIZE );
-
-}
-
-//set up OpenAL
-void initOpenAL()
-{
-
-}
-
-//set up FFmpeg
-void initFFmpeg(string filename, string container, int w, int h, int fps)
-{
-	encoder.SetFps(fps);
-	if (!encoder.InitFile(filename, container))
-	{
-		printf("Cannot init file \n" );
-		cin.get();
-	}
-
-	int bufferImgSize = avpicture_get_size(PIX_FMT_BGR24, w, h);
-
-	// Create frame
-	frame = avcodec_alloc_frame();
-	uint8_t* frameBuffer = (uint8_t*)av_mallocz(bufferImgSize);
-	avpicture_fill((AVPicture*)frame, frameBuffer, PIX_FMT_BGR24, w, h);
-
-	// Create ready frame
-	readyFrame = avcodec_alloc_frame();
-	uint8_t* readyFrameBuffer = (uint8_t*)av_mallocz(bufferImgSize);
-	avpicture_fill((AVPicture*)readyFrame, readyFrameBuffer, PIX_FMT_BGR24, w, h);
-
-	nSampleSize = 2 * 22050.0f / fps; // 1 / fps sec * FORMAT SIZE(S16)
-
-	// Create sample
-	sample = new char[nSampleSize];
-
-	// Create ready sample
-	//readySample = new char[nSampleSize];
-}
-
-//set up libs and comps
-void initAll()
-{
-	initOpenCV();
-	initOpenAL();
-	initFFmpeg(FILE_NAME, CONTAINER, w, h, FPS);
-}
-// Create test video frame
-void CreateFrame(char * buffer, int w, int h, int bytespan)
-{
-	/* get a frame */
-	CVframe = cvQueryFrame( capture );
-
-	/* always check */
-	if( !CVframe ) {
-		printf("No CV frame captured");
-		cin.get();
-	}
-
-	/* display current frame */
-
-	destination = cvCreateImage( cvSize(w , h ), CVframe->depth, CVframe->nChannels );
-
-	//use cvResize to resize source to a destination image
-	cvResize(CVframe, destination);
-
-	redchannel = cvCreateImage(cvGetSize(destination), 8, 1);
-	greenchannel = cvCreateImage(cvGetSize(destination), 8, 1);
-	bluechannel = cvCreateImage(cvGetSize(destination), 8, 1);
-
-	cvSplit(destination, bluechannel, greenchannel, redchannel, NULL);
-	for(int y=0;y<destination->height;y++)
-	{
-		char* line = buffer + y * bytespan;
-		for(int x=0;x<destination->width;x++)
-		{
-
-			line[0] = cvGetReal2D(redchannel, y, x);
-			line[1] = cvGetReal2D(greenchannel, y, x);
-			line[2] = cvGetReal2D(bluechannel, y, x);
-			line += 3;
-		}
-	}
-
-	cvShowImage( "result", destination );
-
-}
-
-// Create sample
-void CreateSample(short * buffer, int sampleCount)
-{
-	int frequency = 440;
-	int amplitude = 2000; // volume
-	float temp =  3.14 * frequency / sampleCount;
-	for (int i = 0; i < sampleCount; i++)
-		buffer[i] = amplitude * sin(temp * (offset + i));
-	//float nu = 3.14 / (22050.0f) * 15.0f;
-	//for (int i = 0; i < sampleCount; i ++)
-	//{
-	//	// Sound :)
-	//	buffer [i] = sin( nu *i  ) * 0x4FFF;
-	//}
-} 
-void closeOpenCV()
-{
-	/* free memory */
-	cvDestroyWindow( "result" );
-	cvReleaseCapture( &capture );
-}
-
-void closeOpenAL()
-{
-}
-
-void closeFFmpeg()
-{
-	encoder.Finish();
-	av_free(frame->data[0]);
-	av_free(frame);
-	delete[] sample;
-	sample = NULL;
-
-	av_free(readyFrame->data[0]);
-	av_free(readyFrame);
-	//delete[] readySample;
-	//readySample = NULL;
-}
-void closeUp()
-{
-	closeOpenCV();
-	closeOpenAL();
-	closeFFmpeg();
-}
-
-
-
-
-
 class ThreadCaptureVideo : public BaseThread
 {
 protected:
 	virtual void DoStuff()
 	{
-		CreateFrame((char *)frame->data[0], w, h, frame->linesize[0]);
+		CaptureFrame((char *)frame->data[0], W_VIDEO, H_VIDEO, frame->linesize[0]);
 
 		// swap buffers
 		AVFrame* swap = frame;
 		frame = readyFrame;
 		readyFrame = swap;
-
-		//CreateSample((short *)sample, nSampleSize/2, offset);
-		//offset += nSampleSize/2;
-		//	if (offset == 7*nSampleSize/2)
-		//	{
-		//		offset = 0;
-		//		cout << "cleared!"<< endl;
-		//	}
 	}
 };
-//class ThreadCaptureAudio : public BaseThread
-//{
-//protected:
-//	virtual void DoStuff()
-//	{
-//		CreateSample((short *)sample, nSampleSize/2);
-//		offset += nSampleSize/2;
-//		//	if (offset == 7*nSampleSize/2)
-//		//	{
-//		//		offset = 0;
-//		//		cout << "cleared!"<< endl;
-//		//	}
-//
-//		// swap buffers
-//		char* swap = sample;
-//		sample = readySample;
-//		readySample = swap;
-//	}
-//};
-
 
 int main()
 {
-	initAll();
-	
-	ThreadCaptureVideo thread_ThreadCaptureVideo_instance;
-	boost::thread ThreadCaptureVideo = boost::thread(thread_ThreadCaptureVideo_instance);
+	init();
 
-	//ThreadCaptureAudio thread_ThreadCaptureAudio_instance;
-	//boost::thread ThreadCaptureAudio = boost::thread(thread_ThreadCaptureAudio_instance);
+	ThreadCaptureVideo threadCaptureVideo;
+	boost::thread ThreadCaptureVideo = boost::thread(threadCaptureVideo);
 
-	double desiredTime = 1000.0f / FPS;
 	boost::timer t;
+	int key = 0;
+	int offset = 0;
+	double desiredTime = 1000.0f / FPS;
 
 	while(key != 'q')
 	{
 		t.restart();
 
-		CreateSample((short *)sample, nSampleSize/2);
-		offset += nSampleSize/2;
+		GenerateSample((short *)sample, nSampleSize / 2, offset);
+		offset += nSampleSize / 2;
 
 		if (!encoder.AddFrame(readyFrame, sample, nSampleSize))
-			printf("Cannot write frame\n");
-		
-		double leftTime = desiredTime - t.elapsed();
+			printf("Cannot write frame!\n");
 
-		if (leftTime>0)
-			this_thread::sleep(boost::posix_time::milliseconds(leftTime));
+		double sleepTime = desiredTime - t.elapsed();
 
-		/* exit if user press 'q' */		
-		key = cvWaitKey( 1 );
+		if (sleepTime>0)
+			boost::this_thread::sleep(boost::posix_time::milliseconds(sleepTime));
+
+		key = cvWaitKey(1);
 	}
-	
-	closeUp();
+
+	close();
 	return 0;
 }
