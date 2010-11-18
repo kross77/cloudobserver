ddf0896e872f8233e897dbdbc57d3f00b1a438da
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-11-18 21:54:28 +0000

    CloudObserverLite http response generator added. The server now shares its current directory on the web.

---
+++ CloudObserverLite/HttpResponseStruct.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.IO;
+
+namespace CloudObserverLite
+{
+    public struct HttpResponseStruct
+    {
+        public int status;
+        public string version;
+        public Hashtable headers;
+        public int BodySize;
+        public byte[] bodyData;
+        public FileStream fileStream;
+    }
+}
---
+++ CloudObserverLite/ResponseState.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudObserverLite
+{
+    public enum ResponseState
+    {
+        OK = 200,
+        BAD_REQUEST = 400,
+        NOT_FOUND = 404
+    }
+}
