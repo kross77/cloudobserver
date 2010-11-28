30688ec9ebfd55b690fb85189ccdfd9d2fc770be
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-11-28 13:52:41 +0000

    Tested multiple cam and mick support.

--- CloudForeverWriter/HelloVideoStreaming/list.cpp
+++ CloudForeverWriter/HelloVideoStreaming/list.cpp
@@ -37,7 +37,8 @@ int CamerasList::SelectFromList()
 {	int i = 0;
     int SelectedIndex;
 	IEnumMoniker *pEnum;
-
+	printf("\nLet us select video device\n");
+	printf("Available Capture Devices are:\n");
 	HRESULT hr;
 	hr = EnumerateDevices(CLSID_VideoInputDeviceCategory, &pEnum);
 	if (SUCCEEDED(hr))
@@ -78,20 +79,36 @@ int CamerasList::SelectFromList()
 
 		}
  		SelectedIndex = 999;
+	if (i <= 0)
+	{
+		cout <<"No devices found. \n " << endl;
+		cout <<"Please restart application."  << endl;
+		cin.get();
+		Sleep(999999);
+
+	}else if(i == 1){
+			cout <<"Default device will be used" << std::endl;
+			SelectedIndex = 0;
+		}else{
 		while(SelectedIndex > i-1 || SelectedIndex < 0)
 		{
-			try{
-			std::cout <<"please input index from 0 to " << i-1 << std::endl;
+			try{		
 			std::string s;
 			std::getline( cin, s, '\n' );
 			SelectedIndex =  boost::lexical_cast<int>(s);
 			}
-			catch(std::exception& e){SelectedIndex = 999;}
-		}
+			catch(std::exception& e){
+				std::cout <<"please input index from 0 to " << i-1 << std::endl;
+				SelectedIndex = 999;
+			}
+		}}
 		pEnum->Release();
 	}else
 	{
 		printf("no Video Devices found. \n") ;
+		cout <<"Please restart application."  << endl;
+		cin.get();
+		Sleep(999999);
 		return 999;
 	}
 	return SelectedIndex;
