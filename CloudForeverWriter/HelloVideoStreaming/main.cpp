22b53aa4eef7fd01b90bd40f364c99d1f240e315
anton.bondarev1978 <anton.bondarev1978@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-11-28 22:19:58 +0000

    added Application class

--- CloudForeverWriter/HelloVideoStreaming/main.cpp
+++ CloudForeverWriter/HelloVideoStreaming/main.cpp
@@ -1,14 +1,8 @@
 #include "stdafx.h"
+#include "Application.h"
 #include <windows.h>
-#include <stdio.h>
 #include <iostream>
-
-//#include <unistd.h>
-//#include <stdlib.h>
-//#include <Pthread.h>
-
 #include <boost/thread.hpp>
-#include <signal.h>
 
 using namespace std;
 
@@ -37,25 +31,6 @@ if(outputUserName == ""){
 	return 0;
 }
 
-static boost::thread captureThread;
-void CloseApplication()
-{
-	captureThread.interrupt();
-	captureThread.join();
-	exit(0);
-}
-
-void WaitingForExitProgram() 
-{
-	string quite;
-	while(quite != "exit")
-	{
-		cout << "Input 'exit' to quite" << endl;
-		cin >> quite;
-		Sleep(250);
-	}
-	CloseApplication();
-}
 
 BOOL WINAPI ConsoleHandler(DWORD CEvent)
 {
@@ -72,36 +47,15 @@ BOOL WINAPI ConsoleHandler(DWORD CEvent)
     case CTRL_SHUTDOWN_EVENT:
         break;
     }
-	CloseApplication();
+	CApplication::CloseApplication();
     return TRUE;
 }
 
-void CaptureLoop(void) 
-{
-	//printf("thread_function started. Arg was %s\n", (char *)arg);
-	// pause for 3 seconds
-	// sleep(3);
-	// exit and  return a message to another thread
-	// that may be waiting for us to finish
-	//pthread_exit ("thread one all done”);
-	while(true)
-	{
-		boost::xtime xt;
-		boost::xtime_get(&xt, boost::TIME_UTC);
-
-
-		xt.sec +=1;
-
-		boost::thread::sleep(xt);
-	}
-}
 
 
 
 int main(int argc, char* argv[])
 {
-	//	pthread_t capture_thread;
-
 	//TODO getopt
 	for(int i = 1; i<argc; i=i+2){
 #if 0
@@ -121,11 +75,12 @@ int main(int argc, char* argv[])
 	}	
 	Sleep(1000);
 
-	captureThread = boost::thread(&CaptureLoop);
+	CApplication::Init();
+//	captureThread = boost::thread(&CaptureLoop);
 
 	SetConsoleCtrlHandler((PHANDLER_ROUTINE)ConsoleHandler,TRUE);
 
-	WaitingForExitProgram();
+	CApplication::MainLoop();
 
 	return 0;
 }
