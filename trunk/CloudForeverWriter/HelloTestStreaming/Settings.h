4eec215d4117ececd0bfae5edc306535df5bc0c6
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-11-28 18:09:24 +0000

    Project for load testing added - generates rainbow like video, crappy way 2 loud audio and is automated for getting random names. (normal names can be set in params, most of sequryty is in place.)

---
+++ trunk/CloudForeverWriter/HelloTestStreaming/Settings.h
@@ -0,0 +1,18 @@
+/*
+  Settings of sample
+ */
+
+#ifndef __SETTINGS_H_
+#define __SETTINGS_H_
+
+
+#define AUDIO_FREQUENCY		440		// hertz
+#define AUDIO_VOLUME		40		// decibels
+
+#define AUDIO_BUFFERSIZE    4410 // size of audio char array
+
+
+
+
+
+#endif // __SETTINGS_H_
