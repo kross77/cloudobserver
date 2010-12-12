4beb2ecec9e34fe4364fa725b52da9a30098dfa3
anton.bondarev1978 <anton.bondarev1978@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-12-12 22:06:25 +0000

    added skelet for sources streaming

---
+++ CloudForeverWriter/CloudClient/Application.cpp
@@ -0,0 +1,43 @@
+#include "StdAfx.h"
+#include <boost/thread.hpp>
+
+#include "Application.h"
+#include "SourceStream.h"
+
+
+using namespace std;
+
+CApplication::CApplication(void)
+{
+}
+
+CApplication::~CApplication(void)
+{
+}
+
+void CApplication::Init()
+{
+	//create camera's list
+}
+
+void CApplication::CloseApplication()
+{
+	SourceStream::stopAll();
+	exit(0);
+}
+
+void CApplication::StartDefault()
+{
+}
+
+void CApplication::MainLoop() 
+{
+	string quite;
+	while(quite != "exit")
+	{
+		cout << "Input 'exit' to quite" << endl;
+		cin >> quite;
+		Sleep(250);
+	}
+	CloseApplication();
+}
