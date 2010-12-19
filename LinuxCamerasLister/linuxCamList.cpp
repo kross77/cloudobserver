eb679689dae346435acf4edb90de47066dcef0d5
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2010-12-19 17:50:47 +0000

    LinuxCamerasLister fixed, cleaned and tested.

--- LinuxCamerasLister/linuxCamList.cpp
+++ LinuxCamerasLister/linuxCamList.cpp
@@ -1,81 +1,70 @@
-//  оригинал в котором есть как смотреть на такие свойства как  depth,  size, Maximum size, Minimum size. http://stackoverflow.com/questions/4290834/how-to-get-a-list-of-video-capture-devices-web-cameras-on-linux-ubuntu-c/4290867#4290867 
-//также стоит обратить внимание на http://www.cs.fsu.edu/~baker/devices/lxr/http/source/xawtv-3.95/console/v4l-info.c#L111 если этот код не будет рабочим ну савсем=)
+// http://stackoverflow.com/questions/4290834/how-to-get-a-list-of-video-capture-devices-web-cameras-on-linux-ubuntu-c/4290867#4290867
+// http://www.cs.fsu.edu/~baker/devices/lxr/http/source/xawtv-3.95/console/v4l-info.c#L111
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <iostream>
+#include <sstream>
+#include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/ioctl.h>
 #include <linux/videodev2.h>
-#include <linux/struct-v4l2.h>
 //#include <boost/lexical_cast.hpp>
 
- // #include "videodev.h"
-  //#include "videodev2.h"
-  
- //#include "struct-dump.h"
- // #include "struct-v4l.h"
- // #include "struct-v4l2.h"
-
 using namespace std;
-int main(){
-    int fd;
-    int i = 0;
-    int SelectedIndex;
-    string devIndexName;
-    	 int tab = 5;
-	printf("\nLet us select video device\n");
-	printf("Available Capture Devices are:\n");
-	
-	devIndexName = "/dev/video";
-	devIndexName += i;
-  
-	
-	
-    while((fd = open(devIndexName.c_str(), O_RDONLY)) != -1){
-    	struct v4l2_capability  capability;
-   printf("general info\n");
-       memset(&capability,0,sizeof(capability));
-     if (-1 == ioctl(fd,VIDIOC_QUERYCAP,&capability)) {}
-         printf("    VIDIOC_QUERYCAP\n");
-         print_struct(stdout,desc_v4l2_capability,&capability,"",tab);
-       printf("\n");
-       // попробуем вывести имя амеры
-       cout << capability.card << endl;  
-    devIndexName = "/dev/video";
-	devIndexName += i;
-	i++;   
-	//close(fd);  // незнаю может тут енто стоит использовать для чистоты кода=)
-    }
-    
-    
-    
-   SelectedIndex = 999;
+
+int main()
+{
+	int fd;
+	int i = 0;
+	int selectedIndex;
+
+	cout << endl << "Let us select video device." << endl << "Available capture devices are:" << endl;
+
+	ostringstream videoDevice;
+	videoDevice << "/dev/video" << i;
+
+	while ((fd = open(videoDevice.str().c_str(), O_RDONLY)) != -1)
+	{
+		struct v4l2_capability capability;
+		memset(&capability, 0, sizeof(capability));
+		if (-1 == ioctl(fd, VIDIOC_QUERYCAP, &capability)) {}
+		cout << i << ". " << capability.card << endl;
+		close(fd);
+
+		i++;
+		videoDevice.str("");
+		videoDevice << "/dev/video" << i;	
+	}
+
+	selectedIndex = 999;
 	if (i <= 0)
 	{
-		cout <<"No devices found. \n " << endl;
-	return 999;
-
-	}else if(i == 1){
-			cout <<"Default device will be used\n" << std::endl;
-			SelectedIndex = 0;
-		}else{
-		while(SelectedIndex > i-1 || SelectedIndex < 0)
+		cout << "No devices found." << endl;
+		return 999;
+	}
+	else if (i == 1)
+	{
+		cout << "Default device will be used.\n" << endl;
+		selectedIndex = 0;
+	}
+	else
+	{
+		while (selectedIndex > i - 1 || selectedIndex < 0)
 		{
-			try{		
-			std::string s;
-	
-	  cin >>	SelectedIndex;
-		//	std::getline( cin, s, '\n' );
-		//	SelectedIndex =  boost::lexical_cast<int>(s);
+			try
+			{
+				cin >> selectedIndex;
+				//string s;
+				//getline(cin, s, '\n');
+				//selectedIndex = boost::lexical_cast<int>(s);
 			}
-			catch(std::exception& e){
-				std::cout <<"please input index from 0 to " << i-1 << std::endl;
-				SelectedIndex = 999;
+			catch(std::exception& e)
+			{
+				cout << "Please input index from 0 to " << i - 1 << endl;
+				selectedIndex = 999;
 			}
-		}}
-		
-   cin.get();
-    return SelectedIndex;
+		}
+	}
+	return selectedIndex;
 }
+
