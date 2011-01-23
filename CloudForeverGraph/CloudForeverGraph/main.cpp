9315e00ca0d9ac0ee6713a6451042a54a2132f91
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-01-23 00:21:24 +0000

    Graph samples added for previewr. no mem leaks

--- CloudForeverGraph/CloudForeverGraph/main.cpp
+++ CloudForeverGraph/CloudForeverGraph/main.cpp
@@ -1,32 +1,17 @@
 // Here we show how to use Graph elements to construct a simple graph.
 #include <iostream>
-#include "GraphElementCameraPreview.h"
-#include "GraphElementExample.h"
-#include "GraphElementCamera.h"
+#include "ElementExampleTest.h"
+#include "CameraPreviewTest.h"
 
 using namespace std;
 
-static void f0 (char* a , int l) { cout << "f0" << endl; delete[] a; }
-static void f1 (char* a, int l) { cout << "f1" << endl; delete[] a; }
-static void f0g (char* a, int l) { cout << "f0 (with get)" << endl; delete[] a; }
-static void f1g (char* a, int l) { cout << "f1 (with get)" << endl; delete[] a; }
-
 int main()
 {
-	GraphElementExample a;
-	a.Init();
-	a.Add (f0) ;
-	a.Add (f1) ;
-	a.StartThread();
+	//ElementExampleTest eet;
+	//eet.main();
 
-	boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+	CameraPreviewTest cpt;
+	cpt.main();
 
-	GraphElementExample::GetResultStructure resultForF0 = a.Get();
-	f0g(resultForF0.ptr, resultForF0.length);
-
-	GraphElementExample::GetResultStructure resultForF1 = a.Get();
-	f1g(resultForF1.ptr, resultForF1.length);
-
-	a.Clean();
-	cin.get();
+	//cin.get();
 }
