8631aadd55e9908c5c7a7df24c013d1f33a1124f
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-02-02 04:10:23 +0000

    New event system - now add creates new threads to evaluate subscribers.

---
+++ CloudForeverGraph/CloudForeverGraph/TestAllElements.h
@@ -0,0 +1,22 @@
+#include "CameraPreviewTest.h"
+#include "GETTest.h"
+
+using namespace std;
+
+class TestAllElements
+{
+public:
+	void main()
+	{
+		cout << "Camera Elements Test" << endl;
+		CameraPreviewTest cpt;
+		cpt.main();
+		cout << "Graph Elements Tutorial Test" << endl;
+		GraphElementTutorialTest gett;
+		gett.main();
+		cout << "All graph elements passed test successfully" << endl;
+		cin.get();
+	}
+
+
+};
