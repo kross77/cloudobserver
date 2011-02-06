637931555b1210075e040134c23b5597da0f2b6c
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-02-06 23:35:09 +0000

    Moving the original Cloud Observer version 0.2 project (demonstrated in the August 2009) from branches to tags. This version is discontinued. v0.2 branch still contains some other related projects, which may be useful in the future.

--- CloudObserver/scripts/host-services.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call %~dp0host-service-storage.bat
-call %~dp0host-service-authentication.bat
-call %~dp0host-service-accounts.bat
-call %~dp0host-service-broadcast.bat
-call %~dp0host-service-ipcameras.bat
--- CloudObserver/settings.bat
+++
@@ -1,8 +0,0 @@
-set MSVisualStudio="C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe"
-set CloudObserverBin=%~dp0bin
-set CloudObserverIP=localhost
-set CloudObserverPort=9000
-set CloudObserverHostController=true
-set CloudObserverController=http://%CloudObserverIP%:%CloudObserverPort%/ControllerService
-set CloudObserverDelay=100
-set CloudObserverTesting=true
