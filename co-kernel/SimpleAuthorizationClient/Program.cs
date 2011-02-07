1a387afc27062953b4a338ae58bbe333ed988be5
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-02-07 16:29:51 +0000

    Fixing my svn commit bug

---
+++ co-kernel/SimpleAuthorizationClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ServiceModel;
+using CloudObserverAuthorizationServiceLibrary;
+
+namespace SimpleAuthorizationClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Port (1024-65535): ");
+                int servicePort = Int32.Parse(Console.ReadLine());
+                if ((servicePort < 1024) || (servicePort > 65535)) throw new Exception("Port out of range.");
+                string serviceAddress = @"http://localhost:" + servicePort + "/CloudObserverAuthorizationService";
+                Console.WriteLine("Connecting to Cloud Observer Authorization service at " + serviceAddress);
+                ICloudObserverAuthorizationService authorizationServiceClient = new ChannelFactory<ICloudObserverAuthorizationService>(new BasicHttpBinding(), serviceAddress).CreateChannel();
+                Console.WriteLine("Connection succeed. Preparing login procedure...");
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+                Console.WriteLine(authorizationServiceClient.UserLogin(email, password) ? "Login succeed." : "Login failed. Invalid email or password.");
+                Console.Write("Press any key to exit...");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
