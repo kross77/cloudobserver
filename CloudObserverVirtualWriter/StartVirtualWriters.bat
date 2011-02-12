6364597d645e1c018d43d881ac3d2327aa5a5b8b
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-02-12 05:09:02 +0000

    Renaming HelloTestStreaming project of CloudForeverWriter solution into CloudObserverVirtualWriter project and moving it into the separate directory in the trunk.

---
+++ CloudObserverVirtualWriter/StartVirtualWriters.bat
@@ -0,0 +1,20 @@
+start ./Debug/CloudObserverVirtualWriter.exe -server http://127.0.0.1:4773/
+ping -n 2 127.0.0.1 >nul
+start ./Debug/CloudObserverVirtualWriter.exe -server http://127.0.0.1:4773/
+ping -n 2 127.0.0.1 >nul
+start ./Debug/CloudObserverVirtualWriter.exe -server http://127.0.0.1:4773/
+ping -n 2 127.0.0.1 >nul
+start ./Debug/CloudObserverVirtualWriter.exe -server http://127.0.0.1:4773/
+ping -n 2 127.0.0.1 >nul
+start ./Debug/CloudObserverVirtualWriter.exe -server http://127.0.0.1:4773/
+ping -n 2 127.0.0.1 >nul
+start ./Debug/CloudObserverVirtualWriter.exe -server http://127.0.0.1:4773/
+ping -n 2 127.0.0.1 >nul
+start ./Debug/CloudObserverVirtualWriter.exe -server http://127.0.0.1:4773/
+ping -n 2 127.0.0.1 >nul
+start ./Debug/CloudObserverVirtualWriter.exe -server http://127.0.0.1:4773/
+ping -n 2 127.0.0.1 >nul
+start ./Debug/CloudObserverVirtualWriter.exe -server http://127.0.0.1:4773/
+ping -n 2 127.0.0.1 >nul
+start ./Debug/CloudObserverVirtualWriter.exe -server http://127.0.0.1:4773/
+ping -n 2 127.0.0.1 >nul
