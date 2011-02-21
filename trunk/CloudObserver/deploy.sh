937e90cbb0a06f225b0c75329e13ffade54489bd
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-02-21 17:30:08 +0000

    Modifying CloudObserver deploy script for Unix-like systems to include version number in the resulting build.

--- trunk/CloudObserver/deploy.sh
+++ trunk/CloudObserver/deploy.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
+revision=`svnversion`
+version=0.4.0.$revision
+target=cloudobserver-$version
+
 executable=./CloudObserver/bin/Release/CloudObserver.exe
 if [ ! -f $executable ]; then
 	echo "No executable found. You should first build the Cloud Observer project in release configuration with Mono."
 	exit 1
 fi
 
-if [ -f cloudobserver.tar.gz ]; then
-	rm cloudobserver.tar.gz
+if [ -f $target.tar.gz ]; then
+	rm $target.tar.gz
 fi
 
-mkdir cloudobserver
-cp $executable cloudobserver/
-cp ./CloudObserver/start.sh cloudobserver/
-cp ./CloudObserver/stop.sh cloudobserver/
-cp -r ./CloudObserverInstaller/htdocs cloudobserver/
-tar -czf cloudobserver.tar.gz --exclude-vcs cloudobserver
-rm -fr cloudobserver
+mkdir $target
+cp $executable $target
+cp ./CloudObserver/start.sh $target
+cp ./CloudObserver/stop.sh $target
+chmod a+x $target/start.sh $target/stop.sh
+cp -r ./CloudObserverInstaller/htdocs $target
+tar -czf $target.tar.gz --exclude-vcs $target
+rm -fr $target
 
 exit 0
