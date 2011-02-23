46715a31bb6bc4cb8ecb92a053a47f1790f3e2f0
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-02-23 00:22:56 +0000

    Importing CloudForeverMac project - the Mac OS X port of the CloudObserverWriter project (former CloudForeverWriter). Compiles under x86_64 architecture, but doesn't work. Video input device selection is also disabled for now as it requires i386 architecture.

---
+++ branches/v0.4/CloudForeverMac/list.h
@@ -0,0 +1,18 @@
+/*#include <iostream>
+#include <cstring>
+#include <boost/lexical_cast.hpp>
+#include <QuickTime/QuickTime.h>
+
+namespace CamerasListNamespace
+{
+	
+	class CamerasList
+	{
+	public:
+		CamerasList(void);
+		~CamerasList(void);
+		
+		int SelectFromList();
+	};
+}
+*/
