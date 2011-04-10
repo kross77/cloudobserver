1ebdda455296312326e664ea50e64274d8ec762d
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-04-10 20:22:36 +0000

    @CloudGroup: initial commit CloudGroup project is targeted to test and develop ways of connecting plugins via shared libraries to main project during execution time.

---
+++ CloudGroup/src/Group.cpp
@@ -0,0 +1,8 @@
+#include "unit.h"
+
+using namespace std;
+
+int main()
+{
+	cin.get();
+}
---
+++ CloudGroup/src/unit.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <map>
+
+// Boost
+#include <boost/asio.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/random.hpp>
