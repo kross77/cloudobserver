ee35c42f7fd29f26e6efdb5a538fa43c84c9ee8b
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-04-17 20:31:23 +0000

    @Boost.Extention.Tutorial: Tutorial from boost docs seemed not informative enough so a premake project was created out of it (tested on windows).

---
+++ branches/v0.4/Boost.Extention.Tutorial/src/factories_main.cpp
@@ -0,0 +1,84 @@
+/*
+ * Boost.Extension / hello world example main
+ *
+ * (C) Copyright Jeremy Pack 2008
+ * Distributed under the Boost Software License, Version 1.0. (See             
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * See http://www.boost.org/ for latest version.
+ */
+
+#include <iostream>
+#include <map>
+
+#include <boost/extension/factory.hpp>
+#include <boost/extension/shared_library.hpp>
+#include <boost/extension/type_map.hpp>
+#include <boost/function.hpp>
+#include <boost/scoped_ptr.hpp>
+
+#include "animal.h"
+using namespace std;
+
+#ifdef WIN
+string extention = ".dll";
+#elif defined LIN
+string extention = ".so";
+#elif defined MAC
+string extention = ".dylib"; // as wall .bundle can be used.
+#else
+#error "unknown platform";
+#endif
+
+int main() {
+  using namespace boost::extensions;
+
+  // In the Jamfile, shared libraries are set to have the same
+  // prefix and extension, even on different operating systems.
+  // This is for convenience in writing cross-platform code, but
+  // is not required. All shared libraries are set to start with
+  // "lib" and end with "extension".
+  string library_path = "Factories_lib";
+library_path +=extention;
+
+  // Create shared_library object with the relative or absolute
+  // path to the shared library.
+  shared_library lib(library_path);
+
+  // Attempt to open the shared library.
+  if (!lib.open()) {
+    cerr << "Library failed to open: " << library_path << endl;
+    return 1;
+  }
+
+  // Use the shared_library::call to automatically call an
+  // Extension-specific function in the shared library,
+  // which takes a type_map.
+  type_map types;
+  if (!lib.call(types)) {
+    cerr << "Function not found!" << endl;
+    return 1;
+  }
+
+  // Retrieve a map of all animal factories taking an int and indexed
+  // by a string from the type_map.
+  map<string, factory<animal, int> >& factories(types.get());
+  if (factories.empty()) {
+    cerr << "Animals not found!" << endl;
+    return 1;
+  }
+
+  // Create each animal.
+  int age = 1;
+  for (map<string, factory<animal, int> >::iterator it
+         = factories.begin();
+       it != factories.end(); ++it) {
+    ++age;
+    cout << "Creating an animal using factory: " << it->first << endl;
+    boost::scoped_ptr<animal> current_animal(it->second.create(age));
+    cout << "Created an animal: " << current_animal->get_name()
+              << " Age: " << current_animal->get_age() << endl;
+  }
+  cin.get();
+}
