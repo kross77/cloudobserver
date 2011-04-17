585f067c38ab35af55bc97a22b801f95805a2f98
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-04-17 21:36:01 +0000

    @CloudClient: Adding the separate video encoder class (extracted from the encoder class).

---
+++ trunk/CloudClient/src/video_encoder.h
@@ -0,0 +1,44 @@
+#ifndef VIDEO_ENCODER_H
+#define VIDEO_ENCODER_H
+
+// The encode buffer size.
+#define ENCODE_BUFFER_SIZE (1024 * 1024 * 10)
+
+// FFmpeg
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+}
+
+#include "multiplexer.h"
+
+class video_encoder
+{
+public:
+	video_encoder(int bitrate, int framerate, int width, int height);
+	~video_encoder();
+	void connect(multiplexer* multiplexer_block);
+	void disconnect();
+	void send(AVFrame* frame);
+
+	class internal_exception: public std::exception { };
+private:
+	int bitrate;
+	int framerate;
+	int width;
+	int height;
+
+	AVStream* stream;
+
+	bool raw_data;
+	bool need_convert;
+
+	uint8_t* encode_buffer;
+	struct SwsContext* convert_context;
+
+	multiplexer* multiplexer_block;
+};
+
+#endif // VIDEO_ENCODER_H
