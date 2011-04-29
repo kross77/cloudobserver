ec00ac9701c3096afb2e73743444709452279170
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-04-29 00:05:36 +0000

    @Boost.Extension.Tutorial: All projects now compile under Linux GCC. (due to modified Boost.Extension library) Some ignores added.

---
+++ Boost.Extension.Tutorial/premake-build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+chmod a+x premake4
+./premake4 clean
+./premake4 --os=linux --BoostIncludesPath="./libs/" --platform=x32 codeblocks
+./premake4 --os=linux --BoostIncludesPath="./libs/" --platform=x32 gmake
+
