4887d4f3f27f5837401f60b1e5ddd934bd8a34d2
Oleg.Jakushkin <Oleg.Jakushkin@gmail.com>
2011-04-29 21:55:27 +0000

    @Boost.Extension.Tutorial: excluded unused codeblocks project generation line inside premake-build.sh

--- Boost.Extension.Tutorial/premake-build.sh
+++ Boost.Extension.Tutorial/premake-build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 chmod a+x premake4
 ./premake4 clean
-./premake4 --os=linux --BoostIncludesPath="./libs/" --platform=x32 codeblocks
 ./premake4 --os=linux --BoostIncludesPath="./libs/" --platform=x32 gmake
 
