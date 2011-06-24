1d2eb701882c449e835f8f2d24d56d444cc06f00
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-06-24 15:50:33 +0000

    @ASL.Tutorial: ASL "begin" GUI deprecated, replaced with pure ASL Adame and Eve vindow demo. Based on ASL future alert.cpp. Build files cleaned up a bit.

--- ASL.AdameAndEve.Tutorial/src/CloudAdamAndEve/CloudAdamAndEve.cpp
+++ ASL.AdameAndEve.Tutorial/src/CloudAdamAndEve/CloudAdamAndEve.cpp
@@ -4,94 +4,151 @@
     or a copy at http://stlab.adobe.com/licenses.html)
 */
 /****************************************************************************************************/
+#include <boost/thread/tss.hpp>
+#include <adobe/future/modal_dialog_interface.hpp>
 
-#include "express_viewer.hpp"
-#include "report_exception.hpp"
-#include <adobe/future/widgets/headers/display.hpp>
-#include <adobe/future/windows_cast.hpp>
-#include <adobe/future/widgets/headers/widget_utils.hpp>
+#include <adobe/any_regular.hpp>
+#include <adobe/name.hpp>
+#include <adobe/name.hpp>
 
-#include <adobe/xstring.hpp>
+#include <utility>
+#include <cassert>
+#include <string>
+#include <sstream>
 
-#include <boost/filesystem/convenience.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/array.hpp>
 
-#include <adobe/future/behavior.hpp>
 
-#define WIN32_LEAN_AND_MEAN
+//using namespace adobe;
 
-/*************************************************************************************************/
-#if defined(BOOST_MSVC) && defined(BOOST_THREAD_USE_LIB)
-namespace boost {
-	void tss_cleanup_implemented()
-{ }
-}
-#endif
-/******************************************* namespace adobe ******************************************************/
-namespace adobe {
+/****************************************************************************************************/
 
-bool os_initialize( application_t* app )
+inline bool always_break(adobe::name_t, const adobe::any_regular_t&)
+    { return true; }
+
+/****************************************************************************************************/
+
+void append_button_to_layout(std::stringstream& layout,
+                             const char* name,
+                             std::size_t index,
+                             bool is_default,
+                             bool is_cancel)
 {
+    if (name == 0)
+        return;
 
-    //
-    // Now we need to get a directory from the command line name.
-    //
-    boost::filesystem::path directory( "/", boost::filesystem::native );
+    // NOTE (fbrereto) : For the sake of indexing the name of the action must be "bN"
 
-    //
-    // Tell the application...
-    //
-    app->set_resource_directory( directory.branch_path() );
+    layout <<
+        "button(name: '" << name << "', action: @b" << static_cast<unsigned long>(index)
+        ;
 
-    return true;
+    // A button cannot be both the default and the cancel button
+    if (is_default)
+        layout << ", default: true";
+    else if (is_cancel)
+        layout << ", cancel: true";
+
+    layout <<
+        ");\n"
+        ;
 }
 
-void os_mainloop(adobe::application_t& app)
+void alert(const char*                    message_text,
+                                   const char*                    window_name,
+                                   const char*                    button_0_name,
+                                   const char*                    button_1_name,
+                                   const char*                    button_2_name,
+                                   const char*                    checkbox_name,
+                                   const boost::filesystem::path& icon_path,
+                                   std::size_t                    default_button_index,
+                                   std::size_t                    cancel_button_index)
 {
-    //
-    // This is just the standard Win32 message pump.
-    //
-    MSG msg;
+    if ((button_0_name || button_1_name || button_2_name) == false)
+        return;// std::make_pair<const char*, bool>(0, false);
 
-    while ( GetMessage( &msg, 0, 0, 0 ) )
+    std::stringstream       sheet;
+    std::stringstream       layout;
+    boost::filesystem::path icon_directory_path;
+
+    // The sheet for the alert dialog
+    sheet <<
+        "sheet alert_sheet\n"
+        "{\n"
+        "interface:\n"
+        "   checkbox_value: false;\n"
+        "output:\n"
+        "   result <== { checkbox_value: checkbox_value };\n"
+        "}\n"
+    ;
+
+    // Start by filling out the header for the layout of the alert
+    layout <<
+        "layout alert_layout\n"
+        "{\n"
+        "   view dialog(name: '" << window_name << "', placement: place_row, spacing: 10)\n"
+        "   {\n";
+
+    if (icon_path != boost::filesystem::path())
     {
-        try
-        {
-            TranslateMessage( &msg );
-            DispatchMessage( &msg );
-        }
-        catch ( ... )
-        {
-            adobe::report_exception();
-        }
+        icon_directory_path = icon_path.branch_path();
 
-        adobe::general_deferred_proc_queue()();
+        layout <<
+            "       label_t(image: '" << icon_path.leaf() << "')\n"
+        ;
     }
-}
 
-void os_end_mainloop()
-{
-    //
-    // This is just the standard Win32 quit message.
-    //
-    ::PostQuitMessage(0);
-}
+    layout <<
+        "       column()\n"
+        "       {\n"
+        "           static_text(name:'" << message_text << "', horizontal: align_fill, characters: 25);\n"
+        ;
 
+    // add the checkbox if we have a name for one
+    if (checkbox_name)
+    {
+        layout <<
+            "checkbox(name: '" << checkbox_name << "', bind: @checkbox_value);\n"
+            ;
+    }
+
+    // add the button set
+    layout <<
+        "row(horizontal: align_right)\n"
+        "{\n"
+        ;
+
+    // add the buttons in *reverse* order, so the first is rightmost
+    append_button_to_layout(layout, button_2_name, 2, default_button_index == 2, cancel_button_index == 2);
+    append_button_to_layout(layout, button_1_name, 1, default_button_index == 1, cancel_button_index == 1);
+    append_button_to_layout(layout, button_0_name, 0, default_button_index == 0, cancel_button_index == 0);
+
+    // close out the rest of the layout
+    layout << 
+        "           }\n" // row
+        "       }\n"     // column
+        "   }\n"         // dialog
+        "}\n"            // layout
+        ;
+
+    // finally set up the params for the modal dialog interface call
+    adobe::dialog_result_t result(adobe::handle_dialog(adobe::dictionary_t(),
+                                                       adobe::dictionary_t(),
+                                                       adobe::dictionary_t(),
+                                                       adobe::dialog_display_s,
+                                                       layout,
+                                                       sheet,
+                                                       &always_break,
+                                                       icon_directory_path));
+
+    bool is_checked(result.command_m[adobe::static_name_t("checkbox_value")].cast<bool>());
 }
-/******************************************* namespace adobe ******************************************************/
 
 int  main(  )
 {
-    try
-    {
-        adobe::application_t* theApp = adobe::application_t::getInstance();
-
-        if( theApp ) theApp->run();
-    }
-    catch( ... )
-    {
-        adobe::report_exception();
-    }
-
+	alert(  "There was an error.","Alert","OK",0,0,0,boost::filesystem::path(),0,1);
+	std::cin.get();
     return 0;
 } 
 
