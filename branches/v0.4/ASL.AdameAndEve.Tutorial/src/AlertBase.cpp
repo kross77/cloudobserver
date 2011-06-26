86fed0e623951303c0fd27ecfef95905d1030d91
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-06-26 19:20:52 +0000

    @ASL.Tutorial: Single threaded basic alert tutorial includes minimized.

--- branches/v0.4/ASL.AdameAndEve.Tutorial/src/AlertBase.cpp
+++ branches/v0.4/ASL.AdameAndEve.Tutorial/src/AlertBase.cpp
@@ -1,17 +1,6 @@
-#include <boost/thread.hpp>
 #include <boost/thread/tss.hpp>
 
-#include <adobe/adam_evaluate.hpp>
-#include <adobe/adam_parser.hpp>
 #include <adobe/adam.hpp>
-#include <adobe/algorithm/for_each.hpp>
-#include <adobe/any_regular.hpp>
-#include <adobe/array.hpp>
-#include <adobe/dictionary.hpp>
-#include <adobe/implementation/expression_parser.hpp>
-#include <adobe/iomanip_asl_cel.hpp>
-#include <adobe/name.hpp>
-#include <adobe/virtual_machine.hpp>
 #include <adobe/future/widgets/headers/alert.hpp>
 
 int main()
