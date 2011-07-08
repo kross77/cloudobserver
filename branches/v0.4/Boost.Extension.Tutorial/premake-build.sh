a2bbdc5d788a316aca4fae868c568665860d264a
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-08 20:58:56 +0000

    @Boost.Extension.Tutorial: Removed projects folder. Boost.Extension.Tutorial is compilable on Windows, Linux and Mac OS X set of examples ported from Boost.Extension svn. We have studies them and included most valuable parts into CloudGroup.

--- branches/v0.4/Boost.Extension.Tutorial/premake-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-chmod a+x premake4
-./premake4 clean
-./premake4 --os=linux --BoostIncludesPath="./libs/" --platform=x32 gmake
-
--- branches/v0.4/Boost.Extension.Tutorial/premake4-build-windows.bat
+++
@@ -1,2 +0,0 @@
-premake4 clean
-cmd /k premake4 --os=windows --BoostIncludesPath="./libs/" --platform=x32 vs2008 
