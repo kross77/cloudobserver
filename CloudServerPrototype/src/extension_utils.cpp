52ea5e8bc9996088d0c574c5ad40cef8a31a0e57
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-08 20:48:04 +0000

    @CloudServerPrototype: Minor Linux compatibility fixes.

--- CloudServerPrototype/src/extension_utils.cpp
+++ CloudServerPrototype/src/extension_utils.cpp
@@ -3,22 +3,22 @@ extension_utils::extension_utils()
 {
 	// Shared library suffixes
 	#ifdef WIN
-		std::string current_extention = ".dll";
+		current_extention = ".dll";
 	#elif defined LIN
-		std::string current_extention = ".so";
+		current_extention = ".so";
 	#elif defined MAC
-		std::string current_extention = ".dylib"; // as wall .bundle can be used.
+		current_extention = ".dylib"; // as wall .bundle can be used.
 	#else
 	#error "unknown platform";
 	#endif
 
 	// Shared library prefixes
 	#ifdef WIN
-		std::string current_prefix = "";
+		current_prefix = "";
 	#elif defined LIN
-		std::string current_prefix = "lib";
+		current_prefix = "lib";
 	#elif defined MAC
-		std::string current_prefix = "lib"; // as wall .bundle can be used.
+		current_prefix = "lib"; // as wall .bundle can be used.
 	#else
 	#error "unknown platform";
 	#endif
