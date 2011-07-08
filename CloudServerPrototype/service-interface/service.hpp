9a589e46a84aa016a98e725666288db5cb6ee441
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-08 03:34:27 +0000

    @CloudServerPrototype: Comments and some TODO memos added, file service currently disabled.

--- CloudServerPrototype/service-interface/service.hpp
+++ CloudServerPrototype/service-interface/service.hpp
@@ -1,6 +1,6 @@
 #ifndef SERVICE_HPP
 #define SERVICE_HPP
-
+// STD
 #include <iostream>
 #include <string>
 
@@ -17,28 +17,37 @@
 //CF
 #include <http.h>
 
+//TODO: Remove using namespace
 using namespace boost::asio::ip;
 using namespace boost::filesystem;
 
+//Main service interface.
 class BOOST_EXTENSION_EXPORT_DECL service
 {
 public:
-	service(path default_path) : file_service_default_path(file_service_default_path) {std::cout << "\nCreated a Service";}
+
+	//We link service to filesystem directory where it should operate (read from / write to)
+	service(path default_path) : service_default_path(default_path) {std::cout << "\nCreated a Service";}
 
 	virtual ~service(void){std::cout << "\nDestroyed a Service";}
-	
+
+	//We pass user request to service for future inspection
 	virtual bool service_call(http_request request, boost::shared_ptr<tcp::socket> socket){return 1;} // bool for service execution status
+
+	//Here we provide default service model with some frequently used methods
+
+
 	
 	path get_default_path(void) {
-		return file_service_default_path;
+		return service_default_path;
 	}
 	
 	void set_default_path(path default_path) {
-		file_service_default_path = default_path;
+		service_default_path = default_path;
 	}
 
 protected:
-	path file_service_default_path;
+	path service_default_path;
 
 };
 
