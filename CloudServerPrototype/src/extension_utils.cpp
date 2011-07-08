b3dc59c87840830467f1b5fb964b147279134655
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-08 16:48:21 +0000

    @CloudServerPrototype: extension_utils.h API file cleaned up and divided into .cpp and _inl.h. server class - minor code style fix.

--- CloudServerPrototype/src/extension_utils.cpp
+++ CloudServerPrototype/src/extension_utils.cpp
@@ -12,7 +12,7 @@ extension_utils::extension_utils()
 	#error "unknown platform";
 	#endif
 
-		// Shared library prefixes
+	// Shared library prefixes
 	#ifdef WIN
 		std::string current_prefix = "";
 	#elif defined LIN
@@ -33,3 +33,13 @@ std::string extension_utils::add_prefix_and_suffix(std::string name)
 	return library_name;
 }
 
+void extension_utils::try_open_lib( boost::extensions::shared_library &lib, std::string &path )
+{
+	if (!lib.open()) {
+		std::cout << "Library failed to open: " << path << std::endl;
+	}
+	else
+	{
+		std::cout << "Library " << path << " opened." << std::endl;
+	}
+}
