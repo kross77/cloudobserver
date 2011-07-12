fb90652d27834b1d8512ae70c2359c802a986cf7
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-12 11:42:14 +0000

    @CloudServerPrototype: Added service_description structure, updated API

--- CloudServerPrototype/src/server_utils.cpp
+++ CloudServerPrototype/src/server_utils.cpp
@@ -4,15 +4,15 @@ server_utils::server_utils()
 {
 	util = new extension_utils();
 	print = new printer();
-	server_root_path = boost::filesystem::current_path();
+	this->description.server_root_path = boost::filesystem::current_path();
 }
 
 boost::shared_ptr<service> server_utils::create_service(std::string library_name, std::string class_name_inside_lib)
 {
-	return util->give_me_class<service, boost::filesystem::path>(library_name, class_name_inside_lib, server_root_path);
+	return util->give_me_class<service, boost::filesystem::path>(library_name, class_name_inside_lib, this->description.server_root_path);
 }
 
-std::map<boost::shared_ptr<service>, server_utils::service_description> server_utils::parse_config( boost::property_tree::ptree config )
+std::map<boost::shared_ptr<service>, server_utils::service_description> server_utils::parse_config_services( boost::property_tree::ptree config )
 {
 	BOOST_FOREACH(boost::property_tree::ptree::value_type &v,
 		config.get_child("config.services", server_utils::empty_class<boost::property_tree::ptree>()))
@@ -41,7 +41,7 @@ std::map<boost::shared_ptr<service>, server_utils::service_description> server_u
 				std::cout << "Class name: " << second_data << std::endl;
 			}
 
-			if(first_data == "root_directory")
+			if(first_data == "root_file_system_directory")
 			{
 				std::string second_data = vs.second.data();
 				std::cout << "Root directory path: " << second_data << std::endl;
@@ -67,10 +67,33 @@ std::map<boost::shared_ptr<service>, server_utils::service_description> server_u
 				{
 					std::cout << "Supported url extension: " << vp.second.data() <<  std::endl;
 				}
+
+				if(first_data == "root_service_web_path")
+				{
+					std::string second_data = vs.second.data();
+					std::cout << "Service web root directory path: " << second_data << std::endl;
+				}
 			}
 		}
 	}
-	std::cout << std::endl;
 	std::map<boost::shared_ptr<service>, server_utils::service_description> m;
 	return m;
+}
+
+server_utils::server_description server_utils::parse_config( boost::property_tree::ptree config )
+{
+	server_utils::server_description server_descr;
+	std::cout << std::endl << "Server description: " << std::endl;
+
+	server_descr.server_root_path = config.get("config.server_root_path", boost::filesystem::current_path());
+	std::cout << "Server root path: " << server_descr.server_root_path << std::endl;
+
+	server_descr.port = config.get("config.port", 12345);
+	std::cout << "Server port: " << server_descr.port << std::endl;
+
+	std::cout << std::endl << "Server services: ";
+	server_descr.service_map = server_utils::parse_config_services( config );
+	std::cout << std::endl;
+
+	return server_descr;
 }
