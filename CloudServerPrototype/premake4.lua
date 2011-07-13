f9dc56960761748940fa382663f1f0c1662a1dc8
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-13 22:37:13 +0000

    @CloudServerPrototype: The Cloud Service was successfully ported into the C++ from the C# code of the Cloud Server v0.4 (the current version in the trunk). The service is capable of processing the '?action=read' and '?action=write' requests providing the same FLV retransmission functionality as the original .NET server. The service does not provide the 'users.json' file so far. The service is linked into the separate shared library and can be loaded into the server with the following configuration file:
    <?xml version="1.0" encoding="utf-8"?>
    <config>
    	<port>4773</port>
    	<services>
    		<service>
    			<name>Cloud Service</name>
    			<class_name>cloud_service</class_name>
    			<library_name>CloudService</library_name>
    			<properties>
    				<arguments>
    					<action>write</action>
    					<action>read</action>
    				</arguments>
    			</properties>
    		</service>
    	</services>
    </config>

--- CloudServerPrototype/premake4.lua
+++ CloudServerPrototype/premake4.lua
@@ -470,3 +470,25 @@ solution "CloudServerPrototype"
 		configuration "Release"
 			defines { "NDEBUG" }
 			flags { "OptimizeSpeed" , "Unicode"}
+
+	project "CloudService"
+		kind "SharedLib"
+		language "C++"
+		location ( "projects/" .. os.get() .. "-" .. action )
+		
+		cloud.project.init()
+		cloud.project.useBoost()
+
+		links { "cf-http" }
+		includedirs { "3rdparty/cf-http" }
+		
+		files { "src/default-services/cloud_service/**"}
+		files { "service-interface/**" }
+
+		configuration "Debug"
+			defines { "DEBUG" }
+			flags { "Symbols" , "Unicode"}
+		
+		configuration "Release"
+			defines { "NDEBUG" }
+			flags { "OptimizeSpeed" , "Unicode"}
