88dd6b5e8f81939fbaf9b399bb8bac44bdf51bb8
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-14 22:10:04 +0000

    @CloudServer: The useless constructor and destructor of the base service class were removed.

--- CloudServer/service-interface/service.hpp
+++ CloudServer/service-interface/service.hpp
@@ -24,15 +24,6 @@
 class service
 {
 public:
-
-	//We link service to filesystem directory where it should operate (read from / write to)
-	service(boost::property_tree::ptree config)
-	{
-		std::cout << "\nCreated a Service";
-	}
-
-	~service(void){std::cout << "\nDestroyed a Service" << std::endl;}
-
 	//We pass user request to service for future inspection
 	virtual bool service_call(http_request request, boost::shared_ptr<boost::asio::ip::tcp::socket> socket){return 1;} // bool for service execution status
 };
