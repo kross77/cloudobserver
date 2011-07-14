f04189b8e6691bfec237067dc328d9b1db188059
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-14 01:04:45 +0000

    @CloudServerPrototype: The 'htdocs' folder was copied from the Cloud Server v0.4 (the current version in the trunk). The premake script was modified to copy it into the build directories. The configuration file was modified to set the root directory of the File Service to the 'htdocs' folder. Running the default configuration of the server now produces almost exactly the same result as the Cloud Server v0.4.

--- branches/v0.4/CloudServerPrototype/premake4.lua
+++ branches/v0.4/CloudServerPrototype/premake4.lua
@@ -110,6 +110,37 @@ end
 
 cloud = { }
 
+function os.copydir(src_dir, dst_dir, filter, single_dst_dir)
+	if not os.isdir(src_dir) then error(src_dir .. " is not an existing directory!") end
+	filter = filter or "**"
+	src_dir = src_dir .. "/"
+	print('copy "' .. src_dir .. filter .. '" to "' .. dst_dir .. '".')
+	dst_dir = dst_dir .. "/"
+	local dir = path.rebase(".",path.getabsolute("."), src_dir) -- root dir, relative from src_dir
+ 
+	os.chdir( src_dir ) -- change current directory to src_dir
+		local matches = os.matchfiles(filter)
+	os.chdir( dir ) -- change current directory back to root
+ 
+	local counter = 0
+	for k, v in ipairs(matches) do
+		local target = iif(single_dst_dir, path.getname(v), v)
+		--make sure, that directory exists or os.copyfile() fails
+		os.mkdir( path.getdirectory(dst_dir .. target))
+		if os.copyfile( src_dir .. v, dst_dir .. target) then
+			counter = counter + 1
+		end
+	end
+ 
+	if counter == #matches then
+		print( counter .. " files copied.")
+		return true
+	else
+		print( "Error: " .. counter .. "/" .. #matches .. " files copied.")
+		return nil
+	end
+end
+
 function cloud.addLibDir(path)
 	--
 	if  path then
@@ -404,6 +435,18 @@ function cloud.project.createDumpsFolder()
 	os.mkdir("projects/" .. os.get() .. "-" .. action .. "/bin/release/dumps")
 end
 
+function cloud.project.copyHtdocsFolder()
+	if not os.isdir("projects/" .. os.get() .. "-" .. action .. "/bin/debug/htdocs" ) then
+		os.mkdir("projects/" .. os.get() .. "-" .. action .. "/bin/debug/htdocs")
+	end
+	if not os.isdir("projects/" .. os.get() .. "-" .. action .."/bin/release/htdocs" ) then
+		os.mkdir("projects/" .. os.get() .. "-" .. action .. "/bin/release/htdocs")
+	end
+	os.copydir("assets/htdocs",  "projects/" .. os.get() .. "-" .. action .. "/bin/debug/htdocs")
+	os.copydir("assets/htdocs",  "projects/" .. os.get() .. "-" .. action .. "/bin/release/htdocs")
+	os.copydir("assets/htdocs",  "projects/" .. os.get() .. "-" .. action .. "/htdocs")
+end
+
 solution "CloudServerPrototype"
 	location ( "projects/".. os.get() .. "-" ..  action )
 	configurations { "Debug", "Release" }
@@ -432,6 +475,7 @@ solution "CloudServerPrototype"
 		
 		cloud.project.copyConfig()
 		cloud.project.createDumpsFolder()
+		cloud.project.copyHtdocsFolder()
 		
 		links { "cf-http" }
 		includedirs { "3rdparty/cf-http"}
