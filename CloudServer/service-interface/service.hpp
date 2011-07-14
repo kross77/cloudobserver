12a1558450616450c073ee0343918e2c36ac1665
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-14 23:04:35 +0000

    @CloudServer: The base service class source code was cleaned up. Also a missing include directive for the <boost/filesystem.hpp> was added into the 'server_utils.h' (it was recursively included from the 'service.hpp' earlier).

--- CloudServer/service-interface/service.hpp
+++ CloudServer/service-interface/service.hpp
@@ -1,31 +1,17 @@
 #ifndef SERVICE_HPP
 #define SERVICE_HPP
-// STD
-#include <iostream>
-#include <string>
 
-////Boost-Extension
-//#include <typeinfo>
-//#include <boost/extension/extension.hpp>
-
-//Boost-FileSystem
-#include <boost/filesystem.hpp>
-
-//Boost-PropertyTree
+// Boost
+#include <boost/asio.hpp>
 #include <boost/property_tree/ptree.hpp>
 
-//Boost-ASIO
-#include <boost/asio.hpp>
-
-//CF
+// Cloud Forever
 #include <http.h>
 
-//Main service interface.
 class service
 {
 public:
-	//We pass user request to service for future inspection
-	virtual void service_call(http_request request, boost::shared_ptr<boost::asio::ip::tcp::socket> socket) = 0;
+	virtual void service_call(http_request, boost::shared_ptr<boost::asio::ip::tcp::socket>) = 0;
 };
 
-#endif
+#endif // SERVICE_HPP
