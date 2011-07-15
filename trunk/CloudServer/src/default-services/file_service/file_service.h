9cff71f190a163045953924249bad75be9293c32
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-15 00:47:02 +0000

    @CloudServer: A lot of the File Service source code refactoring was performed. The service class declaration was extracted into the 'file_service.h' header file. The rest of the code was cleaned up and structured. Some useless code fragments were deleted.

---
+++ trunk/CloudServer/src/default-services/file_service/file_service.h
@@ -0,0 +1,35 @@
+#ifndef FILE_SERVICE_H
+#define FILE_SERVICE_H
+
+#include <fstream>
+#include <map>
+#include <string>
+
+// Boost
+#include <boost/asio.hpp>
+#include <boost/date_time.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+// Boost Extension
+#include <boost/extension/extension.hpp>
+#include <boost/extension/factory.hpp>
+#include <boost/extension/type_map.hpp>
+
+#include "../../../service-interface/service.hpp"
+
+class file_service: public service
+{
+public:
+	file_service();
+	virtual void service_call(boost::shared_ptr<boost::asio::ip::tcp::socket> socket, boost::shared_ptr<http_request> request, boost::shared_ptr<http_response> response);
+	virtual void apply_config(boost::property_tree::ptree config);
+private:
+	boost::filesystem::path root_path;
+	bool show_directory_contents;
+	boost::posix_time::time_duration expiration_period;
+};
+
+#endif // FILE_SERVICE_H
