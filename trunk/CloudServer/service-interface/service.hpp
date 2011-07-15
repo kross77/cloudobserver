a4dac2c05842e884ec8c7c05a1e8d4b7ad4c7bbc
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-15 22:13:25 +0000

    @CloudServer: Modified service interface API with start and stop methods. Changed service_map from unsearchable via std container to container with name as key, updated sources to such new service_map.

--- trunk/CloudServer/service-interface/service.hpp
+++ trunk/CloudServer/service-interface/service.hpp
@@ -16,7 +16,12 @@ public:
 	virtual void service_call(boost::shared_ptr<boost::asio::ip::tcp::socket>, boost::shared_ptr<http_request>, boost::shared_ptr<http_response>) = 0;
 	virtual void apply_config(boost::property_tree::ptree) { throw not_configurable_exception(); };
 
+	virtual void start() { throw not_startable_exception(); };
+	virtual void stop() { throw not_stopable_exception(); };
+
 	class not_configurable_exception: public std::exception { };
+	class not_startable_exception: public std::exception { };
+	class not_stopable_exception: public std::exception { };
 };
 
 #endif // SERVICE_HPP
