f237a73e962c6d7e03a3c7bf38eb47cec1f5ad66
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-16 20:33:03 +0000

    @CloudServer: fixed `close socket` aka `logo.png` bug with automatic thread destruction.

--- CloudServer/src/server.h
+++ CloudServer/src/server.h
@@ -24,7 +24,7 @@ public:
 private:
 	void request_response_loop(boost::shared_ptr<boost::asio::ip::tcp::socket> socket); //Each request received by server will be sent to a new thread and processed by request_response_loop
 	void user_info(boost::asio::ip::tcp::socket &socket);
-	std::map<boost::thread::id, boost::thread*> threads_pool;
+	std::set<boost::thread::id> threads_pool;
 	boost::thread *acceptor_thread;
 };
 
