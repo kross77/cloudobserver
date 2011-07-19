e33389eb7deffc3efb860aa2a1aded562753682b
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-19 14:04:07 +0000

    @CloudLoader: Turned spaces into tabs where was needed, fixed boost building into shared libs, updated infrastructure.

--- CloudLoader/boost_net_setup.sh
+++ CloudLoader/boost_net_setup.sh
@@ -8,6 +8,34 @@ BOOST_ROOT_DIR=boost_libraries
 BOOST_INSTALL_SUBDIR=install-dir
 BOOST_COMPILE_SUBDIR=build-dir
 
+KEEP_OLD="true"
+
+echo API: boost_net_setup.sh BOOST_ROOT_DIR BOOST_INSTALL_SUBDIR KEEP_OLD BOOST_VERSION BOOST_NAME BOOST_DISTRO_SITE
+
+if [ "$1" != "" ]; then
+	BOOST_ROOT_DIR="$1"
+fi
+
+if [ "$2" != "" ]; then
+	BOOST_INSTALL_SUBDIR="$2"
+fi
+
+if [ "$3" != "" ]; then
+	KEEP_OLD="$3"
+fi
+
+if [ "$4" != "" ]; then
+	BOOST_VERSION="$4"
+fi
+
+if [ "$5" != "" ]; then
+	BOOST_NAME="$5"
+fi
+
+if [ "$6" != "" ]; then
+	BOOST_DISTRO_SITE="$6"
+fi
+
 #-L protects against redirects
 CURL_CMD=curl\ -L 
 
@@ -18,18 +46,43 @@ CURL_CMD=curl\ -L
 #
 echo_run ()
 {
-    echo "$@"
-    "$@"
-    r=$?
-    if test $r -ne 0 ; then
-        exit $r
-    fi
+	echo "$@"
+	"$@"
+	r=$?
+	if test $r -ne 0 ; then
+		exit $r
+	fi
 }
 
-test_path()
+extract_and_clean()
 {
-    hash $1 1>/dev/null 2>/dev/null
+	echo_run tar -xzf $BOOST_DISTRO_NAME
+	echo_run rm -rf $BOOST_ROOT_DIR
+	echo_run mv $BOOST_NAME $BOOST_ROOT_DIR
+	
+	if [ -d $BOOST_INSTALL_SUBDIR ]; then
+		echo_run rm -rf $BOOST_INSTALL_SUBDIR
+		echo_run mkdir $BOOST_INSTALL_SUBDIR
+	fi
+	if [ ! -d $BOOST_INSTALL_SUBDIR ]; then
+		echo_run mkdir $BOOST_INSTALL_SUBDIR
+	fi
+	if [ -d $BOOST_COMPILE_SUBDIR ]; then
+		echo_run rm -rf $BOOST_COMPILE_SUBDIR
+		echo_run mkdir $BOOST_COMPILE_SUBDIR
+	fi
+	if [ ! -d $BOOST_COMPILE_SUBDIR ]; then
+		echo_run mkdir $BOOST_COMPILE_SUBDIR
+	fi
+	
+	cd $BOOST_ROOT_DIR
+
+	echo_run ./bootstrap.sh
+
+	echo_run ./bjam -j4 link=shared --builddir=./$BOOST_COMPILE_SUBDIR install --without-mpi --without-chrono --without-exception --without-graph --without-graph_parallel --without-iostreams --without-wave --without-python --prefix=./$BOOST_INSTALL_SUBDIR
+	# or call # echo_run ./bjam -j4 link=static threading=single,multi --builddir=./$BOOST_COMPILE_SUBDIR install --with-mpi --without-python  --prefix=./$BOOST_INSTALL_SUBDIR
 }
+
 
 WD=`pwd`
 MACHINE=`uname`
@@ -37,48 +90,23 @@ HERE=`dirname $0`
 
 cd $HERE
 
-if [ "$1" != "" ]; then
-    BOOST_DISTRO_SITE="$1"
-fi
-
 if [ ! -e $BOOST_DISTRO_NAME ]; then
-    # get boost
-    echo_run ${CURL_CMD} http://$BOOST_DISTRO_SITE/project/boost/boost/$BOOST_VERSION/$BOOST_DISTRO_NAME -o $BOOST_DISTRO_NAME
+	# get boost
+	echo_run ${CURL_CMD} http://$BOOST_DISTRO_SITE/project/boost/boost/$BOOST_VERSION/$BOOST_DISTRO_NAME -o $BOOST_DISTRO_NAME
 fi
   
 if [ ! -d $BOOST_ROOT_DIR ]; then
-    echo_run mkdir $BOOST_ROOT_DIR
+	echo_run mkdir $BOOST_ROOT_DIR
 fi
 
 # move the boost distro into place
-echo_run tar -xzf $BOOST_DISTRO_NAME
-echo_run rm -rf $BOOST_ROOT_DIR
-echo_run mv $BOOST_NAME $BOOST_ROOT_DIR
-
-cd $BOOST_ROOT_DIR
-
-if [ -d $BOOST_INSTALL_SUBDIR ]; then
-    echo_run rm -rf $BOOST_INSTALL_SUBDIR
-    echo_run mkdir $BOOST_INSTALL_SUBDIR
+if [ ! -d $BOOST_ROOT_DIR/$BOOST_INSTALL_SUBDIR/lib ]; then
+	extract_and_clean
+else
+	if [ ! KEEP_OLD=="true" ]; then
+		extract_and_clean
+	fi
 fi
-
-if [ ! -d $BOOST_INSTALL_SUBDIR ]; then
-    echo_run mkdir $BOOST_INSTALL_SUBDIR
-fi
-
-
-if [ -d $BOOST_COMPILE_SUBDIR ]; then
-    echo_run rm -rf $BOOST_COMPILE_SUBDIR
-    echo_run mkdir $BOOST_COMPILE_SUBDIR
-fi
-if [ ! -d $BOOST_COMPILE_SUBDIR ]; then
-    echo_run mkdir $BOOST_COMPILE_SUBDIR
-fi
-
-echo_run ./bootstrap.sh
-
-echo_run ./bjam -j4 link=static --builddir=./$BOOST_COMPILE_SUBDIR install --without-mpi --without-chrono --without-exception --without-graph --without-graph_parallel --without-iostreams --without-wave --without-python --prefix=./$BOOST_INSTALL_SUBDIR
-# or call # echo_run ./bjam -j4 link=static threading=single,multi --builddir=./$BOOST_COMPILE_SUBDIR install --with-mpi --without-python  --prefix=./$BOOST_INSTALL_SUBDIR
 
 echo Done!
 
