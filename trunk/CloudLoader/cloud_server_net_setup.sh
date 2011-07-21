76bfead9546897f64eae1238e496b82ec4e66b56
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-21 21:36:33 +0000

    @CloudLoader: Added to server net setup openssl dependency.

--- trunk/CloudLoader/cloud_server_net_setup.sh
+++ trunk/CloudLoader/cloud_server_net_setup.sh
@@ -11,10 +11,15 @@ CLOUD_LOCAL_BUILDING_FILE_NAME=build.sh
 OS_NAME=linux # could be `macosx`
 
 SVN_OPT=export # could be `co` or `checkout`
-FORCE_SVN="true"
+KEEP_OLD=no
+
 BOOST_ROOT_DIR=boost_libraries
 BOOST_INSTALL_SUBDIR=install-dir
 BOOST_SETUP_FILE_NAME=boost_net_setup.sh
+
+OPENSSL_ROOT_DIR=openssl_libraries
+OPENSSL_INSTALL_SUBDIR=install-dir
+OPENSSL_SETUP_FILE_NAME=openssl_net_setup.sh
 
 PREMAKE_SETUP_FILE_NAME=premake_net_setup.sh
 
@@ -23,14 +28,14 @@ echo
 echo --CF autobuild team welcomes you!----------------------------------------------
 echo -------------------------------------------------------------------------------
 echo --This build API:--------------------------------------------------------------
-echo ./cloud_server_net_setup.sh FORCE_SVN CLOUD_INSTALL_SUBDIR BOOST_INSTALL_SUBDIR BOOST_ROOT_DIR BOOST_SETUP_FILE_NAME PREMAKE_SETUP_FILE_NAME
+echo ./cloud_server_net_setup.sh KEEP_OLD CLOUD_INSTALL_SUBDIR BOOST_INSTALL_SUBDIR BOOST_ROOT_DIR BOOST_SETUP_FILE_NAME PREMAKE_SETUP_FILE_NAME
 echo -------------------------------------------------------------------------------
 echo Please be patient. Go get yourself a coup of coffee - auto build process can take a long time.
 echo -------------------------------------------------------------------------------
 echo
 
 if [ "$1" != "" ]; then
-	FORCE_SVN="$1"
+	KEEP_OLD="$1"
 fi
 
 if [ "$2" != "" ]; then
@@ -82,12 +87,14 @@ if [ ! -d $CLOUD_INSTALL_SUBDIR ]; then
 	echo_run mkdir $CLOUD_INSTALL_SUBDIR
 fi
 
-if [ FORCE_SVN=="true" ]; then
-	if [ ! -d $CLOUD_COMPONENT_NAME ]; then
-		echo_run mkdir $CLOUD_COMPONENT_NAME
+if [ "$KEEP_OLD" = "yes" ]; then
+	if [ -d $CLOUD_COMPONENT_NAME ]; then
+		echo We will keep old Server code files
+	else
+		echo_run ${SVN_CMD} https://$CLOUD_DISTRO_SITE/svn/trunk/$CLOUD_COMPONENT_NAME/ $CLOUD_COMPONENT_NAME
 	fi
-	echo_run ${SVN_CMD} --force https://$CLOUD_DISTRO_SITE/svn/trunk/$CLOUD_COMPONENT_NAME/ $CLOUD_COMPONENT_NAME
 else
+	echo_run rm -rf $CLOUD_COMPONENT_NAME/
 	echo_run ${SVN_CMD} https://$CLOUD_DISTRO_SITE/svn/trunk/$CLOUD_COMPONENT_NAME/ $CLOUD_COMPONENT_NAME
 fi
 
@@ -97,6 +104,13 @@ if [ ! -e $BOOST_SETUP_FILE_NAME ]; then
 fi
 
 echo_run ./$BOOST_SETUP_FILE_NAME $BOOST_ROOT_DIR $BOOST_INSTALL_SUBDIR
+
+if [ ! -e $OPENSSL_SETUP_FILE_NAME ]; then
+	echo_run wget http://cloudobserver.googlecode.com/svn/trunk/CloudLoader/$OPENSSL_SETUP_FILE_NAME
+	echo_run chmod u+x $OPENSSL_SETUP_FILE_NAME
+fi
+
+echo_run ./$OPENSSL_SETUP_FILE_NAME $OPENSSL_ROOT_DIR $OPENSSL_INSTALL_SUBDIR
 
 if [ ! -e $PREMAKE_SETUP_FILE_NAME ]; then
 	echo_run wget http://cloudobserver.googlecode.com/svn/trunk/CloudLoader/$PREMAKE_SETUP_FILE_NAME
@@ -108,8 +122,8 @@ echo_run ./$PREMAKE_SETUP_FILE_NAME
 cd $CLOUD_COMPONENT_NAME
 
 if [ ! -e $CLOUD_LOCAL_BUILDING_FILE_NAME ]; then
-	echo ../premake-4.3/bin/release/premake4 --os=$OS_NAME --BoostLibsPath=../$BOOST_ROOT_DIR/$BOOST_INSTALL_SUBDIR/lib --BoostIncludesPath=../$BOOST_ROOT_DIR/$BOOST_INSTALL_SUBDIR/include  --platform=x32 gmake > $CLOUD_LOCAL_BUILDING_FILE_NAME
-	chmod u+x ./$CLOUD_LOCAL_BUILDING_FILE_NAME
+	echo_run echo ../premake-4.3/bin/release/premake4 --os=$OS_NAME --BoostLibsPath=../$BOOST_ROOT_DIR/$BOOST_INSTALL_SUBDIR/lib  --OpenSSLLibsPath=../$OPENSSL_ROOT_DIR/$OPENSSL_INSTALL_SUBDIR/lib  --BoostIncludesPath=../$BOOST_ROOT_DIR/$BOOST_INSTALL_SUBDIR/include --OpenSSLIncludesPath=../$OPENSSL_ROOT_DIR/$OPENSSL_INSTALL_SUBDIR/include  --platform=x32 gmake > $CLOUD_LOCAL_BUILDING_FILE_NAME
+	echo_run chmod u+x ./$CLOUD_LOCAL_BUILDING_FILE_NAME
 fi
 
 echo_run ./$CLOUD_LOCAL_BUILDING_FILE_NAME
@@ -125,7 +139,6 @@ fi
 
 echo_run cp -r ./bin/release/* ../../../$CLOUD_INSTALL_SUBDIR/
 cd ../../../
-echo_run rm -rf 
 echo -------------------------------------------------------------------------------
 echo Done!
 
