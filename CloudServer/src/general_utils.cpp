d44f8c70204e2aec08ed6c1964b5b58dc0458a69
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-07-26 16:17:35 +0000

    @CloudServer: Added url encoding and decoding to general_utils (RFC1630, RFC1738, RFC2396)

--- CloudServer/src/general_utils.cpp
+++ CloudServer/src/general_utils.cpp
@@ -47,3 +47,70 @@ std::string general_utils::get_sha256( std::string value )
 	}
 	return std::string(data);
 }
+
+std::string general_utils::url_encode( std::string sSrc )
+{
+	const char DEC2HEX[16 + 1] = "0123456789ABCDEF";
+	const unsigned char * pSrc = (const unsigned char *)sSrc.c_str();
+	const int SRC_LEN = sSrc.length();
+	unsigned char * const pStart = new unsigned char[SRC_LEN * 3];
+	unsigned char * pEnd = pStart;
+	const unsigned char * const SRC_END = pSrc + SRC_LEN;
+
+	for (; pSrc < SRC_END; ++pSrc)
+	{
+		if (SAFE[*pSrc]) 
+			*pEnd++ = *pSrc;
+		else
+		{
+			// escape this char
+			*pEnd++ = '%';
+			*pEnd++ = DEC2HEX[*pSrc >> 4];
+			*pEnd++ = DEC2HEX[*pSrc & 0x0F];
+		}
+	}
+
+	std::string sResult((char *)pStart, (char *)pEnd);
+	delete [] pStart;
+	return sResult;
+}
+
+std::string general_utils::url_decode( std::string sSrc )
+{
+	// Note from RFC1630:  "Sequences which start with a percent sign
+	// but are not followed by two hexadecimal characters (0-9, A-F) are reserved
+	// for future extension"
+
+	const unsigned char * pSrc = (const unsigned char *)sSrc.c_str();
+	const int SRC_LEN = sSrc.length();
+	const unsigned char * const SRC_END = pSrc + SRC_LEN;
+	const unsigned char * const SRC_LAST_DEC = SRC_END - 2;   // last decodable '%' 
+
+	char * const pStart = new char[SRC_LEN];
+	char * pEnd = pStart;
+
+	while (pSrc < SRC_LAST_DEC)
+	{
+		if (*pSrc == '%')
+		{
+			char dec1, dec2;
+			if (-1 != (dec1 = HEX2DEC[*(pSrc + 1)])
+				&& -1 != (dec2 = HEX2DEC[*(pSrc + 2)]))
+			{
+				*pEnd++ = (dec1 << 4) + dec2;
+				pSrc += 3;
+				continue;
+			}
+		}
+
+		*pEnd++ = *pSrc++;
+	}
+
+	// the last 2- chars
+	while (pSrc < SRC_END)
+		*pEnd++ = *pSrc++;
+
+	std::string sResult(pStart, pEnd);
+	delete [] pStart;
+	return sResult;
+}
