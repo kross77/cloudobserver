acbf6837db49c1cb24097a06e12b901ef56d532f
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-08-08 20:26:37 +0000

    @CloudServer: Distributed premake4.lua file into lots of smaller files.

---
+++ trunk/CloudServer/lua/cloud/cloud.project.lua
@@ -0,0 +1,150 @@
+cloud.project = {}
+
+function cloud.project.init()
+	--
+	cloud.addLibDir(  _OPTIONS["libsPath"] )
+	cloud.addIncludeDir( _OPTIONS["includesPath"] )
+	libdirs {
+		"./"
+		}
+	if os.get() == "windows" then
+		defines { "WIN" }
+	end
+	
+	if os.get() == "linux" then
+		defines { "LIN" }
+		libdirs {
+		"/opt/local/lib",
+		"/usr/lib",
+		"/usr/local/lib"
+		}
+		
+		includedirs {
+		"/opt/local/include",
+		"/usr/include",
+		"/usr/local/include"
+		}	 
+	end
+	
+	if os.get() == "macosx" then
+		defines { "MAC" }
+		links {
+		"Carbon.framework"
+		}
+		
+		libdirs {
+		"/usr/lib",
+		"/usr/local/lib"
+		}
+		
+		includedirs {
+		"/usr/include",
+		"/usr/local/include"
+		}
+	end 
+end
+
+function cloud.project.useBoost()
+	--
+	cloud.addIncludeDir( _OPTIONS["BoostIncludesPath"] )
+	cloud.addLibDir( _OPTIONS["BoostLibsPath"])
+		includedirs {"3rdparty/header-only"}
+		defines{ "BOOST_ASIO_DISABLE_IOCP" }
+	if os.get() == "windows" then
+		defines { "WIN" } 
+		cloud.win.addLibFromProgrammFiles("Boost-1.46.1")
+	end	
+	if os.get() == "linux" then
+		defines { "LIN" }
+		links {
+		"boost_regex",
+		"boost_system",
+		"boost_thread",
+		"boost_filesystem",
+		"boost_program_options",
+		"dl"
+		}
+	end
+	if os.get() == "macosx" then
+		defines { "MAC" }
+		links {
+		"boost_regex",
+		"boost_system",
+		"boost_thread",
+		"boost_filesystem",
+		"boost_program_options",
+		"dl"
+		}	
+	end 
+end
+
+function cloud.project.useopenSSL()
+	cloud.addLibDir(  _OPTIONS["OpenSSLLibsPath"] )
+	cloud.addIncludeDir(  _OPTIONS["OpenSSLIncludesPath"] )
+		
+	if os.get() == "windows" then
+		defines { "WIN" }
+		links {
+		"libeay32",
+		"ssleay32"
+		}
+		cloud.win.addLibFromProgrammFiles("openssl-1.0.0d")
+		if  _OPTIONS["CopySharedLibraries"] then
+			cloud.win.copyDLL("openssl-1.0.0d/lib", "libeay32.dll")
+			cloud.win.copyDLL("openssl-1.0.0d/lib", "ssleay32.dll")
+		end
+	end
+	
+	if os.get() == "linux" then
+		defines { "LIN" }
+		links {
+		"ssl",
+		"crypto"
+		}
+	end
+	
+	if os.get() == "macosx" then
+		defines { "MAC" }
+		links {
+		"ssl",
+		"crypto"
+		}
+	end 
+	
+end
+
+function cloud.project.copyConfig()
+	if not os.isdir("projects/" .. os.get() .. "-" .. action .. "/bin/debug" ) then
+		os.mkdir("projects/" .. os.get() .. "-" .. action .. "/bin/debug")
+	end
+	if not os.isdir("projects/" .. os.get() .. "-" .. action .."/bin/release" ) then
+		os.mkdir("projects/" .. os.get() .. "-" .. action .. "/bin/release")
+	end
+	os.copyfile("assets/config.xml" , "projects/" .. os.get() .. "-" .. action .. "/config.xml" )
+	os.copyfile("assets/config.xml" , "projects/" .. os.get() .. "-" .. action .. "/bin/debug/config.xml" )
+	os.copyfile("assets/config.xml" , "projects/" .. os.get() .. "-" .. action .. "/bin/release/config.xml" )
+end
+
+function cloud.project.createDumpsFolder()
+	if not os.isdir("projects/" .. os.get() .. "-" .. action .. "/bin/debug" ) then
+		os.mkdir("projects/" .. os.get() .. "-" .. action .. "/bin/debug")
+	end
+	if not os.isdir("projects/" .. os.get() .. "-" .. action .."/bin/release" ) then
+		os.mkdir("projects/" .. os.get() .. "-" .. action .. "/bin/release")
+	end
+	os.mkdir("projects/" .. os.get() .. "-" .. action .. "/dumps")
+	os.mkdir("projects/" .. os.get() .. "-" .. action .. "/bin/debug/dumps")
+	os.mkdir("projects/" .. os.get() .. "-" .. action .. "/bin/release/dumps")
+end
+
+function cloud.project.copyHtdocsFolder()
+	if not os.isdir("projects/" .. os.get() .. "-" .. action .. "/bin/debug/htdocs" ) then
+		os.mkdir("projects/" .. os.get() .. "-" .. action .. "/bin/debug/htdocs")
+	end
+	if not os.isdir("projects/" .. os.get() .. "-" .. action .."/bin/release/htdocs" ) then
+		os.mkdir("projects/" .. os.get() .. "-" .. action .. "/bin/release/htdocs")
+	end
+	os.copydir("assets/htdocs",  "projects/" .. os.get() .. "-" .. action .. "/bin/debug/htdocs")
+	os.copydir("assets/htdocs",  "projects/" .. os.get() .. "-" .. action .. "/bin/release/htdocs")
+	os.copydir("assets/htdocs",  "projects/" .. os.get() .. "-" .. action .. "/htdocs")
+end
---
+++ trunk/CloudServer/lua/project.CloudServer.lua
@@ -0,0 +1,39 @@
+project "CloudServer"
+	kind "ConsoleApp"
+	language "C++"
+	location ( "projects/" .. os.get() .. "-" .. action )
+	
+	cloud.project.init()
+	cloud.project.useBoost()
+	cloud.project.useopenSSL()
+	
+	cloud.project.copyConfig()
+	cloud.project.createDumpsFolder()
+	cloud.project.copyHtdocsFolder()
+	
+	
+	includedirs { "service-interface/" }
+	
+	links { "sqlite" }
+	includedirs { "3rdparty/sqlite"}	
+	
+	links { "cf-http" }
+	includedirs { "3rdparty/cf-http"}
+	
+	links { "cf-util" }
+	includedirs { "src/cf-util"}
+
+	links { "cf-server" }
+	includedirs { "src/cf-server"}
+	
+	files { "src/main.cpp" }
+
+	excludes { "src/default-services/**" , "src/cf-util/**", "src/cf-server/**" }
+
+	configuration "Debug"
+		defines { "DEBUG" }
+		flags { "Symbols" , "Unicode"}
+	
+	configuration "Release"
+		defines { "NDEBUG" }
+		flags { "OptimizeSpeed" , "Unicode"}
