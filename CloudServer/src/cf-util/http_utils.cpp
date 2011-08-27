19f587232f62e12f7eae0167d57399fe612a0870
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-08-27 02:17:42 +0000

    @CloudServer: Added utf-8 to utf-16 and back std::wstring into std::string converters.

--- CloudServer/src/cf-util/http_utils.cpp
+++ CloudServer/src/cf-util/http_utils.cpp
@@ -224,3 +224,124 @@ std::string http_utils::url_decode( std::string sSrc )
 	delete [] pStart;
 	return sResult;
 }
+
+std::wstring http_utils::utf8_to_utf16(const std::string& utf8)
+{
+	std::vector<unsigned long> unicode;
+	size_t i = 0;
+	while (i < utf8.size())
+	{
+		unsigned long uni;
+		size_t todo;
+		bool error = false;
+		unsigned char ch = utf8[i++];
+		if (ch <= 0x7F)
+		{
+			uni = ch;
+			todo = 0;
+		}
+		else if (ch <= 0xBF)
+		{
+			throw std::logic_error("not a UTF-8 string");
+		}
+		else if (ch <= 0xDF)
+		{
+			uni = ch&0x1F;
+			todo = 1;
+		}
+		else if (ch <= 0xEF)
+		{
+			uni = ch&0x0F;
+			todo = 2;
+		}
+		else if (ch <= 0xF7)
+		{
+			uni = ch&0x07;
+			todo = 3;
+		}
+		else
+		{
+			throw std::logic_error("not a UTF-8 string");
+		}
+		for (size_t j = 0; j < todo; ++j)
+		{
+			if (i == utf8.size())
+				throw std::logic_error("not a UTF-8 string");
+			unsigned char ch = utf8[i++];
+			if (ch < 0x80 || ch > 0xBF)
+				throw std::logic_error("not a UTF-8 string");
+			uni <<= 6;
+			uni += ch & 0x3F;
+		}
+		if (uni >= 0xD800 && uni <= 0xDFFF)
+			throw std::logic_error("not a UTF-8 string");
+		if (uni > 0x10FFFF)
+			throw std::logic_error("not a UTF-8 string");
+		unicode.push_back(uni);
+	}
+	std::wstring utf16;
+	for (size_t i = 0; i < unicode.size(); ++i)
+	{
+		unsigned long uni = unicode[i];
+		if (uni <= 0xFFFF)
+		{
+			utf16 += (wchar_t)uni;
+		}
+		else
+		{
+			uni -= 0x10000;
+			utf16 += (wchar_t)((uni >> 10) + 0xD800);
+			utf16 += (wchar_t)((uni & 0x3FF) + 0xDC00);
+		}
+	}
+	return utf16;
+}
+
+std::string http_utils::utf16_to_utf8(const std::wstring & in)
+{
+	std::string out;
+	unsigned int codepoint;
+	bool completecode = false;
+	for (std::wstring::const_iterator p = in.begin();  p != in.end();  ++p)
+	{
+		if (*p >= 0xd800 && *p <= 0xdbff)
+		{
+			codepoint = ((*p - 0xd800) << 10) + 0x10000;
+			completecode = false;
+		}
+		else if (!completecode && *p >= 0xdc00 && *p <= 0xdfff)
+		{
+			codepoint |= *p - 0xdc00;
+			completecode = true;
+		}
+		else
+		{
+			codepoint = *p;
+			completecode = true;
+		}
+		if (completecode)
+		{
+			if (codepoint <= 0x7f)
+				out.push_back(codepoint);
+			else if (codepoint <= 0x7ff)
+			{
+				out.push_back(0xc0 | ((codepoint >> 6) & 0x1f));
+				out.push_back(0x80 | (codepoint & 0x3f));
+			}
+			else if (codepoint <= 0xffff)
+			{
+				out.push_back(0xe0 | ((codepoint >> 12) & 0x0f));
+				out.push_back(0x80 | ((codepoint >> 6) & 0x3f));
+				out.push_back(0x80 | (codepoint & 0x3f));
+			}
+			else
+			{
+				out.push_back(0xf0 | ((codepoint >> 18) & 0x07));
+				out.push_back(0x80 | ((codepoint >> 12) & 0x3f));
+				out.push_back(0x80 | ((codepoint >> 6) & 0x3f));
+				out.push_back(0x80 | (codepoint & 0x3f));
+			}
+		}
+	}
+	return out;
+}
