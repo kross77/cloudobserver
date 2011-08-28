cdc372c9fb786233d2464d042dbcd08fdae11ff8
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-08-28 00:29:27 +0000

    @Branches.0.5.ConcurrentMaps: Added test_map_wraper_pooled with "boost::asio + boost::tread_group" based threads pool.
    Notes: 
     - default group size set to 4;
     - thread pool based solution has given me 400% more performance than parsing all requests at once=)

--- ConcurrentMaps/src/main.cpp
+++ ConcurrentMaps/src/main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include "general_map_data_structure.h"
 #include "test_map_wraper.h"
+#include "test_map_wraper_with_asio_based_thread_pool.h"
 
 int main()
 {
 	int threads_n = 1000;
 	int tests = 5;
+
 	std::cout << "Number of required tests: " << tests << std::endl << "Number of threads in each test: " << threads_n << std::endl << "Wait for it..." << std::endl;
 	for(int i = 1; i < tests+1; ++i)
 	{
-		std::cout << "starting test number: " << i << std::endl;
+		std::cout << "number of tasks == number of created threads in this test (number: " << i << ")" << std::endl;
 		test_map_wraper<general_map_data_structure> GeneralMapTest(threads_n);
 		GeneralMapTest.start_tests();
 		GeneralMapTest.tests_end();
 		std::cout << "test complete" << std::endl <<  std::endl; 
 	}
+
+	for(int i = 1; i < tests+1; ++i)
+	{
+		std::cout << "Thread pool used - number of tasks != number of created threads in this test (number: " << i << ")" << std::endl;
+		test_map_wraper_pooled<general_map_data_structure> GeneralMapTest(threads_n);
+		GeneralMapTest.start_tests();
+		GeneralMapTest.tests_end();
+		std::cout << std::endl << "test complete" << std::endl <<  std::endl; 
+	}
+
+	std::cout << std::endl  << "All tests complete" << std::endl;
 	std::cin.get();
 	return 0;
 }
