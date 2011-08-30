f57368369124fa7738d4650538d384eef25f738b
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-08-30 21:14:25 +0000

    @CloudServer: Some minor refactoring of the 'timer' class was performed.

--- trunk/CloudServer/src/cf-util/timer.h
+++ trunk/CloudServer/src/cf-util/timer.h
@@ -3,22 +3,22 @@
 
 #include <boost/date_time.hpp>
 
-class timer 
-{ 
-public: 
-	timer() : start_time_(boost::posix_time::microsec_clock::local_time()) {} 
-	
-	void restart() 
-	{
-		start_time_ = boost::posix_time::microsec_clock::local_time();
-	} 
+class timer
+{
+public:
+	timer() : start_time(boost::posix_time::microsec_clock::local_time()) { }
 
-	boost::posix_time::time_duration elapsed() const 
+	void restart()
 	{
-		return boost::posix_time::microsec_clock::local_time() - start_time_;
-	} 
-private: 
-	boost::posix_time::ptime start_time_; 
+		start_time = boost::posix_time::microsec_clock::local_time();
+	}
+
+	boost::posix_time::time_duration elapsed() const
+	{
+		return boost::posix_time::microsec_clock::local_time() - start_time;
+	}
+private:
+	boost::posix_time::ptime start_time;
 };
 
 #endif // TIMER_H
