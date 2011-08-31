9191436b7792b7387d8d553503240aca39e807c1
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-08-31 19:27:10 +0000

    @Branches.0.5.ConcurrentMaps: Minimized output.

--- branches/v0.5/ConcurrentMaps/src/main.cpp
+++ branches/v0.5/ConcurrentMaps/src/main.cpp
@@ -11,41 +11,34 @@ int main()
 
 	std::cout << std::endl << "----" << std::endl << "Int tests" << std::endl << "----" << std::endl;
 
+	std::cout << std::endl << "testing general_map_data_structure:" << std::endl;
 	for(int i = 1; i < tests+1; ++i)
 	{
-		std::cout << "Thread pool used - number of tasks != number of created threads in this test (number: " << i << ")" << std::endl;
 		test_map_wraper_pooled<general_map_data_structure<int, int>, int, int > GeneralMapTest(tasks_n);
 		GeneralMapTest.start_tests("int");
 		GeneralMapTest.tests_end();
-		std::cout << std::endl << "test complete" << std::endl <<  std::endl; 
 	}
-
+	std::cout << std::endl << "testing general_map_data_structure_shared_locks:" << std::endl;
 	for(int i = 1; i < tests+1; ++i)
 	{
-		std::cout << "Thread pool used - number of tasks != number of created threads in this test (number: " << i << ")" << std::endl;
 		test_map_wraper_pooled<general_map_data_structure_shared_locks<int, int>, int, int > GeneralMapTest(tasks_n);
 		GeneralMapTest.start_tests("int");
 		GeneralMapTest.tests_end();
-		std::cout << std::endl << "test complete" << std::endl <<  std::endl; 
 	}
 	std::cout << std::endl << "----" << std::endl << "String tests" << std::endl << "----" << std::endl;
-
+	std::cout << std::endl << "testing general_map_data_structure:" << std::endl;
 	for(int i = 1; i < tests+1; ++i)
 	{
-		std::cout << "Thread pool used - number of tasks != number of created threads in this test (number: " << i << ")" << std::endl;
 		test_map_wraper_pooled<general_map_data_structure<std::string, std::string>, std::string, std::string > GeneralMapTest(tasks_n);
 		GeneralMapTest.start_tests("string");
 		GeneralMapTest.tests_end();
-		std::cout << std::endl << "test complete" << std::endl <<  std::endl; 
 	}
-
+	std::cout << std::endl << "testing general_map_data_structure_shared_locks:" << std::endl;
 	for(int i = 1; i < tests+1; ++i)
 	{
-		std::cout << "Thread pool used - number of tasks != number of created threads in this test (number: " << i << ")" << std::endl;
 		test_map_wraper_pooled<general_map_data_structure_shared_locks<std::string, std::string>, std::string, std::string > GeneralMapTest(tasks_n);
 		GeneralMapTest.start_tests("string");
 		GeneralMapTest.tests_end();
-		std::cout << std::endl << "test complete" << std::endl <<  std::endl; 
 	}
 
 	std::cout << std::endl  << "All tests complete" << std::endl;
