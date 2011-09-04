d4bd927ea5174ecf2a7966dd7117d2d8625bcfd6
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-09-04 10:35:42 +0000

    @Branches.0.5.ConcurrentMaps: moved general testing into separate class

--- ConcurrentMaps/src/main.cpp
+++ ConcurrentMaps/src/main.cpp
@@ -1,49 +1,14 @@
 #include <iostream>
-#include "general_map_data_structure.h"
-#include "general_map_data_structure_shared_locks.h"
-#include "general_test.h"
+#include "general_test_runer.h"
 
 int main()
 {
 	int tests = 5;
-	int tasks_n = 10000;
-	std::cout << "Number of required tests: " << tests << std::endl << "Number of threads in each test: " << tasks_n << std::endl << "Wait for it..." << std::endl;
+	int tasks_n = 1000;
+	
+	general_test_runer gt(tests, tasks_n);
+	gt.start();
 
-	std::cout << std::endl << "----" << std::endl << "Int tests" << std::endl << "----" << std::endl;
-
-	std::cout << std::endl << "testing general_map_data_structure:" << std::endl;
-	for(int i = 1; i < tests+1; ++i)
-	{
-		general_test<general_map_data_structure<int, int>, int, int > GeneralMapTest(tasks_n);
-		GeneralMapTest.start_tests("int");
-		GeneralMapTest.tests_end();
-	}
-	std::cout << std::endl << "testing general_map_data_structure_shared_locks:" << std::endl;
-	for(int i = 1; i < tests+1; ++i)
-	{
-		general_test<general_map_data_structure_shared_locks<int, int>, int, int > GeneralMapTest(tasks_n);
-		GeneralMapTest.start_tests("int");
-		GeneralMapTest.tests_end();
-	}
-
-	std::cout << std::endl << "----" << std::endl << "String tests" << std::endl << "----" << std::endl;
-
-	std::cout << std::endl << "testing general_map_data_structure:" << std::endl;
-	for(int i = 1; i < tests+1; ++i)
-	{
-		general_test<general_map_data_structure<std::string, std::string>, std::string, std::string > GeneralMapTest(tasks_n);
-		GeneralMapTest.start_tests("string");
-		GeneralMapTest.tests_end();
-	}
-	std::cout << std::endl << "testing general_map_data_structure_shared_locks:" << std::endl;
-	for(int i = 1; i < tests+1; ++i)
-	{
-		general_test<general_map_data_structure_shared_locks<std::string, std::string>, std::string, std::string > GeneralMapTest(tasks_n);
-		GeneralMapTest.start_tests("string");
-		GeneralMapTest.tests_end();
-	}
-
-	std::cout << std::endl  << "All tests complete" << std::endl;
 	std::cin.get();
 	return 0;
 }
