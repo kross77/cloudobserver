51988fdad06b05803d5cb8a2f65d038bd8def5c6
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-10-14 01:08:42 +0000

    @CloudServer: UFS lua project file added

---
+++ trunk/CloudServer/lua/services/project.UsersFilesService.lua
@@ -0,0 +1,32 @@
+project "UsersFilesService"
+	kind "SharedLib"
+	language "C++"
+	location ( "projects/" .. os.get() .. "-" .. action )
+	
+	cloud.project.init()
+	cloud.project.useBoost()
+	cloud.project.useopenSSL()
+
+	links { "sqlite" }
+	includedirs { "3rdparty/sqlite"}
+
+	links { "cf-http" }
+	includedirs { "3rdparty/cf-http" }
+	
+	links { "cf-util" }
+	includedirs { "src/cf-util"}
+	
+	links { "cf-fs" }
+	includedirs { "src/cf-fs"}
+	
+	includedirs { "service-interface/" }
+	
+	files { "src/default-services/users_files_service/**"}
+
+	configuration "Debug"
+		defines { "DEBUG" }
+		flags { "Symbols" , "Unicode"}
+	
+	configuration "Release"
+		defines { "NDEBUG" }
+		flags { "OptimizeSpeed" , "Unicode"}
