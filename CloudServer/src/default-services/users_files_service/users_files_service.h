bc26ca6e5336df791dcbcc134deb0cf5f62b3c77
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-10-14 01:06:49 +0000

    @CloudServer: Started creation of new service - Users Files Service (for safe personal files storing), fixed bug (premake4.lua file cf-fs include)

---
+++ CloudServer/src/default-services/users_files_service/users_files_service.h
@@ -0,0 +1,60 @@
+#ifndef FILE_SERVICE_H
+#define FILE_SERVICE_H
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+
+// Boost
+#include <boost/cstdint.hpp> //boost::uintmax_t
+#include <boost/asio.hpp>
+#include <boost/timer.hpp>
+#include <boost/date_time.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/shared_array.hpp>
+#include <boost/foreach.hpp>
+// Boost Extension
+#include <boost/extension/extension.hpp>
+#include <boost/extension/factory.hpp>
+#include <boost/extension/type_map.hpp>
+//SQLite
+#include <sqlite3pp.h>
+
+#include <general_utils.h>
+#include <http_utils.h>
+#include <fs_file.h>
+
+#include <service.hpp>
+
+
+class users_files_service: public service
+{
+public:
+	users_files_service();
+	virtual void service_call(boost::shared_ptr<boost::asio::ip::tcp::socket> socket, boost::shared_ptr<http_request> request, boost::shared_ptr<http_response> response);
+	virtual void apply_config(boost::property_tree::ptree config);
+	virtual void start(){}
+	virtual void stop(){}
+private:
+	std::string get_user_name(boost::shared_ptr<http_request> request);
+
+	boost::filesystem::path root_path;
+	boost::posix_time::time_duration expiration_period;
+	general_utils *general_util;
+	http_utils *http_util;
+	std::string max_age;
+
+	void insert_file_headers( boost::shared_ptr<fs_file> f, boost::shared_ptr<boost::asio::ip::tcp::socket> socket, boost::shared_ptr<http_response> response ){}
+	void send_not_modified_304( boost::shared_ptr<boost::asio::ip::tcp::socket> socket,  boost::shared_ptr<http_response> response ){}
+	void save_string_into_file(std::string contents, std::string name){}
+	void send_uncachable_file(boost::shared_ptr<fs_file> f,boost::shared_ptr<boost::asio::ip::tcp::socket> socket, boost::shared_ptr<http_response> response){}
+	void send_404(std::string encoded_url,boost::shared_ptr<boost::asio::ip::tcp::socket> socket, boost::shared_ptr<http_request> request, boost::shared_ptr<http_response> response){}
+
+};
+
+#endif // FILE_SERVICE_H
