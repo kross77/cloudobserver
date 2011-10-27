abe3089635fb2a966e3e0b5822aec6152fcc6b90
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-10-27 22:42:47 +0000

    @CloudServer: Updated UFS SQLite DB commands list.

--- trunk/CloudServer/src/default-services/users_files_service/users_files_service.h
+++ trunk/CloudServer/src/default-services/users_files_service/users_files_service.h
@@ -46,48 +46,48 @@ public:
 		this->expiration_period = boost::posix_time::minutes(200);
 		this->max_age = "max-age=" + boost::lexical_cast<std::string>(this->expiration_period.total_seconds());
 
-		this->command_create_users_table = "CREATE TABLE IF NOT EXISTS files (encoded_url varchar(300) UNIQUE NOT NULL primary key, file_name varchar(65) NOT NULL, user_name varchar(65) NOT NULL, is_public BOOLEAN NOT NULL, modified DATETIME NOT NULL default CURRENT_TIMESTAMP )";
-		this->command_create_user =  "INSERT INTO files (encoded_url, file_name, user_name, is_public, ) VALUES (?, ?, ?, ?, now)";
-		this->command_find_user = "SELECT email, pass FROM users WHERE email=";
+		this->default_db_name = "ufs.db";
+
+		this->command_create_files_table = "CREATE TABLE IF NOT EXISTS files (encoded_url varchar(300) UNIQUE NOT NULL primary key, file_name varchar(150) NOT NULL, user_name varchar(65) NOT NULL, is_public BOOLEAN NOT NULL, modified DATETIME NOT NULL default CURRENT_TIMESTAMP )";
+		this->command_create_file =  "INSERT INTO files (encoded_url, file_name, user_name, is_public ) VALUES (:encoded_url, :file_name, :user_name, :is_public)";
+		this->command_update_file = " UPDATE files SET encoded_url=:new_encoded_url, file_name=:new_file_name, is_public=:is_public, modified=CURRENT_TIMESTAMP  WHERE encoded_url=:encoded_url";
+		this->command_delete_file = "DELETE FROM files WHERE encoded_url=:encoded_url";
+		this->command_find_file = "SELECT file_name, user_name, is_public, modified FROM files WHERE encoded_url=:encoded_url";
+		this->command_find_all_user_files = "SELECT encoded_url, file_name, user_name, is_public, modified FROM files WHERE user_name=:user_name";
 
 	}
 	
 	virtual void service_call(boost::shared_ptr<boost::asio::ip::tcp::socket> socket, boost::shared_ptr<http_request> request, boost::shared_ptr<http_response> response)
 	{
-		send_fuck(socket, request, response);
-	}
-
-	void send_fuck( boost::shared_ptr<boost::asio::ip::tcp::socket> socket, boost::shared_ptr<http_request> request, boost::shared_ptr<http_response> response )
-	{
-		std::ostringstream body;
-		body << "Go fuck your self! " << http_util->url_decode(request->url) << " does not exist\n <br/> <a href='/'>" << "Dear " << http_util->url_decode(fs_util->get_user_name(request)) <<", please now fuck your self again!</a>";
-
-		response->body = "<head></head><body><h1>" + body.str() + "</h1></body>";
-		response->send(*socket);
+		
 	}
 
 	virtual void apply_config(boost::shared_ptr<boost::property_tree::ptree> config)
 	{
 		this->root_path = config->get<std::string>("users_files_directory", this->root_path.string());
 	}
+
 	virtual void start(){}
 	virtual void stop(){}
+
 private:
-
-
 	boost::filesystem::path root_path;
 	boost::posix_time::time_duration expiration_period;
 	general_utils *general_util;
 	http_utils *http_util;
 	fs_utils *fs_util;
 	std::string max_age;
-	std::string command_find_user;
-	std::string command_create_user;
-	std::string command_create_users_table;
+
+	std::string command_create_files_table;
+	std::string command_update_file;
+	std::string command_delete_file;
+	std::string command_create_file;
+	std::string command_find_file;
+	std::string default_db_name;
+	std::string command_find_all_user_files;
 	void insert_file_headers( boost::shared_ptr<fs_file> f, boost::shared_ptr<boost::asio::ip::tcp::socket> socket, boost::shared_ptr<http_response> response ){}
 	void send_uncachable_file(boost::shared_ptr<fs_file> f,boost::shared_ptr<boost::asio::ip::tcp::socket> socket, boost::shared_ptr<http_response> response){}
 	
-
 	CLOUD_SERVICE_AUXILIARIES;
 };
 
