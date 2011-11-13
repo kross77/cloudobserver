360da5ed3b82e868cd4382e5ca2b60824e534fe7
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-11-13 13:14:25 +0000

    @CloudServer:  Added linking of openCV libraries function. Windows tested

--- trunk/CloudServer/lua/cloud/cloud.project.lua
+++ trunk/CloudServer/lua/cloud/cloud.project.lua
@@ -44,6 +44,102 @@ function cloud.project.init()
 	end 
 end
 
+function cloud.project.useCV()
+	--
+	cloud.addLibDir( _OPTIONS["OpenCVLibsPath"] )
+	cloud.addIncludeDir(  _OPTIONS["OpenCVIncludesPath"] )
+	if os.get() == "windows" then
+		defines { "WIN" }
+		if  _OPTIONS["OpenCV-Shared"] then
+				links {
+					"opencv_core230",
+					"opencv_highgui230",
+					"opencv_imgproc230"
+				}
+			
+				cloud.win.copyDLL("OpenCV-2.3.0", "opencv_core230.dll")
+				cloud.win.copyDLL("OpenCV-2.3.0", "opencv_highgui230.dll")
+				cloud.win.copyDLL("OpenCV-2.3.0", "opencv_imgproc230.dll")
+			else	
+				configuration { }
+				
+				configuration { "Debug" }
+					links {
+						"opencv_core230d",
+						"opencv_highgui230d",
+						"opencv_imgproc230d",
+						"libjasperd",
+						"libjpegd",
+						"libpngd",
+						"libtiffd",
+						"zlibd",
+						"vfw32",
+						"comctl32"
+					}
+				configuration { }
+				
+				configuration { "Release" }
+					links {
+						"opencv_core230",
+						"opencv_highgui230",
+						"opencv_imgproc230",
+						"libjasper",
+						"libjpeg",
+						"libpng",
+						"libtiff",
+						"zlib",
+						"vfw32",
+						"comctl32"
+					}
+				configuration { }
+			end
+		cloud.win.addLibFromProgrammFiles("OpenCV-2.3.0")
+	end
+	
+	if os.get() == "linux" then
+		defines { "LIN" }
+		links {
+			"opencv_imgproc",
+			"opencv_core",
+			"opencv_highgui"
+		}
+		if  _OPTIONS["OpenCV-Shared"] then else
+			links {
+				"libjasper",
+				"libjpeg",
+				"libpng",
+				"libtiff",
+				"zlib"
+			}
+		end
+	end
+	
+	if os.get() == "macosx" then
+		defines { "MAC" }
+		links {
+			"opencv_imgproc",
+			"opencv_core",
+			"opencv_highgui",
+			"QuickTime.framework"
+		}
+		
+		if  _OPTIONS["OpenCV-Shared"] then else
+			links {
+				"libjasper",
+				"libjpeg",
+				"libpng",
+				"libtiff",
+				"zlib",
+				"AppKit.framework",
+				"CoreVideo.framework",
+				"QTKit.framework",
+			}
+		end
+	end 
+end
+
+
+
 function cloud.project.useBoost()
 	--
 	cloud.addIncludeDir( _OPTIONS["BoostIncludesPath"] )
