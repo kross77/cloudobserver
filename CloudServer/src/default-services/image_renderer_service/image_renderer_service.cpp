865c81edce6934056dc7f424ecd1f2df16e5c64b
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-11-24 21:13:11 +0000

    @CloudServer:  Added a dummy for Image Renderer service (IRS). Currently it has all includes will probably need in real IRS, it traces  `hello image renderer service` on /image_renderer.service url.

---
+++ CloudServer/src/default-services/image_renderer_service/image_renderer_service.cpp
@@ -0,0 +1,7 @@
+#include "image_renderer_service.h"
+
+BOOST_EXTENSION_TYPE_MAP_FUNCTION
+{
+	std::map<std::string, boost::extensions::factory<service> > &factories(types.get());
+	factories["image_renderer_service"].set<image_renderer_service>();
+}
