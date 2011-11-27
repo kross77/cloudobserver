edd574e8e4ec142d9c69d34970e4f9b1501977f5
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2011-11-27 19:16:40 +0000

    @CloudServer: Added documentation generation files. Documented timer class, provided a demo.

--- CloudServer/src/cf-util/timer.h
+++ CloudServer/src/cf-util/timer.h
@@ -2,12 +2,54 @@
 #define TIMER_H
 
 #include <boost/date_time.hpp>
-
+/*!
+ * \brief Provides a stopwatch mechanism 
+ *
+ * Designed designed to measure the amount of time elapsed from a particular time when activated to when is deactivated.
+ *
+ * \note timer class requires boost_date_time library
+ * 
+ * quick demo:
+ * \include cf-util/timer_demo.cpp
+ */
 class timer
 {
 public:
+	/*!
+	 * \brief Initializes a new instance of the timer class, and sets all the properties to their initial values.
+	 *
+	 * \n FullName:  timer::timer
+	 * \n Access:    public  
+	 */
 	timer();
+	/*!
+	 * \brief restarts a timer
+	 *
+	 * Resets the stopwatch to zero.
+	 *
+	 * \n FullName:  timer::restart
+	 * \n Access:    public  
+	 *
+	 * \return void
+	 *
+	 */
 	void restart();
+	/*!
+	 * \brief returns elapsed time
+	 *
+	 * returns elapsed time from timer creation or restart()
+	 *
+	 * \n FullName:  timer::elapsed
+	 * \n Access:    public  
+	 *
+	 * \return boost::posix_time::time_duration
+	 * \note you can use std::cout with this function
+	 * \code
+	 * timer t;
+	 * std::cout << "time passed: " << t.elapsed() << std::endl ;
+	 * \endcode
+	 *
+	 */
 	boost::posix_time::time_duration elapsed() const;
 private:
 	boost::posix_time::ptime start_time;
