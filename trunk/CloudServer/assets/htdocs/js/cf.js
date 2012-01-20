a837a695d7831bbd503e9be275c24a64259bfddd
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-01-20 10:44:59 +0000

    @CloudServer: Made footer expendable via JS.

--- trunk/CloudServer/assets/htdocs/js/cf.js
+++ trunk/CloudServer/assets/htdocs/js/cf.js
@@ -31,7 +31,7 @@ function readCookie(name) {
 })(jQuery);
 
 $(document).ready(function() {
-	$('body').append("<div style='position:fixed;min-height:20px;height:auto !important;height:20px;background-color:#3f3b8d;background-color:rgba(0,0,0,0.6);bottom:0; width:100%'><p style='font-size: 8pt'> Copyright &copy; 2012 <a id='rol' href='#cloudobserver' onclick=\"{	newwindow=window.open(\'http://code.google.com/p/cloudobserver/\',\'CloudObserver\',\'height=750,width=900\');	if (window.focus) {newwindow.focus()}}\">Cloud Forever</a>. Some rights reserved. </p><div>");
+	$('body').append("<div id=\"cf-footer\" style='position:fixed;min-height:20px;height:auto !important;height:20px;background-color:#3f3b8d;background-color:rgba(0,0,0,0.6);bottom:0; width:100%'><p id=\"cf-footer-paragraph\" style='font-size: 8pt'> Copyright &copy; 2012 <a id='rol' href='#cloudobserver' onclick=\"{	newwindow=window.open(\'http://code.google.com/p/cloudobserver/\',\'CloudObserver\',\'height=750,width=900\');	if (window.focus) {newwindow.focus()}}\">Cloud Forever</a>. Some rights reserved. </p><div>");
 
 	$(".butt").live('mousedown', function(){
 		$(this).addClass("hilight2");
