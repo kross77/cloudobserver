52d3ddcbf3379c46e3780c2ba58f45c5666ba8a6
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-01-25 23:33:45 +0000

    @CloudLoader: Made code base update less painful for dynamically created databases.

--- trunk/CloudLoader/cloud_server_net_setup.sh
+++ trunk/CloudLoader/cloud_server_net_setup.sh
@@ -90,8 +90,8 @@ cd $CLOUD_ROOT_DIR
 if [ ! -d $CLOUD_INSTALL_SUBDIR ]; then
 	echo_run mkdir $CLOUD_INSTALL_SUBDIR
 else
-	echo_run rm -rf $CLOUD_INSTALL_SUBDIR
-	echo_run mkdir $CLOUD_INSTALL_SUBDIR
+	echo_run rm -rf $CLOUD_INSTALL_SUBDIR/htdocs
+	echo_run rm -rf $CLOUD_INSTALL_SUBDIR/config.xml 
 fi
 
 if [ "$KEEP_OLD" = "yes" ]; then
