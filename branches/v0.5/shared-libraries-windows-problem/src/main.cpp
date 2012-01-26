1ff6a12d9b259447290b1ec09a8c3eef5f27e735
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-01-26 03:15:53 +0000

    @Branches: Showing the way we'll get around windows heap corruption problem using pure boost shared pointers API and inheritance (all objects sent into services would have getters and setters inside, all fields will be represented as shared pointers)

--- branches/v0.5/shared-libraries-windows-problem/src/main.cpp
+++ branches/v0.5/shared-libraries-windows-problem/src/main.cpp
@@ -10,34 +10,54 @@
 
 #include "plug_in_interface.h"
 
+void fuck2(int *&b)
+{
+	(*b)++;
+}
+
+void fuck(int **b)
+{
+	(**b)++;
+}
+
 int main()
 {
-	{
-		std::cout << "creating plugin instance:" << std::endl;
-		std::pair< boost::shared_ptr<boost::extensions::shared_library>, boost::shared_ptr<plugin> > map = extension_utils::give_me_class<plugin>("DemoPlugin", "my_plugin"); 
-		boost::shared_ptr<plugin> my_plugin = map.second;
-		std::cout << "plugin instance created from dynamically loaded shared library." << std::endl;
-		std::cout << "calling plugin instance:" << std::endl;
-		my_plugin->simple_call();
-		std::cin.get();
-		std::cout << "instance called." << std::endl;
+	
+	std::cout << "creating plugin instance:" << std::endl;
+	boost::shared_ptr<plugin> my_plugin = extension_utils::give_me_class<plugin>("DemoPlugin", "my_plugin");
+	std::cout << "plugin instance created from dynamically loaded shared library." << std::endl;
 		
-		{
-			std::string **a = new std::string*;
-			*a = new std::string("A b c d e f g");
-			my_plugin->pass_and_modify_data(a);
-			std::cout << "modified data: " <<  **a << std::endl;
-			std::cin.get();
-			my_plugin->delete_ptr(*a);
-			delete a;
-			std::cout << "removed data" << std::endl;
-		}
-		std::cin.get();
-
-		//currently we can not close lib successfully
-		//map.first->close();
+	std::cout << "Demo call to plugin instance:" << std::endl;
+	{
+		my_plugin->simple_call();
+		std::cout << "instance called." << std::endl;
 	}
+		
+	std::cout << std::endl << "Old School memory management:" << std::endl;
 	std::cin.get();
+	{
+		std::string **a = new std::string*;
+		*a = new std::string("A b c d e f g");
+		my_plugin->pass_and_modify_data(a);
+		std::cout << "modified data: " <<  **a << std::endl;
+		my_plugin->delete_ptr(*a);
+		delete a;
+		std::cout << "removed data" << std::endl;
+	}
+
+	std::cout << std::endl << "Modern memory management:" << std::endl;
+	std::cin.get();
+	{
+		boost::shared_ptr<std::string> a(new std::string("A b c d e f g"));
+		boost::shared_ptr<std::string> b = my_plugin->pass_and_modify_data(a);
+		std::cout << "modified data: " << *b << std::endl;
+	}
+	std::cout << "removed data" << std::endl;
+	std::cin.get();
+
+	std::cout << "And now we will crush because do not know how to close the library:" << std::endl;
+	std::cin.get();
+
 }
 
 
