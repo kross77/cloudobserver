e85e041f61c4aed5fea29575926580e946945e92
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-01-29 21:25:57 +0000

    @CloudServer: now passing raw_in and raw_out from/to services.

--- CloudServer/service-interface/service.hpp
+++ CloudServer/service-interface/service.hpp
@@ -10,6 +10,7 @@
 
 #include <boost/serialization/serialization.hpp>
 #include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
 #include <boost/serialization/map.hpp> 
 #include <boost/serialization/string.hpp>
 
@@ -21,7 +22,25 @@
 #define SHARED_HPP
 class shared
 {
-public: std::map<std::string, std::string> data;
+public:
+	std::map<std::string, std::string> data;
+
+	boost::shared_ptr<std::string> serialize()
+	{
+		std::stringstream oa_ss_data;
+		boost::archive::text_oarchive  oa_data(oa_ss_data);
+		oa_data << *this;
+		return boost::shared_ptr<std::string>(new std::string(oa_ss_data.str()),boost::bind(&pointer_utils::delete_ptr<std::string>, _1) );
+	}
+
+	void deserialize( boost::shared_ptr<std::string> request_string)
+	{
+		std::stringstream ia_ss_data;
+		ia_ss_data << *request_string;
+		boost::archive::text_iarchive ia_data(ia_ss_data);
+		ia_data >> *this;	 
+	}
+
 private:
 	friend class boost::serialization::access;
 
@@ -35,19 +54,37 @@ private:
 
 #ifndef RAW_HPP
 #define RAW_HPP
-struct raw {
+struct raw_in {
+	raw_in(): raw_request(new std::string(""), boost::bind(&pointer_utils::delete_ptr<std::string>, _1)), 
+		shared_data(new std::string(""), boost::bind(&pointer_utils::delete_ptr<std::string>, _1)), 
+		error_data(new std::string(""), boost::bind(&pointer_utils::delete_ptr<std::string>, _1)),
+		raw_response(new std::string(""), boost::bind(&pointer_utils::delete_ptr<std::string>, _1))
+	{
+	}
  boost::shared_ptr<std::string> raw_request;
  boost::shared_ptr<std::string> raw_response;
  boost::shared_ptr<std::string> shared_data;
  boost::shared_ptr<std::string> error_data;
  boost::shared_ptr<boost::asio::ip::tcp::socket> socket;
 };
+
+struct raw_out {
+	raw_out() : raw_request(new std::string(""), boost::bind(&pointer_utils::delete_ptr<std::string>, _1)), 
+		shared_data(new std::string(""), boost::bind(&pointer_utils::delete_ptr<std::string>, _1)), 
+		error_data(new std::string(""), boost::bind(&pointer_utils::delete_ptr<std::string>, _1))
+	{
+	}
+	boost::shared_ptr<std::string> raw_request;
+	boost::shared_ptr<std::string> shared_data;
+	boost::shared_ptr<std::string> error_data;
+};
+
 #endif // RAW_HPP
 
 class base_service
 {
 public:
-	virtual boost::shared_ptr<std::string> make_service_call(raw serialized_data) = 0;
+	virtual raw_out make_service_call(raw_in serialized_data) = 0;
 
 	virtual void service_call(boost::shared_ptr<boost::asio::ip::tcp::socket>, boost::shared_ptr<http_request>, boost::shared_ptr<http_response>) = 0;
 	virtual void apply_config(boost::shared_ptr<boost::property_tree::ptree>) { throw not_configurable_exception(); };
@@ -63,27 +100,36 @@ public:
 class service : public base_service
 {
 public:
-	virtual boost::shared_ptr<std::string> make_service_call(raw serialized_data)
+	virtual raw_out make_service_call(raw_in serialized_data)
 	{
-
+		
 		boost::shared_ptr<boost::asio::ip::tcp::socket> socket = serialized_data.socket;
 
-		boost::shared_ptr<http_request> request (new http_request());
+		boost::shared_ptr<http_request> request (new http_request(),  boost::bind(&pointer_utils::delete_ptr<http_request>, _1));
 		request->deserialize(serialized_data.raw_request);
 
-		boost::shared_ptr<http_response> response (new http_response());
+		boost::shared_ptr<http_response> response (new http_response(), boost::bind(&pointer_utils::delete_ptr<http_response>, _1));
 		response->deserialize(serialized_data.raw_response);
 
-		boost::shared_ptr<std::string> err(new std::string(""), boost::bind(&pointer_utils::delete_ptr<std::string>, _1));
+		boost::shared_ptr<shared> shared_data (new shared(), boost::bind(&pointer_utils::delete_ptr<shared>, _1));
+		shared_data->deserialize(serialized_data.shared_data);
+
 		try
 		{
 			service_call(socket, request, response);
 		}
 		catch(std::exception &e)
 		{
+			boost::shared_ptr<std::string> err(new std::string(""), boost::bind(&pointer_utils::delete_ptr<std::string>, _1));
 			*err = std::string( e.what());
+			raw_out out;
+			out.error_data = err;
+			return out;
 		}
-		return err;
+		raw_out out;
+		out.raw_request = request->serialize();
+		out.shared_data = shared_data->serialize();
+		return out;
 	}
 };
 
