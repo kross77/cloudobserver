44a16a7ef01948f3ce1807072f9ac3717698cde0
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-02-02 17:55:05 +0000

    @CloudServer: Made user name appear so that it would always stay right top of the head panel

--- trunk/CloudServer/assets/htdocs/js/cf.head.panel.js
+++ trunk/CloudServer/assets/htdocs/js/cf.head.panel.js
@@ -1,7 +1,14 @@
+function hidePopUbBubble(){
+	$(".new").empty();
+	$(".new").detach();
+	$(".new").remove();
+}
+
 $(document).ready(function() {
+
 	$('body').append("<div class=\"hidden-c\" style=\"display: none;\"><!-- --><div id=\"tabs-logout\" class=\"logout\"><fieldset><legend>Logout?</legend><form  action=\"./uac.service\"><input type=\"hidden\" name=\"redirect_to\" value=\""+location.href+"\"><input type=\"hidden\" name=\"user_control\" value=\"logout\"><input type=\"hidden\" name=\"logout\" value=\"true\"><input type=\"submit\" class=\"eButton\" value=\"OK\" /><input type=\"button\" class=\"eButton\" value=\"Cancel\" onClick=\'hideDialog($(\".alert\"));\' /></form></fieldset></div><!-- --></div>");
 
-	$('body').append("<div class='header'><div id='marx-user-name' > <div><a href='index.html' alt='Cloud Server Main Page'><p style='background: url(/logo-small.png); width:40px; height:40px;' class='logo'></p></a></div> <div data-template><a><p class='header-item' id='user_name' class='hidden-c' >{{user_name}}</p></a></div></div><div id='marx-services-list'><div data-template><a href='{{url}}'><p class='header-item'>{{name}}</p></a></div></div></div>");
+	$('body').append("<div class='header'><table style='padding:0px; margin: 0px 0px 0px 0px; width:100%' cellpadding='0' cellspacing='0'><tr><td id='plogo'  style='width:40px; height:40px;'><div><a href='index.html' alt='Cloud Server Main Page'><p style='background: url(/logo-small.png); width:40px; height:40px;' class='logo'></p></a></div></td><td><div id='marx-services-list'><div data-template><a href='{{url}}'><p class='header-item' style='min-width:100px;'>{{name}}</p></a></div></div></td><td style='white-space:nowrap;text-align:right;'> <div id='marx-user-name' ><div data-template><a><p class='header-item' id='user_name' class='hidden-c' >{{user_name}}</p></a></div></div></td></tr></table></div>");
 
    var servicesList = Tempo.prepare('marx-services-list').notify( function (event) {
     if ( event.type === TempoEvent.Types.RENDER_COMPLETE) { page_update.run(); }
@@ -49,7 +56,7 @@ $(document).ready(function() {
 	//( elemPos.top  + (40%(eventObj.pageY))
 			$(newer_id).grumble({
 				text: "  <a href='#' class='logout-but'><h3>Log me out</h3></a> <br/> <a href='#' ><h3>Cancel</h3></a>    ",
-				angle: (Math.random() * 50 + 130),
+				angle: (Math.random() * 50 + 170),
 				distance: 3,
 				showAfter: 15,
 				hideAfter: false,
@@ -57,7 +64,7 @@ $(document).ready(function() {
 				hasHideButton: true,
 				// just shows the button
 				buttonHideText: 'Pop!',
-				onShow: function() {
+				onShow: function() {	
 					$('.logout-but').mouseup(function() {
 						showForm($('#tabs-logout'), 'Logout form',350, 120);
 					});
@@ -67,9 +74,8 @@ $(document).ready(function() {
 					$('.border').removeClass("border");
 				},
 				onHide: function() {
-					$(".new").empty();
-					$(".new").detach();
-					$(".new").remove();
+					hidePopUbBubble();
+					$(newer_id).grumble('hide');
 				}
 			});
 	
