757ff571b08c719acd861fe07bc72117b0341e52
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-02-29 15:25:21 +0000

    @CloudServer: UFS RIA context menu now provides 'get link' option

--- trunk/CloudServer/assets/htdocs/js/cf.ufs.application.js
+++ trunk/CloudServer/assets/htdocs/js/cf.ufs.application.js
@@ -1,8 +1,10 @@
 var file_id;
+var file_title;
 $(document).ready(function() {
 	user = readCookie('session-id');
 	var files_menu = ui.menu()
-	.add('Download item' , function(){ window.open('/'+file_id, '_blank'); });
+	.add('Download item' , function(){ window.open('/'+file_id, '_blank'); })
+	.add('Get link' , function(){ showAlert( 600, 100 , ('link to ' + file_title + ' file.') , '<input type="text" style="width:590px" class="text" value=\"' + location.host + ('/'+file_id ) + '\">'); });
 	
 	if (user != null) {	
 			$('.loged-in-user').show();
@@ -27,6 +29,7 @@ $(document).ready(function() {
 	}
 	
 	$('.lst .uf .butt2').live('click', function(e) {
+		file_title = this.title;
 		file_id = this.id;
 		files_menu.moveTo(e.pageX, e.pageY).show();
 	}).live('mousedown', function(e){
