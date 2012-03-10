950d808ba6639ebefa23ae50b41d726988badd4e
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-03-10 10:54:13 +0000

    @CloudServer: FLV-related constants definition was moved into separate header file in the Observer service.

---
+++ trunk/CloudServer/src/default-services/observer_service/flv_format.h
@@ -0,0 +1,14 @@
+#ifndef FLV_FORMAT_H
+#define FLV_FORMAT_H
+
+#define HEADER_LENGTH 13
+#define SIGNATURE1 0x46
+#define SIGNATURE2 0x4C
+#define SIGNATURE3 0x56
+#define VERSION 1
+#define TAG_HEADER_LENGTH 11
+#define TAGTYPE_AUDIO 8
+#define TAGTYPE_VIDEO 9
+#define TAGTYPE_DATA 18
+
+#endif // FLV_FORMAT_H
