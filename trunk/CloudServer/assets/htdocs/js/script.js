6099660954220214088452b9784992022a990e7c
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-03-11 00:31:39 +0000

    @CloudServer: making player page open request look as different request each time to browsers

--- trunk/CloudServer/assets/htdocs/js/script.js
+++ trunk/CloudServer/assets/htdocs/js/script.js
@@ -145,7 +145,7 @@ openWin = function(name, width, height)
 		left = w + 1;
 		tops = h + 1;
 		w = w + width + 7;
-		var file = "./player.html#" + name;
+		var file = "./player.html?" + Math.floor(Math.random()*100+1) +"#" + name;
 		var settings = 'width=' + width + ', height=' + height + ', left=' + left + ', top=' + tops + ', screenX=' + left + ', screenY=' + tops;
 		wins.push(window.open(file, name, settings));
 		exNames.push(name);
