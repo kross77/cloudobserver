af2b791978de8ea19c7e3c667613ad42f4adad67
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-03-12 00:54:21 +0000

    @CloudServer: header panel refactoring.

--- trunk/CloudServer/assets/htdocs/js/cf.head.panel.js
+++ trunk/CloudServer/assets/htdocs/js/cf.head.panel.js
@@ -1,3 +1,48 @@
+	function updateMenu(){
+		$('.nav li').each( function(i, e) { $(e).css("opacity", 1);});
+		var lastItemIndex = Number.POSITIVE_INFINITY;
+		var lastItemText = "";
+		var maxWidth = 0;
+		var overflowCount=0;
+		var navHeight = $('.nav').height() ;
+		$('.nav li').each(function(i,e){
+		console.log($(e).position().top);
+			if($(e).position().top>=navHeight){
+				if(i<=lastItemIndex) lastItemIndex=i-1;
+				if($(e).width()>maxWidth) maxWidth = $(e).width();
+				overflowCount++;
+			}
+		});
+		maxWidth = Math.max(maxWidth,$('.nav li:eq('+(lastItemIndex)+')').width());
+		$('.nav li:eq('+(lastItemIndex)+')').css("opacity", 0);
+				var moreHeight =  (overflowCount+2)*(navHeight);
+		$('#moreMenu').remove();
+		if(overflowCount>0){
+			//alert(overflowCount);
+			$('<ul id="moreMenu"/>').appendTo('body').width(maxWidth+40).height(navHeight);
+			$('#moreMenu').offset($('.nav li:eq('+(lastItemIndex)+')').offset());
+			if( (lastItemIndex - 1) >= 0 ){
+				$('#moreMenu').append('<li>More... <span class="profile-triangle">▾</span></li>');
+		        $('.nav li:gt(' + (lastItemIndex - 1) + ')').each(function(i, e) {
+		            $('#moreMenu').append('<li>' + $(e).html() + '</li>');
+		        });
+		    }else{
+		    	$('#moreMenu').append('<li>Services <span class="profile-triangle">▾</span></li>');
+		        $('.nav li').each(function(i, e) {
+		            $('#moreMenu').append('<li>' + $(e).html() + '</li>');
+		        });
+		    }
+			moreHeight = 0;
+			$('#moreMenu li').each(function(i, e) {
+				moreHeight += $(e).innerHeight();
+			});
+			$('#moreMenu').hover(
+				function(){$(this).height(moreHeight);},
+				function(){$(this).height(40);});
+		}
+	}
+
+
 function hidePopUbBubble(){
 	$(".new").empty();
 	$(".new").detach();
@@ -8,10 +53,11 @@ $(document).ready(function() {
 
 	$('body').append("<div class=\"hidden-c\" style=\"display: none;\"><!-- --><div id=\"tabs-logout\" class=\"logout\"><fieldset><legend>Logout?</legend><form  action=\"./uac.service\"><input type=\"hidden\" name=\"redirect_to\" value=\""+location.href+"\"><input type=\"hidden\" name=\"user_control\" value=\"logout\"><input type=\"hidden\" name=\"logout\" value=\"true\"><input type=\"submit\" class=\"eButton\" value=\"OK\" /><input type=\"button\" class=\"eButton\" value=\"Cancel\" onClick=\'hideDialog($(\".alert\"));\' /></form></fieldset></div><!-- --></div>");
 
-	$('body').append("<div class='header'><table style='padding:0px; margin: 0px 0px 0px 0px; width:100%' cellpadding='0' cellspacing='0'><tr><td id='plogo'  style='width:40px; height:40px;'><div><a href='index.html' alt='Cloud Server Main Page'><p style='background: url(/logo-small.png); width:40px; height:40px;' class='logo'></p></a></div></td><td><div id='marx-services-list'><div data-template><a href='{{url}}'><p class='header-item' style='min-width:100px;'>{{name}}</p></a></div></div></td><td style='white-space:nowrap;text-align:right;'> <div id='marx-user-name' ><div data-template><a><p class='header-item' id='user_name' class='hidden-c' >{{user_name}}</p></a></div></div></td></tr></table></div>");
-
+	$('body').prepend("<div class='header'''><table style='padding:0px; margin: 0px 100px 0px 0px; width:100%' cellpadding='0' cellspacing='0'><tr><td id='plogo' style='width:50px; height:40px;'><div><a href='index.html'></a><p class='logo' style='background: url(/logo-small.png); width:50px; height:40px; background-repeat: no-repeat;	background-position: center center;'></p></div></td><td><div id='marx-services-list'><ul class='nav'><div data-template><li><a href='{{url}}' style='width:100%; height:100%'><p style='min-width:100px;'>{{name}}</p></a></li></div></ul></div></td><td style='white-space:nowrap;text-align:right;'><div id='marx-user-name' ><div data-template><a><p class='header-item' id='user_name' class='hidden-c' style='min-width:100px; z-index=999;'>{{user_name}}</p></a></div></div></td></tr></table></div>");
+	//$(window).resize(updateMenu);
+	page_update.add(updateMenu);
    var servicesList = Tempo.prepare('marx-services-list').notify( function (event) {
-	if ( event.type === TempoEvent.Types.RENDER_COMPLETE) { page_update.run(); }
+	if ( event.type === TempoEvent.Types.RENDER_COMPLETE) { page_update.run();}
 	});
 			servicesList.starting();
 	
@@ -19,21 +65,11 @@ $(document).ready(function() {
 		servicesList.render(data);
 		});
 	
-	$('.header-item, .logo').live('mousedown', function(){
-		$(this).addClass("hilight");
-	}).live('mouseup', function(){
-		$(this).removeClass("hilight");
-	}).live('mouseover', function(){
-		$(this).addClass("border");
-	}).live('mouseout', function(){
-		$(this).removeClass("border");
-		$(this).removeClass("hilight");
-	});
-	
+	//updateMenu();
 	user = readCookie('session-id');
 	if (user != null) {
 		var user_name = Tempo.prepare('marx-user-name').notify( function (event) {
-	if ( event.type === TempoEvent.Types.RENDER_COMPLETE) { page_update.run(); }
+	if ( event.type === TempoEvent.Types.RENDER_COMPLETE) { page_update.run();}
 	});
 		user_name.starting();
 		
@@ -92,4 +128,5 @@ $(document).ready(function() {
 	}else{
 		$('#user_name').hide();
 		}
+	page_update.run();
 });
