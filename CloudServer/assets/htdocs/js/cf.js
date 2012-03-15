af4a8d11afea8ba8407de125a1762bdcb1edcc9f
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-03-15 14:24:03 +0000

    @CloudServer: updating GA script

--- CloudServer/assets/htdocs/js/cf.js
+++ CloudServer/assets/htdocs/js/cf.js
@@ -110,27 +110,15 @@ $(document).ready(function() {
 	
 	var _gaq = _gaq || [];
 	_gaq.push(['_setAccount', 'UA-21524436-1']);
-	_gaq.push(['_setDomainName', 'none']);
+	_gaq.push(['_setDomainName', 'apmath.spbu.ru']);
 	_gaq.push(['_setAllowLinker', true]);
 	_gaq.push(['_trackPageview']);
-
-	(function()
-	{
-		var ga = document.createElement('script');
-		ga.type = 'text/javascript';
-		ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(ga, s);
-	})();
-	_gaq.push(['_setCustomVar', 1, // This custom var is set to slot #1
-	'URL', // The name acts as a kind of category for the user activity
-	window.location.toString(), // This value of the custom variable
-	1]);
-	_gaq.push(['_trackEvent', 'Visit on page', // category of activity
-	window.location.toString() // Action
-	]);
 	
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
 
 	$(window).bind('resize', function() {
 		$('html', 'body').css("background-size", "cover");
