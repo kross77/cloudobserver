781240b7ec41ffbf4a50f196858a70ec33949b16
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-03-15 14:38:29 +0000

    @CloudServer: fixing "GA script does not work" bug

--- trunk/CloudServer/assets/htdocs/js/cf.js
+++ trunk/CloudServer/assets/htdocs/js/cf.js
@@ -108,19 +108,18 @@ $(document).ready(function() {
 	
 	page_update.add(updateBodyWidth);
 	
-
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-21524436-1']);
-	_gaq.push(['_trackPageview']);
-	
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-
 	$(window).bind('resize', function() {
 		$('html', 'body').css("background-size", "cover");
 		 page_update.run();
 	});	
 });
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-21524436-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
