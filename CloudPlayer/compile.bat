8dd0c7fcd6a2f4a196e31c9af4b5f04a2f44db88
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-03-22 18:08:40 +0000

    @CloudServer: updating CloudPlayer.

--- CloudPlayer/compile.bat
+++ CloudPlayer/compile.bat
@@ -1,4 +1,4 @@
 :: requires mxmlc in PATH;
 :: tested with Open Source Flex SDK 4.5.1 (MIT) http://opensource.adobe.com/wiki/display/flexsdk/Download+Flex+4.5
 
-mxmlc  -library-path+=libs/as3crypto-1_3_patched.swc  -library-path+=libs/corelib-20070830.swc  -library-path+=libs/flexunit-20071226.swc  --target-player=10.2 --optimize=true -static-link-runtime-shared-libraries --debug=false --default-script-limits 5000 60 --advanced-anti-aliasing=true  src/player.mxml -o player.swf
+mxmlc  -library-path+=libs/as3crypto-1_3_patched.swc  -library-path+=libs/corelib-20070830.swc  -library-path+=libs/flexunit-20071226.swc  --target-player=10.1 --optimize=true -static-link-runtime-shared-libraries --debug=false --default-script-limits 5000 60 --advanced-anti-aliasing=true  src/player.mxml -o player.swf
