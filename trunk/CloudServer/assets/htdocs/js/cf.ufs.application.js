b24f8b943bf32ca65f07ccdcc54489e66eb39c74
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-03-26 02:25:35 +0000

    @CloudServer: started using DELETE GET method of UFS service inside UFS js RIA.

--- trunk/CloudServer/assets/htdocs/js/cf.ufs.application.js
+++ trunk/CloudServer/assets/htdocs/js/cf.ufs.application.js
@@ -1,47 +1,60 @@
 var file_id;
 var file_title;
 $(document).ready(function() {
-	user = readCookie('session-id');
+
 	var files_menu = ui.menu()
 	.add('Download item' , function(){ window.open(""+file_id, '_blank'); })
-	.add('Get link' , function(){ showAlert( 600, 100 , ('link to ' + file_title + ' file.') , '<input type="text" style="width:590px" class="text" value=\"' + location.host + ('/'+file_id ) + '\">'); });
-	
-	if (user != null) {	
-			$('.loged-in-user').show();
-			$('.not-loged-in-user').remove();
-			   var fs = Tempo.prepare('marx-brothers')
-			   .notify( function (event) {
-    if (event.type === TempoEvent.Types.RENDER_COMPLETE) {
-        $( "#marx-brothers" ).selectable({
-        	start: function(e, ui) { files_menu.hide(); },
-			stop:  function(e, ui) {
-				var selected_array = []
-				$( "div.ui-selected > li > a > p", this ).each(function() {
-				if (! $(this).parent('a').hasClass('ui-selected'))
-				{
-					$(this).parent('a').addClass('ui-selected');
-				}
-				
-				var element = this; //$(this) .closest('a > p');
-				selected_array = selected_array.concat( element );
-
-				});
-				if(selected_array.length == 1)
-				{
-					file_title = selected_array[0].title;
-					file_id = selected_array[0].id;
-					files_menu.moveTo(e.pageX, e.pageY).show();
-				}
+	.add('Get link' , function(){ showAlert( 600, 100 , ('link to ' + file_title + ' file.') , '<input type="text" style="width:590px" class="text" value=\"' + location.host + ('/'+file_id ) + '\">'); })
+	.add('Delete item' , function(){ $.ajax({
+		type: "GET",
+		url:"ufs.service", 
+		data: "action=delete&url="+file_id,
+		success: function(data) {
+			$.getJSON("ufs.json", function(data) {
+				fs.render(data);
+			});
 		}
+	});
+});
+
+	user = readCookie('session-id');
+	if (user != null) {	
+		$('.loged-in-user').show();
+		$('.not-loged-in-user').remove();
+		var fs = Tempo.prepare('marx-brothers')
+			.notify( function (event) {
+				if (event.type === TempoEvent.Types.RENDER_COMPLETE) {
+					$( "#marx-brothers" ).selectable({
+						start: function(e, ui) { files_menu.hide(); },
+						stop:  function(e, ui) {
+							var selected_array = []
+							$( "div.ui-selected > li > a > p", this ).each(function() {
+							if (! $(this).parent('a').hasClass('ui-selected'))
+							{
+								$(this).parent('a').addClass('ui-selected');
+							}
+							
+							var element = this; //$(this) .closest('a > p');
+							selected_array = selected_array.concat( element );
+			
+							});
+							if(selected_array.length == 1)
+							{
+								file_title = selected_array[0].title;
+								file_id = selected_array[0].id;
+								files_menu.moveTo(e.pageX, e.pageY).show();
+							}
+					}
 				});
-		    }
+			}
 		});
 		
 		$(document).click( function (){$(".ui-selected").removeClass("ui-selected");});
-				fs.starting();
+		
+		fs.starting();
 	
 		$.getJSON("ufs.json", function(data) {
-		    fs.render(data);
+			fs.render(data);
 		});
 	}
 	
