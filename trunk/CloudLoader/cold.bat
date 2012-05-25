896a96584db02c870292ea31defe8676bbaf4771
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-05-25 19:20:03 +0000

    @CloudLoader: The wrapper for the loader script on Windows was modified to define VSVERSION variable which indicates the version of Visual Studio (2008 or 2010) that has been detected. The variable will be used by the loader script later.

--- trunk/CloudLoader/cold.bat
+++ trunk/CloudLoader/cold.bat
@@ -55,15 +55,19 @@ if defined ProgramFiles(x86) (
 )
 for /f "tokens=2,*" %%a in ('reg query %BaseRegistryPath%\VCExpress\9.0\Setup\VS /v "ProductDir" 2^>nul ^| findstr ProductDir') do (
 	set VisualStudioPath=%%b
+	set VSVERSION=9
 )
 for /f "tokens=2,*" %%a in ('reg query %BaseRegistryPath%\VisualStudio\9.0\Setup\VS /v "ProductDir" 2^>nul ^| findstr ProductDir') do (
 	set VisualStudioPath=%%b
+	set VSVERSION=9
 )
 for /f "tokens=2,*" %%a in ('reg query %BaseRegistryPath%\VCExpress\10.0\Setup\VS /v "ProductDir" 2^>nul ^| findstr ProductDir') do (
 	set VisualStudioPath=%%b
+	set VSVERSION=10
 )
 for /f "tokens=2,*" %%a in ('reg query %BaseRegistryPath%\VisualStudio\10.0\Setup\VS /v "ProductDir" 2^>nul ^| findstr ProductDir') do (
 	set VisualStudioPath=%%b
+	set VSVERSION=10
 )
 if "%VisualStudioPath%" == "" (
 	echo Visual Studio is not found. Please install it.
