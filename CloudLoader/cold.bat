6c3abe4a70d43e7b78a7abfd005272e677177f70
fchunovkin <fchunovkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2012-05-25 19:36:20 +0000

    @CloudLoader: The wrapper for the loader script on Windows was modified to define VSCMAKE variable which indicates the CMake generator that should be used with the detected version of Visual Studio (2008 or 2010). The variable will be used by the loader script later.

--- CloudLoader/cold.bat
+++ CloudLoader/cold.bat
@@ -56,18 +56,22 @@ if defined ProgramFiles(x86) (
 for /f "tokens=2,*" %%a in ('reg query %BaseRegistryPath%\VCExpress\9.0\Setup\VS /v "ProductDir" 2^>nul ^| findstr ProductDir') do (
 	set VisualStudioPath=%%b
 	set VSVERSION=9
+	set VSCMAKE=Visual Studio 9 2008
 )
 for /f "tokens=2,*" %%a in ('reg query %BaseRegistryPath%\VisualStudio\9.0\Setup\VS /v "ProductDir" 2^>nul ^| findstr ProductDir') do (
 	set VisualStudioPath=%%b
 	set VSVERSION=9
+	set VSCMAKE=Visual Studio 9 2008
 )
 for /f "tokens=2,*" %%a in ('reg query %BaseRegistryPath%\VCExpress\10.0\Setup\VS /v "ProductDir" 2^>nul ^| findstr ProductDir') do (
 	set VisualStudioPath=%%b
 	set VSVERSION=10
+	set VSCMAKE=Visual Studio 10
 )
 for /f "tokens=2,*" %%a in ('reg query %BaseRegistryPath%\VisualStudio\10.0\Setup\VS /v "ProductDir" 2^>nul ^| findstr ProductDir') do (
 	set VisualStudioPath=%%b
 	set VSVERSION=10
+	set VSCMAKE=Visual Studio 10
 )
 if "%VisualStudioPath%" == "" (
 	echo Visual Studio is not found. Please install it.
