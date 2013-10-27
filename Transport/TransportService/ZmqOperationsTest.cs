5e27e676a132bb75b0c28faa0914b7a2d669c1e1
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2013-10-27 14:01:47 +0000

    @v0.6: Round-robin operations sample

--- Transport/TransportService/ZmqOperationsTest.cs
+++ Transport/TransportService/ZmqOperationsTest.cs
@@ -16,49 +16,41 @@ namespace TransportService
 
             var context = ZmqContext.Create();
 
-            var server = context.CreateSocket(SocketType.ROUTER);
-            server.Bind("tcp://*:4782");
+            var serverA = context.CreateSocket(SocketType.ROUTER);
+            serverA.Bind("tcp://*:4782");
 
-            var clientA = context.CreateSocket(SocketType.DEALER);
-            clientA.Identity = "A".ToByteArray();
-            clientA.Connect("tcp://localhost:4782");
+            var serverB = context.CreateSocket(SocketType.ROUTER);
+            serverB.Bind("tcp://*:4783");
 
-            var clientB = context.CreateSocket(SocketType.DEALER);
-            clientB.Identity = "B".ToByteArray();
-            clientB.Connect("tcp://localhost:4782");
+            var client = context.CreateSocket(SocketType.DEALER);
+            client.Identity = "client".ToByteArray();
+            client.Connect("tcp://localhost:4782");
+            client.Connect("tcp://localhost:4783");
 
-            clientA.ReceiveReady += OnServerResponseToA;
-            clientB.ReceiveReady += OnServerResponseToB;
-            server.ReceiveReady += OnServerRequest;
+            client.ReceiveReady += OnServerResponse;
+            serverA.ReceiveReady += (s, a) => OnServerRequest(s, a, "SA");
+            serverB.ReceiveReady += (s, a) => OnServerRequest(s, a, "SB");
 
-            var poller = new Poller(new List<ZmqSocket> { clientA, clientB, server });
+            var poller = new Poller(new List<ZmqSocket> { client, serverA, serverB });
 
             var timeout = new TimeSpan(0, 0, 1);
 
             while (true)
             {
                 poller.Poll(timeout);
-                clientA.Send("Hello".ToByteArray());
+                client.Send("Hello".ToByteArray());
             }
         }
 
-        private static void OnServerResponseToA(object sender, SocketEventArgs e)
+        private static void OnServerResponse(object sender, SocketEventArgs e)
         {
             var result = e.Socket.ReceiveMessage();
 
             var message = result.Aggregate("", (current, f) => current + f.Buffer.ToUtf8() + "\n");
-            Console.Write("client A receiverd: {0}", message);
+            Console.Write("client receiverd: {0}", message);
         }
 
-        private static void OnServerResponseToB(object sender, SocketEventArgs e)
-        {
-            var result = e.Socket.ReceiveMessage();
-
-            var message = result.Aggregate("", (current, f) => current + f.Buffer.ToUtf8() + "\n");
-            Console.Write("client B receiverd: {0}", message);
-        }
-
-        private static void OnServerRequest(object sender, SocketEventArgs e)
+        private static void OnServerRequest(object sender, SocketEventArgs e, string serverName)
         {
             var server = e.Socket;
             var result = server.ReceiveMessage();
@@ -67,12 +59,12 @@ namespace TransportService
             {
                 var senderId = result[0].Buffer.ToUtf8();
                 var message = new byte[0];
-                for(var i = 1; i < result.FrameCount; ++i)
+                for (var i = 1; i < result.FrameCount; ++i)
                 {
                     message = message.Concat(result[i].Buffer).ToArray();
                 }
 
-                Console.WriteLine("server received: {0}, from {1}", message.ToUtf8(), senderId);
+                Console.WriteLine("server {0} received: {1}, from {2}", serverName, message.ToUtf8(), senderId);
 
                 server.SendMore(senderId.ToByteArray());
                 server.Send("world".ToByteArray());
