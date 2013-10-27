ac85f01ab79358b7174c7de998ce8e57bfdc27c9
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2013-10-27 16:27:58 +0000

    @v0.6: Transport Connection Factory interface

--- Transport/TransportAPI/Interface/IConnection.cs
+++ Transport/TransportAPI/Interface/IConnection.cs
@@ -2,12 +2,20 @@ using System;
 
 namespace Transport.Interface
 {
+    public delegate void Response(byte[] data);
+
+    public interface IConnectionFactory
+    {
+        IConnection GetConnection(string address);
+    }
+
     public interface IConnection
     {
-        void Connect(string address);
-
-        void SubscribeToOperations(string topic, Action<byte[]> callback);
+        void SubscribeToOperations(string topic, Action<byte[], Response> callback);    
         void SubscribeToEvents(string topic, Action<byte[]> callback);
+
+        void UnSubscribeToOperations(string topic);
+        void UnSubscribeToEvents(string topic);
 
         IOperationsPublisher GetOperationsPublisher(string topic);
         IEventsPublisher GetEventsPublisher(string topic);
