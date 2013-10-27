d38e3f1c26973eb744beb7d10d0ccd97a5f1fbaf
oleg.jakushkin <oleg.jakushkin@696a5d69-19d7-2beb-5e15-9d02a1c5e70f>
2013-10-27 01:25:55 +0000

    @v0.6: basic ZeroMQ Pub-Sub sample with topics

--- Transport/TransportService/Program.cs
+++ Transport/TransportService/Program.cs
@@ -1,20 +1,112 @@
-﻿using System;
+﻿#region
+
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using Transport.Protocol.Utilities;
+using ZeroMQ;
+
+#endregion
 
 namespace TransportService
 {
-    class Program
+    public static class Extensions
     {
-        static void Main()
+        public static byte[] ToByteArray(this string tag)
         {
-            /*
+            return Encoding.UTF8.GetBytes(tag);
+        }
+    }
+
+    internal class Program
+    {
+        private static void TestProtocol()
+        {
             var schema = SchemaPrinter.Print();
             Console.WriteLine(schema);
             File.WriteAllText("TransportProtocol.proto", schema);
+        }
 
-            Console.ReadKey();
-             */
+        private static void TestZmq()
+        {
+            var context = ZmqContext.Create();
+            var publisher = context.CreateSocket(SocketType.XPUB);
+            publisher.Bind("tcp://*:4780");
+
+            var subscriberA = context.CreateSocket(SocketType.XSUB);
+            subscriberA.Connect("tcp://127.0.0.1:4780");
+            subscriberA.Subscribe("A".ToByteArray());
+
+
+            var subscriberB = context.CreateSocket(SocketType.XSUB);
+            subscriberB.Connect("tcp://127.0.0.1:4780");
+            subscriberB.Subscribe("B".ToByteArray());
+
+            subscriberA.ReceiveReady += OnMessageToA;
+            subscriberB.ReceiveReady += OnMessageToB;
+            publisher.SendReady += OnSender;
+
+            var poller = new Poller(new List<ZmqSocket> {publisher, subscriberA, subscriberB});
+
+            var sleep = new TimeSpan(0, 0, 1);
+            while (true)
+            {
+                poller.Poll(sleep);
+            }
+        }
+
+        private static void OnMessageToA(object sender, SocketEventArgs e)
+        {
+            var result = e.Socket.ReceiveMessage();
+            if (result.FrameCount > 1)
+            {
+                Console.WriteLine("A said: {0}", Encoding.UTF8.GetString(result[1].Buffer));
+            }
+            else
+            {
+                Console.WriteLine("error on A");
+            }
+        }
+
+        private static void OnMessageToB(object sender, SocketEventArgs e)
+        {
+            var result = e.Socket.ReceiveMessage();
+            if (result.FrameCount > 1)
+            {
+                Console.WriteLine("B said: {0}", Encoding.UTF8.GetString(result[1].Buffer));
+            }
+            else
+            {
+                Console.WriteLine("error on B");
+            }
+        }
+
+        private static void OnSender(object sender, SocketEventArgs e)
+        {
+            var m2A = new ZmqMessage();
+            m2A.Append("A".ToByteArray());
+            m2A.Append("data".ToByteArray());
+            e.Socket.SendMessage(m2A);
+
+            var m2B = new ZmqMessage();
+            m2B.Append("B".ToByteArray());
+            m2B.Append("data 2".ToByteArray());
+            e.Socket.SendMessage(m2B);
+        }
+
+        private static void Main()
+        {
+            try
+            {
+                TestProtocol();
+                TestZmq();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+                Console.ReadKey();
+            }
         }
     }
 }
